f018c0cb8fc7e9bc21e3d5cb6cd0f1d56cf8d7d9
Ataollah Roudgar <aroudgar@sfu.ca>
2017-12-07 18:44:10 -0500

    Update build_wheel.sh

--- build_wheel.sh
+++ build_wheel.sh
@@ -96,17 +96,19 @@ elif [[ "$PACKAGE" == "mpi4py" ]]; then
 elif [[ "$PACKAGE" == "pytorch-cpu" ]];then
 	PACKAGE="pytorch"
 	MODULE_DEPS="gcc/6.4.0 imkl/11.3.4.258"
-        PYTHON_DEPS="pyyaml numpy"
-        PRE_BUILD_COMMANDS="export MAX_JOBS=3; export MKL_ROOT=$MKLROOT; export MKL_LIBRARY=$MKLROOT/lib/intel64; export CMAKE_LIBRARY_PATH=$MKL_LIBRARY"
+    PYTHON_DEPS="pyyaml numpy"
+    PRE_BUILD_COMMANDS="export MAX_JOBS=3; export MKL_ROOT=$MKLROOT; export MKL_LIBRARY=$MKLROOT/lib/intel64; export CMAKE_LIBRARY_PATH=$MKL_LIBRARY"
 	PACKAGE_FOLDER_NAME="$PACKAGE"
 	PACKAGE_SUFFIX='-cpu'
+	PYTHON_IMPORT_NAME="torch"
 elif [[ "$PACKAGE" == "pytorch-gpu" ]];then
 	PACKAGE="pytorch"
-        MODULE_DEPS="imkl/11.3.4.258 gcc/5.4.0 magma/2.2.0 cuda/8.0.44 cudnn/7.0 magma/2.2.0"
-        PYTHON_DEPS="pyyaml numpy"
-        PRE_BUILD_COMMANDS="export MAX_JOBS=3; export MKL_ROOT=$MKLROOT; export MKL_LIBRARY=$MKLROOT/lib/intel64; export LIBRARY_PATH=/cvmfs/soft.computecanada.ca/nix/lib/:$LIBRARY_PATH; export CMAKE_PREFIX_PATH=$EBROOTMAGMA; export CMAKE_LIBRARY_PATH=$MKL_LIBRARY"
-        PACKAGE_FOLDER_NAME="$PACKAGE"
-        PACKAGE_SUFFIX='-gpu'
+    MODULE_DEPS="imkl/11.3.4.258 gcc/5.4.0 magma/2.2.0 cuda/8.0.44 cudnn/7.0 magma/2.2.0"
+    PYTHON_DEPS="pyyaml numpy"
+    PRE_BUILD_COMMANDS="export MAX_JOBS=3; export MKL_ROOT=$MKLROOT; export MKL_LIBRARY=$MKLROOT/lib/intel64; export LIBRARY_PATH=/cvmfs/soft.computecanada.ca/nix/lib/:$LIBRARY_PATH; export CMAKE_PREFIX_PATH=$EBROOTMAGMA; export CMAKE_LIBRARY_PATH=$MKL_LIBRARY"
+    PACKAGE_FOLDER_NAME="$PACKAGE"
+    PACKAGE_SUFFIX='-gpu'
+    PYTHON_IMPORT_NAME="torch"
 elif [[ "$PACKAGE" == "mpmath" ]]; then
 	# need to patch it so it supports bdist_wheel
 	PRE_BUILD_COMMANDS='sed -i -e "s/distutils.core/setuptools/g" setup.py'
