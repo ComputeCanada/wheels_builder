f0e4072738da2a6ce5d559f73929a956d86d5d6a
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2018-04-30 13:54:20 +0000

    Included a bunch of new python packages to the build script

--- build_wheel.sh
+++ build_wheel.sh
@@ -4,23 +4,62 @@ if [[ -z "$PYTHON_VERSIONS" ]]; then
 	PYTHON_VERSIONS="python/2.7 python/3.5 python/3.6"
 fi
 
-ALL_PACKAGES="nose numpy scipy Cython h5py matplotlib dateutil numexpr bottleneck pandas pyzmq qiime future pyqi bio-format cogent qiime-default-reference pynast burrito burrito-fillings gdata emperor qcli scikit-bio natsort click subprocess32 cycler python-dateutil dlib shapely affine rasterio numba llvmlite velocyto htseq mpi4py sympy mpmath blist paycheck lockfile deap arff cryptography paramiko pyparsing netifaces netaddr funcsigs mock pytz enum34 bitstring Cycler PyZMQ path.py pysqlite requests nbformat Pygments singledispatch certifi backports_abc tornado MarkupSafe Jinja2 jupyter_client functools32 jsonschema mistune ptyprocess terminado simplegeneric ipython_genutils pathlib2 pickleshare traitlets notebook jupyter_core ipykernel pexpect backports.shutil_get_terminal_size prompt_toolkit ipywidgets widgetsnbextension ipython iptest testpath cffi pycparser asn1crypto ipaddress pynacl pyasn1 bcrypt nbconvert entrypoints configparser pandocfilters dnspython pygame pyyaml fuel pillow olefile seaborn theano Amara bx-python python-lzo RSeQC xopen cutadapt cgat kiwisolver torchvision dask distributed arboretum netCDF4 mdtraj"
+ALL_PACKAGES="nose numpy scipy Cython h5py matplotlib dateutil numexpr bottleneck pandas pyzmq qiime future pyqi bio-format cogent qiime-default-reference pynast burrito burrito-fillings gdata emperor qcli scikit-bio natsort click subprocess32 cycler python-dateutil dlib shapely affine rasterio numba llvmlite velocyto htseq mpi4py sympy mpmath blist paycheck lockfile deap arff cryptography paramiko pyparsing netifaces netaddr funcsigs mock pytz enum34 bitstring Cycler PyZMQ path.py pysqlite requests nbformat Pygments singledispatch certifi backports_abc tornado MarkupSafe Jinja2 jupyter_client functools32 jsonschema mistune ptyprocess terminado simplegeneric ipython_genutils pathlib2 pickleshare traitlets notebook jupyter_core ipykernel pexpect backports.shutil_get_terminal_size prompt_toolkit ipywidgets widgetsnbextension ipython iptest testpath cffi pycparser asn1crypto ipaddress pynacl pyasn1 bcrypt nbconvert entrypoints configparser pandocfilters dnspython pygame pyyaml fuel pillow olefile seaborn theano Amara bx-python python-lzo RSeQC xopen cutadapt cgat kiwisolver torchvision dask distributed arboretum netCDF4 mdtraj biom-format grpcio absl-py gast protobuf tensorboard astor Markdown metasv cvxpy cvxopt dill multiprocess scs fastcache toolz eco CVXcanon CoffeeScript PyExecJS"
 
 PACKAGE=$1
 VERSION=$2
 PYTHON_IMPORT_NAME="$PACKAGE"
 PACKAGE_FOLDER_NAME="$PACKAGE"
+PACKAGE_DOWNLOAD_NAME="$PACKAGE"
+if [[ -n "$VERSION" ]]; then
+	PACKAGE_DOWNLOAD_ARGUMENT="$PACKAGE==$VERSION"
+else
+	PACKAGE_DOWNLOAD_ARGUMENT="$PACKAGE"
+fi
 RPATH_TO_ADD=""
 if [[ "$PACKAGE" == "numpy" ]]; then
 	MODULE_DEPS="imkl"
 	PYTHON_DEPS="nose pytest"
 	PYTHON_TESTS="numpy.__config__.show(); numpy.test()"
+elif [[ "$PACKAGE" == "ray" ]]; then
+	if [[ -z "$VERSION" ]]; then
+		VERSION="0.4.0"
+	fi
+	PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/ray-project/ray/archive/ray-$VERSION.tar.gz"
+	MODULE_DEPS="gcc/5.4.0 qt/5.10.1"
+	PYTHON_DEPS="numpy scipy cython"
+	PACKAGE_FOLDER_NAME="ray-ray-*/python"
+	PRE_BUILD_COMMANDS="pwd; sed -i -e 's/-DPARQUET_BUILD_TESTS=off/-DPARQUET_BUILD_TESTS=off -DCMAKE_SKIP_RPATH=ON -DCMAKE_SKIP_INSTALL_RPATH=ON/g' ../thirdparty/scripts/build_parquet.sh && sed -i -e 's/-DARROW_WITH_ZSTD=off/-DARROW_WITH_ZSTD=off  -DCMAKE_SKIP_RPATH=ON -DCMAKE_SKIP_INSTALL_RPATH=ON/g' ../thirdparty/scripts/build_arrow.sh"
+elif [[ "$PACKAGE" == "scs" ]]; then
+	PYTHON_DEPS="numpy scipy"
+elif [[ "$PACKAGE" == "absl-py" ]]; then
+	PYTHON_IMPORT_NAME="absl"
+elif [[ "$PACKAGE" == "CoffeeScript" ]]; then
+	PYTHON_DEPS="PyExecJS"
+elif [[ "$PACKAGE" == "PyExecJS" ]]; then
+	PYTHON_IMPORT_NAME="execjs"
+elif [[ "$PACKAGE" == "CVXcanon" ]]; then
+	PYTHON_DEPS="numpy scipy"
+elif [[ "$PACKAGE" == "cvxpy" ]]; then
+	PYTHON_DEPS="numpy multiprocess scs fastcache scipy six toolz CVXcanon dill"
+elif [[ "$PACKAGE" == "cvxopt" ]]; then
+	MODULE_DEPS="suitesparse fftw imkl"
+	PRE_BUILD_COMMANDS="export CVXOPT_LAPACK_LIB=mkl_rt; export CVXOPT_BLAS_LIB=mkl_rt; export CVXOPT_BLAS_LIB_DIR=$MKLROOT/lib; export CVXOPT_SUITESPARSE_LIB_DIR=$EBROOTSUITESPARSE/lib; export CVXOPT_SUITESPARSE_INC_DIR=$EBROOTSUITESPARSE/include; "
+elif [[ "$PACKAGE" == "multiprocess" ]]; then
+	PYTHON_DEPS="dill"
+elif [[ "$PACKAGE" == "grpcio" ]]; then
+	PYTHON_IMPORT_NAME="grpc"
+elif [[ "$PACKAGE" == "metasv" ]]; then
+	PYTHON_DEPS="cython pysam"
 elif [[ "$PACKAGE" == "scipy" ]]; then
 	MODULE_DEPS="imkl"
 	PYTHON_DEPS="nose numpy pytest"
 	PYTHON_TESTS="scipy.__config__.show(); scipy.test()"
 elif [[ "$PACKAGE" == "mdtraj" ]]; then
 	PYTHON_DEPS="cython numpy scipy pandas"
+elif [[ "$PACKAGE" == "biom-format" ]]; then
+	PYTHON_DEPS="scipy"
+	PACKAGE_DOWNLOAD_NAME="biom_format"
 elif [[ "$PACKAGE" == "netCDF4" ]]; then
 	MODULE_DEPS="hdf5-mpi netcdf-mpi"
 	PYTHON_DEPS="numpy Cython"
@@ -50,6 +89,7 @@ elif [[ "$PACKAGE" == "numexpr" ]]; then
 elif [[ "$PACKAGE" == "bottleneck" ]]; then
 	PYTHON_DEPS="numpy nose"
 	PYTHON_TESTS="bottleneck.test();" # bottleneck.bench()"
+	PACKAGE_DOWNLOAD_NAME="Bottleneck"
 	PACKAGE_FOLDER_NAME="Bottleneck"
 elif [[ "$PACKAGE" == "tables" ]]; then
 	MODULE_DEPS="hdf5"
@@ -76,6 +116,7 @@ elif [[ "$PACKAGE" == "dlib-cpu" ]]; then
 	PACKAGE="dlib"
 	PYTHON_IMPORT_NAME="$PACKAGE"
 	PACKAGE_FOLDER_NAME="$PACKAGE"
+	PACKAGE_DOWNLOAD_NAME="$PACKAGE"
 	PACKAGE_SUFFIX='-cpu'
 elif [[ "$PACKAGE" == "dlib-gpu" ]]; then
 	MODULE_DEPS="gcc/5.4.0 boost imkl cuda cudnn"    # it does not work with Intel, and requires Boost
@@ -84,12 +125,14 @@ elif [[ "$PACKAGE" == "dlib-gpu" ]]; then
 	PACKAGE="dlib"
 	PYTHON_IMPORT_NAME="$PACKAGE"
 	PACKAGE_FOLDER_NAME="$PACKAGE"
+	PACKAGE_DOWNLOAD_NAME="$PACKAGE"
 	PACKAGE_SUFFIX='-gpu'
 elif [[ "$PACKAGE" == "shapely" ]]; then
 	MODULE_DEPS="geos"
 	# need to patch geos.py to find libgeos_c.so based on the module that was loaded at build time
 	PRE_BUILD_COMMANDS='sed -i -e "s;os.path.join(sys.prefix, \"lib\", \"libgeos_c.so\"),;\"$EBROOTGEOS/lib/libgeos_c.so\",;g" $(find . -name "geos.py")'
 	PACKAGE_FOLDER_NAME="Shapely"
+	PACKAGE_DOWNLOAD_NAME="Shapely"
 elif [[ "$PACKAGE" == "rasterio" ]]; then
 	PYTHON_DEPS="numpy affine snuggs cligj click-plugins enum34"
 	MODULE_DEPS="gdal"
@@ -111,6 +154,7 @@ elif [[ "$PACKAGE" == "velocyto" ]]; then
 elif [[ "$PACKAGE" == "htseq" ]]; then
 	PYTHON_DEPS="numpy Cython pysam"
 	PACKAGE_FOLDER_NAME="HTSeq"
+	PACKAGE_DOWNLOAD_NAME="HTSeq"
 	PYTHON_IMPORT_NAME="HTSeq"
 elif [[ "$PACKAGE" == "mpi4py" ]]; then
 	MODULE_DEPS="openmpi"
@@ -120,6 +164,7 @@ elif [[ "$PACKAGE" == "pytorch-cpu" ]];then
     PYTHON_DEPS="pyyaml numpy typing"
     PRE_BUILD_COMMANDS="export MAX_JOBS=3; export MKL_ROOT=$MKLROOT; export MKL_LIBRARY=$MKLROOT/lib/intel64; export CMAKE_LIBRARY_PATH=$MKL_LIBRARY"
 	PACKAGE_FOLDER_NAME="$PACKAGE"
+	PACKAGE_DOWNLOAD_NAME="$PACKAGE"
 	PACKAGE_SUFFIX='-cpu'
 	PYTHON_IMPORT_NAME="torch"
 elif [[ "$PACKAGE" == "pytorch-gpu" ]];then
@@ -128,6 +173,7 @@ elif [[ "$PACKAGE" == "pytorch-gpu" ]];then
     PYTHON_DEPS="pyyaml numpy typing"
     PRE_BUILD_COMMANDS="export MAX_JOBS=3; export MKL_ROOT=$MKLROOT; export MKL_LIBRARY=$MKLROOT/lib/intel64; export LIBRARY_PATH=/cvmfs/soft.computecanada.ca/nix/lib/:$LIBRARY_PATH; export CMAKE_PREFIX_PATH=$EBROOTMAGMA; export CMAKE_LIBRARY_PATH=$MKL_LIBRARY"
     PACKAGE_FOLDER_NAME="$PACKAGE"
+	PACKAGE_DOWNLOAD_NAME="$PACKAGE"
     PACKAGE_SUFFIX='-gpu'
     PYTHON_IMPORT_NAME="torch"
 elif [[ "$PACKAGE" == "mpmath" ]]; then
@@ -157,6 +203,7 @@ elif [[ "$PACKAGE" == "python-lzo" ]]; then
 	PYTHON_IMPORT_NAME="lzo"
 elif [[ "$PACKAGE" == "iptest" ]]; then
 	PACKAGE_FOLDER_NAME="IPTest"
+	PACKAGE_DOWNLOAD_NAME="IPTest"
 	PYTHON_VERSIONS="python/2.7"
 elif [[ "$PACKAGE" == "sympy" ]]; then
 	PYTHON_DEPS="mpmath"
@@ -166,6 +213,7 @@ elif [[ "$PACKAGE" == "ipaddress" ]]; then
 	PYTHON_VERSIONS="python/2.7"
 elif [[ "$PACKAGE" == "pynacl" ]]; then
 	PACKAGE_FOLDER_NAME="PyNaCl"
+	PACKAGE_DOWNLOAD_NAME="PyNaCl"
 	PYTHON_IMPORT_NAME="nacl"
 elif [[ "$PACKAGE" == "functools32" ]]; then
 	PYTHON_VERSIONS="python/2.7"
@@ -175,10 +223,12 @@ elif [[ "$PACKAGE" == "pygame" ]]; then
 	PRE_BUILD_COMMANDS="export LOCALBASE=$NIXUSER_PROFILE"
 elif [[ "$PACKAGE" == "pyyaml" ]]; then
 	PACKAGE_FOLDER_NAME="PyYAML"
+	PACKAGE_DOWNLOAD_NAME="PyYAML"
 	PYTHON_IMPORT_NAME="yaml"
 elif [[ "$PACKAGE" == "pillow" ]]; then
 	PYTHON_DEPS="olefile"
 	PACKAGE_FOLDER_NAME="Pillow"
+	PACKAGE_DOWNLOAD_NAME="Pillow"
 	PYTHON_IMPORT_NAME="PIL"
 elif [[ "$PACKAGE" == "olefile" ]]; then
 	# need to patch it so it supports bdist_wheel
@@ -191,6 +241,7 @@ elif [[ "$PACKAGE" == "backports.functools-lru-cache" ]]; then
 	PYTHON_IMPORT_NAME="backports.functools_lru_cache"
 elif [[ "$PACKAGE" == "theano" ]]; then
 	PACKAGE_FOLDER_NAME="Theano"
+	PACKAGE_DOWNLOAD_NAME="Theano"
 	PYTHON_DEPS="numpy scipy six"
 elif [[ "$PACKAGE" == "alignlib-lite" ]]; then
 	MODULE_DEPS="boost"
@@ -235,12 +286,8 @@ for pv in $PYTHON_VERSIONS; do
 		fi
 		git submodule update --init
 	else
-		if [[ -n "$VERSION" ]]; then
-			pip download --no-binary --no-deps $PACKAGE==$VERSION
-		else
-			pip download --no-binary --no-deps $PACKAGE
-		fi
-		ARCHNAME=$(ls $PACKAGE_FOLDER_NAME-[0-9]*{.zip,.tar.gz,.whl})
+		pip download --no-binary --no-deps $PACKAGE_DOWNLOAD_ARGUMENT
+		ARCHNAME=$(ls $PACKAGE_DOWNLOAD_NAME-[0-9]*{.zip,.tar.gz,.whl})
 		# skip packages that are already in whl format
 		if [[ $ARCHNAME == *.whl ]]; then
 			cp $ARCHNAME ..
@@ -264,6 +311,7 @@ mkl_libs = mkl_rt
 lapack_libs =
 EOF
 	fi
+	eval $PRE_BUILD_COMMANDS
 	eval $PRE_BUILD_COMMANDS
 	# change the name of the wheel to add a suffix
 	if [[ -n "$PACKAGE_SUFFIX" ]]; then
