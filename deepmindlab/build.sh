69a02a68cf3dd3c4cb5c6390c07674a477a057d3
Evgeny Naumov <evgeny.naumov@calculquebec.ca>
2018-09-12 18:24:46 +0000

    deepmindlab: support multiple python versions

--- deepmindlab/build.sh
+++ deepmindlab/build.sh
@@ -1,12 +1,20 @@
 #!/cvmfs/soft.computecanada.ca/nix/var/nix/profiles/16.09/bin/bash
 
+shopt -s nullglob
+set -e
+PYVER="$1"
+
+if [[ ! -f "cc_build_py$PYVER.patch" ]] ; then
+    "Unable to find patch file for python version $PYVER. Dying"
+    exit 1
+fi
+
 module load \
     bazel \
-    python/2.7 \
+    python/$PYVER \
     scipy-stack
 
 set -x
-shopt -s nullglob
 
 WDIR=$(pwd)
 BDIR=$(mktemp -d)
@@ -14,12 +22,13 @@ BDIR=$(mktemp -d)
 cd $BDIR
 git clone --depth=1 --branch=release-2018-06-20 https://github.com/deepmind/lab
 cd *
-git apply $WDIR/cc_build.patch
+git apply $WDIR/cc_build_py$PYVER.patch
 
 bazel clean --expunge
 bazel build\
 	-c opt \
 	--jobs 24 \
+    --verbose_failures \
 	--action_env=C_INCLUDE_PATH="${C_INCLUDE_PATH}:$(python -c 'import numpy; print(numpy.get_include())')"\
 	--action_env=LIBFFI_CFLAGS="-I/cvmfs/soft.computecanada.ca/nix/var/nix/profiles/16.09/include"\
 	--action_env=LIBFFI_LIBS="-L/cvmfs/soft.computecanada.ca/nix/var/nix/profiles/16.09/lib -lffi"\
