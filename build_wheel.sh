dde390032e65addcdbdd2ba0f99a80ac90e54f63
Charles Coulombe <charles.coulombe@calculquebec.ca>
2018-10-18 18:13:28 +0000

    Fixed torchtext for 0.3.1

--- build_wheel.sh
+++ build_wheel.sh
@@ -338,6 +338,10 @@ elif [[ "$PACKAGE" == "torchtext" ]]; then
         # torch_cpu, six and numpy are only for testing purposes. They are not in torchtext requirements.
         # torchtext should be installed along with : numpy, six, torch-[cg]pu
         PYTHON_DEPS="certifi urllib3 chardet idna requests tqdm six numpy torch_cpu"
+        
+        # Remove torch requirements as the user need to either install torch-[cg]pu wheel and not PyPI torch.
+        # Remove egg-info artefact as we are rebuilding a wheel.
+        PRE_BUILD_COMMANDS="sed -i -e \"s/'torch',//g\" setup.py; rm -r torchtext.egg-info;"
 elif [[ "$PACKAGE" == "wxPython" ]]; then
         MODULE_DEPS="gtk+3/3.20.9"
         PYTHON_DEPS="six typing PyPubSub"
