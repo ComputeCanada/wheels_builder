dc97beb1adf565aa4a0e1a4950d7e19da85074a6
fafor10 <felix-antoine.fortin@calculquebec.ca>
2019-02-26 22:06:37 +0000

    Specify gcc/7.3.0 when building h5py

--- build_wheel.sh
+++ build_wheel.sh
@@ -221,7 +221,7 @@ elif [[ "$PACKAGE" == "cgat" ]]; then
 	PYTHON_IMPORT_NAME="cgat"
 	PYTHON_DEPS="numpy cython pysam setuptools pyparsing pyaml alignlib-lite matplotlib biopython"
 elif [[ "$PACKAGE" == "h5py" ]]; then
-	MODULE_BUILD_DEPS="gcc hdf5"
+	MODULE_BUILD_DEPS="gcc/7.3.0 hdf5"
 	PYTHON_DEPS="nose numpy six Cython unittest2"
 	PYTHON_TESTS="h5py.run_tests()"
 elif [[ "$PACKAGE" == "matplotlib" ]]; then
