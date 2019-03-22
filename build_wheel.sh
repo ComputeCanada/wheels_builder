891e9078388bbe912767d0d2a90520bc7666b297
Charles Coulombe <charles.coulombe@calculquebec.ca>
2019-03-22 14:44:05 +0000

    Updated cupy with cuda10

--- build_wheel.sh
+++ build_wheel.sh
@@ -128,7 +128,7 @@ elif [[ "$PACKAGE" == "openslide-python" ]]; then
 	PRE_BUILD_COMMANDS='sed -i -e "/import sys/a import os" -e "s;.libopenslide.so.0.;os.environ.get(\"EBROOTOPENSLIDE\",\"$EBROOTOPENSLIDE\") + \"/lib/libopenslide.so.0\";g" $(find . -name "lowlevel.py")'
 elif [[ "$PACKAGE" == "cupy" ]]; then
 	PYTHON_DEPS="numpy fastrlock"
-	MODULE_BUILD_DEPS="gcc/5.4.0 cuda/9.0 cudnn/7.0 nccl/2.3.5"
+	MODULE_BUILD_DEPS="gcc/7.3.0 cuda/10 cudnn nccl"
 	# needed otherwise it does not find libcuda.so
 	PRE_DOWNLOAD_COMMANDS='export LDFLAGS="-L$EBROOTCUDA/lib64/stubs -L$EBROOTNCCL/lib" ; export CFLAGS="-I$EBROOTNCCL/include/"'
 elif [[ "$PACKAGE" == "chainer" ]]; then
