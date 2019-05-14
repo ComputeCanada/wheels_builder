d68d76d4017bee2c231a3dcf334d086857c1b8e6
Pawel Pomorski <ppomorsk@sharcnet.ca>
2019-05-14 22:31:06 +0000

    added blmath

--- build_wheel.sh
+++ build_wheel.sh
@@ -523,6 +523,11 @@ elif [[ "$PACKAGE" == "GPy" ]]; then
 elif [[ "$PACKAGE" == "GPyOpt" ]]; then
     PYTHON_DEPS="matplotlib"
     PYTHON_VERSIONS="python/2.7 python/3.5 python/3.6"
+elif [[ "$PACKAGE" == "blmath" ]]; then
+    PRE_BUILD_COMMANDS='sed -i -e "s@-fno-inline@-fno-inline\x27,\x27-L${EBROOTSUITESPARSE}/lib@g" setup.py; sed -i -e "s@/usr/include/suitesparse@${EBROOTSUITESPARSE}/include@g" setup.py; sed -i -e "s@lapack@mkl@g" setup.py'
+    MODULE_BUILD_DEPS="suitesparse imkl"
+    PYTHON_DEPS="numpy"
+    PYTHON_VERSIONS="python/2.7"
 fi
 
 DIR=tmp.$$
