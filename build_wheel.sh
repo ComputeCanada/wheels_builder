7adbe6130946f7d144c3d8b8c55cfc9e6f640c3d
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2019-08-06 13:56:00 +0000

    added parameters for MinkowskiEngine

--- build_wheel.sh
+++ build_wheel.sh
@@ -189,6 +189,13 @@ elif [[ "$PACKAGE" == "ray" ]]; then
 	PYTHON_DEPS="numpy scipy cython"
 	PACKAGE_FOLDER_NAME="ray-ray-*/python"
 	PRE_BUILD_COMMANDS="pwd; sed -i -e 's/-DPARQUET_BUILD_TESTS=off/-DPARQUET_BUILD_TESTS=off -DCMAKE_SKIP_RPATH=ON -DCMAKE_SKIP_INSTALL_RPATH=ON/g' ../thirdparty/scripts/build_parquet.sh && sed -i -e 's/-DARROW_WITH_ZSTD=off/-DARROW_WITH_ZSTD=off  -DCMAKE_SKIP_RPATH=ON -DCMAKE_SKIP_INSTALL_RPATH=ON/g' ../thirdparty/scripts/build_arrow.sh"
+elif [[ "$PACKAGE" == "MinkowskiEngine" ]]; then
+	PYTHON_VERSIONS="python/3.6 python/3.7"
+	MODULE_BUILD_DEPS="cuda/10 imkl"
+	PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/StanfordVL/MinkowskiEngine/archive/master.zip"
+	PACKAGE_DOWNLOAD_NAME="master.zip"
+	PRE_BUILD_COMMANDS='sed -i -e "s/make /make BLAS=mkl /g" -e "s/, .openblas.//g" $(find . -name "setup.py")'
+	PYTHON_DEPS="torch"
 elif [[ "$PACKAGE" == "scs" ]]; then
 	PYTHON_DEPS="numpy scipy"
 elif [[ "$PACKAGE" == "PyWavelets" ]]; then
