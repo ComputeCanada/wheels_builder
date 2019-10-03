299aad863f900494f8d0c4ce262f13a4c147e009
Charles Coulombe <charles.coulombe@calculquebec.ca>
2019-10-03 13:40:49 -0400

    DRY + comments

--- parbuild_wheel.sh
+++ parbuild_wheel.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Build multiple versions of a wheel in parallel.
+# Usage: bash parbuild_wheel.sh cupy "6.4.0 5.3.0" "3.7 3.6 3.5"
+# To terminate, ctrl+c or `killall -TERM parallel`
+
 function ls_pythons()
 {
 	ls -1d /cvmfs/soft.computecanada.ca/easybuild/software/2017/Core/python/3* | grep -Po "\d\.\d" | sort -u
@@ -10,5 +14,6 @@ versions=${2?Missing versions}
 pythons=${3-$(ls_pythons)}
 
 # Yes, two times the command, display what will be run and then run it.
-parallel --dry-run PYTHON_VERSIONS="python/{1}" bash build_wheel.sh ${wheel} {2} "|&" tee build-${wheel}-{2}-py{1}.log ::: "${pythons}" ::: "${versions}"
-parallel           PYTHON_VERSIONS="python/{1}" bash build_wheel.sh ${wheel} {2} "|&" tee build-${wheel}-{2}-py{1}.log ::: "${pythons}" ::: "${versions}"
+cmd="PYTHON_VERSIONS="python/{1}" bash build_wheel.sh ${wheel} {2} |& tee build-${wheel}-{2}-py{1}.log ::: ${pythons} ::: ${versions}"
+parallel --dry-run $cmd
+parallel           $cmd
