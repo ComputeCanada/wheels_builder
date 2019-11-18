6af19baf1e12d25170b2d5d534a253b02597fa3c
Charles Coulombe <charles.coulombe@calculquebec.ca>
2019-11-18 16:30:08 -0500

    Added cuda supported archs for latest torch_extension

--- config/torch_geometric.sh
+++ config/torch_geometric.sh
@@ -1,3 +1,3 @@
 PYTHON_DEPS="torch>=1.1.0 torch-scatter torch-sparse torch-cluster torch-spline-conv"
-MODULE_BUILD_DEPS="gcc/7.3.0 cuda/10"
-
+MODULE_BUILD_DEPS="gcc/7.3.0 cuda/10.1"
+PRE_BUILD_COMMANDS="export TORCH_CUDA_ARCH_LIST='3.5;3.7;6.0;7.0'"
