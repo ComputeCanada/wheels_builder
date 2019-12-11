830c1775df758192f068320f456e37aa5ba6a1bc
Oliver Stueker <oliver.stueker@ace-net.ca>
2019-12-11 12:27:42 -0330

    config for minieigen

---
+++ config/minieigen.sh
@@ -0,0 +1,4 @@
+MODULE_BUILD_DEPS="gcc/7.3.0 openmpi/3.1.2 boost/1.68.0 eigen/3.3.5"
+MODULE_RUNTIME_DEPS="gcc/7.3.0 eigen/3.3.5"
+PATCHES="$CONFIGDIR/../patches/minieigen-0.5.4-boost1.68.0_libs.patch"
+
