571dad1e203814390e74892242cf525c620fdec4
Charles Coulombe <charles.coulombe@calculquebec.ca>
2020-04-19 21:57:12 -0400

    Upgraded scipy to mkl 2019.3

--- config/scipy.sh
+++ config/scipy.sh
@@ -1,5 +1,4 @@
-MODULE_BUILD_DEPS="imkl/2018.3.222"
+MODULE_BUILD_DEPS="imkl/2019.3.199"
 PYTHON_DEPS="nose numpy pytest"
 PYTHON_DEPS_DEFAULT=""
 PYTHON_TESTS="scipy.__config__.show(); scipy.test()"
-
