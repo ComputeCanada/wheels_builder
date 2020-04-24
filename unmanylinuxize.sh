3b8523345aee0efa3235867a3f38f848fb937b70
Charles Coulombe <charles.coulombe@calculquebec.ca>
2020-04-24 11:37:22 -0400

    Added --add_path and --add_origin to unmanylinuxize

--- unmanylinuxize.sh
+++ unmanylinuxize.sh
@@ -10,12 +10,14 @@ function print_usage
 	echo "Usage: $0 --package <package name> [--version <version>] [--python <comma separated list of python versions>]"
 }
 
-TEMP=$(getopt -o h --longoptions help,package:,version:,python: --name $0 -- "$@")
+TEMP=$(getopt -o h --longoptions help,package:,version:,python:,add_path:,add_origin --name $0 -- "$@")
 if [ $? != 0 ] ; then print_usage; exit 1 ; fi
 eval set -- "$TEMP"
 
 ARG_PACKAGE=""
 ARG_VERSION=""
+ARG_ADD_PATH=""
+ARG_ADD_ORIGIN=0
 while true; do
 	case "$1" in
 		--package)
@@ -24,6 +26,10 @@ while true; do
 			ARG_VERSION=$2; shift 2;;
 		--python)
 			ARG_PYTHON_VERSIONS=$2; shift 2;;
+		--add_path)
+			ARG_ADD_PATH=$2; shift 2 ;;
+		--add_origin)
+			ARG_ADD_ORIGIN=1; shift 1;;
 		-h|--help)
 			print_usage; exit 0 ;;
 		--)
@@ -50,10 +56,19 @@ for pv in $(echo ${ARG_PYTHON_VERSIONS-$(ls_pythons)} | tr ',' ' '); do
 	module load python/$pv
 	PYTHONPATH= pip download --no-deps $PACKAGE_DOWNLOAD_ARGUMENT
 done
+
+setrpaths_cmd="setrpaths.sh --path \$w"
+if [[ ! -z "$ARG_ADD_PATH" ]]; then
+	setrpaths_cmd="${setrpaths_cmd} --add_path ${ARG_ADD_PATH} --any_interpreter"
+fi
+if [[ "$ARG_ADD_ORIGIN" == "1" ]]; then
+	setrpaths_cmd="${setrpaths_cmd} --add_origin"
+fi
 for w in *.whl; do
-	setrpaths.sh --path $w
+	eval $setrpaths_cmd
 	mv $w ${w//$(echo $w | grep -Po "manylinux\d+")/linux}
 done
+
 # Ensure wheels are all readable!
 chmod a+r *.whl
 cp -vp *.whl ..
