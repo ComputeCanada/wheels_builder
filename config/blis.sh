b8eb29c52f2b8559cd08e93701cf87236bd48c15
Charles Coulombe <charles.coulombe@calculquebec.ca>
2020-05-20 11:56:19 -0400

    Blis with arch specific instructions

--- config/blis.sh
+++ config/blis.sh
@@ -1,2 +1,5 @@
 PYTHON_DEPS="numpy cython pytest hypothesis wheel"
+#PRE_BUILD_COMMANDS="export BLIS_ARCH='generic'"
+PRE_BUILD_COMMANDS="export BLIS_ARCH='haswell'"
+#PRE_BUILD_COMMANDS="export BLIS_ARCH='skx'"
 
