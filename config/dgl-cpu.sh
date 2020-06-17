7c1468ac22fb97b752d9e9f11ab1301838114863
Charles Coulombe <charles.coulombe@calculquebec.ca>
2020-06-17 11:43:58 -0400

    Adjusted `dgl-[cg]pu` according to use correct tag names. Updated to cuda 10.1

--- config/dgl-cpu.sh
+++ config/dgl-cpu.sh
@@ -1,14 +1,13 @@
 # The v0.2 is CPU only, GPU is in HEAD of the repo or the next release
 PACKAGE="dgl"
 PACKAGE_SUFFIX="-cpu"
-PACKAGE_DOWNLOAD_NAME=$PACKAGE
+PACKAGE_DOWNLOAD_NAME="$PACKAGE-$VERSION.tar.gz"
 PYTHON_IMPORT_NAME=$PACKAGE
 PACKAGE_FOLDER_NAME=$PACKAGE
 MODULE_BUILD_DEPS="cmake"
-PYTHON_DEPS="cython numpy scipy networkx torch"
+PYTHON_DEPS="cython numpy scipy networkx torch==1.4"
 PACKAGE_DOWNLOAD_METHOD="Git"
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/dmlc/dgl"
-PACKAGE_DOWNLOAD_CMD="git clone --recursive $PACKAGE_DOWNLOAD_ARGUMENT --branch v$VERSION $PACKAGE_FOLDER_NAME"
-POST_DOWNLOAD_COMMANDS="tar -zcf ${PACKAGE}-${VERSION}.tar.gz $PACKAGE_FOLDER_NAME"
+PACKAGE_DOWNLOAD_CMD="git clone --recursive $PACKAGE_DOWNLOAD_ARGUMENT --branch $VERSION $PACKAGE_FOLDER_NAME"
+POST_DOWNLOAD_COMMANDS="tar -zcf $PACKAGE_DOWNLOAD_NAME $PACKAGE_FOLDER_NAME"
 PRE_BUILD_COMMANDS="mkdir build && cd build && cmake .. && make -j 16 && cd ../python "
-
