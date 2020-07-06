91f49956c1e2e4a4ee4eada365401d485c4e7b86
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2020-07-06 15:59:20 +0000

    added configuration for phyluce and viewclust

---
+++ config/phyluce.sh
@@ -0,0 +1,6 @@
+
+#PYTHON_DEPS="numpy dipy scipy nibabel h5py six"
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/faircloth-lab/phyluce/archive/v${VERSION}.tar.gz"
+PACKAGE_DOWNLOAD_NAME="${VERSION}.tar.gz"
+#PACKAGE_FOLDER_NAME="AMICO-master"
+
