9406fbcec0c63038d3f6770f5b059b26a2436b2c
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2020-08-21 20:06:04 +0000

    Revert "added a test with nix and gentoo ldd to see if some libraries are missing"
    This reverts commit 6f45bd999095e59a564a5024e01f4e55f19ef8da.

--- wheel_architecture.sh
+++ wheel_architecture.sh
@@ -23,11 +23,8 @@ function version_lte {
 
 NIX_GLIBC="/cvmfs/soft.computecanada.ca/nix/var/nix/profiles/16.09/lib/libc.so.6"
 GENTOO_GLIBC="/cvmfs/soft.computecanada.ca/gentoo/2020/lib64/libc.so.6"
-NIX_LDD="/cvmfs/soft.computecanada.ca/nix/var/nix/profiles/16.09/bin/ldd"
-GENTOO_LDD="/cvmfs/soft.computecanada.ca/gentoo/2020/usr/bin/ldd"
 NIX_GLIBC_VERSION=$(strings "$NIX_GLIBC" | grep "^GLIBC_" | cut -d'_' -f2 | sort -V | grep "^[0-9]" | tail -1)
 GENTOO_GLIBC_VERSION=$(strings "$GENTOO_GLIBC" | grep "^GLIBC_" | cut -d'_' -f2 | sort -V | grep "^[0-9]" | tail -1)
-
 
 for fname in $(find . -type f); do
 	filetype=$(file -b $fname)
@@ -46,51 +43,26 @@ for fname in $(find . -type f); do
 	else
 		continue
 	fi
-	
-	WORKS_ON_NIX=1
-	WORKS_ON_GENTOO=1
 
 	# remove rpath from python itself from consideration
 	rpath=$(echo $rpath | sed -e "s;[^:]*/Core/python[^:]*;;g")
+	
 	if [[ $rpath =~ 'nix' || $rpath =~ 'easybuild/software/2017' ]]; then
-		WORKS_ON_GENTOO=0
 		COMPATIBILITY_LAYER='nix'
-		echo $fname has rpath in nix rpath=$rpath  >&2
+		echo $fname is $COMPATIBILITY_LAYER $ARCHITECTURE, rpath=$rpath  >&2
 	elif [[ $rpath =~ 'gentoo' || $rpath =~ 'easybuild/software/2020' || $rpath =~ 'easybuild/software/2019' ]]; then
-		WORKS_ON_NIX=0
 		COMPATIBILITY_LAYER='gentoo'
-		echo $fname has rpath in gentoo rpath=$rpath  >&2
+		echo $fname is $COMPATIBILITY_LAYER $ARCHITECTURE, rpath=$rpath  >&2
 	fi
 	if [[ "$(version_lte $min_required_glibc $NIX_GLIBC_VERSION)" == "no" ]]; then
-		WORKS_ON_NIX=0
+		COMPATIBILITY_LAYER='gentoo'
 		echo $fname requires a glibc more recent than that provided by Nix: $min_required_glibc ">" $NIX_GLIBC_VERSION >&2
 	fi
 	if [[ "$(version_lte $min_required_glibc $GENTOO_GLIBC_VERSION)" == "no" ]]; then
-		WORKS_ON_GENTOO=0
+		COMPATIBILITY_LAYER='unknown'
 		echo $fname requires a glibc more recent than that provided by Gentoo: $min_required_glibc ">" $GENTOO_GLIBC_VERSION >&2
 	fi
-
-	$NIX_LDD "$fname" | grep "not found" >/dev/null
-	NIX_HAVE_LIBS=$?
-	$GENTOO_LDD "$fname" | grep "not found" >/dev/null
-	GENTOO_HAVE_LIBS=$?
-	if [[ $NIX_HAVE_LIBS -eq 0 ]]; then
-		WORKS_ON_NIX=0
-		echo $fname is missing some libraries in Nix >&2
-	fi
-	if [[ $GENTO_HAVE_LIBS -eq 0 ]]; then
-		WORKS_ON_GENTOO=0
-		echo $fname is missing some libraries in Gentoo >&2
-	fi
-	if [[ $WORKS_ON_GENTOO -eq 1 && $WORKS_ON_NIX -eq 1 ]]; then
-		COMPATIBILITY_LAYER="generic"
-	elif [[ $WORKS_ON_GENTOO -eq 1 ]]; then
-		COMPATIBILITY_LAYER="gentoo"
-	elif [[ $WORKS_ON_NIX -eq 1 ]]; then
-		COMPATIBILITY_LAYER="nix"
-	else
-		COMPATIBILITY_LAYER="unknown"
-	fi
+	
 
 	if [[ $rpath =~ '/sse3/' && $ARCHITECTURE == "generic" ]]; then
 		ARCHITECTURE='sse3'
