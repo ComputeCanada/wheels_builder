c3fb8cdf716d441953433b6e42ed106cd3fae262
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2020-09-09 21:24:19 +0000

    fixed llvmlite module version fo version 0.32.1

--- config/llvmlite.sh
+++ config/llvmlite.sh
@@ -5,6 +5,9 @@ if [[ -z "$EBROOTGENTOO" ]]; then
 else
   #PYTHON_DEPS="enum34"
   MODULE_BUILD_DEPS="llvm cuda/11.0 tbb"
+  if [[ "$VERSION" == "0.32.1" ]]; then
+	  MODULE_BUILD_DEPS="llvm/8 cuda/11.0 tbb"
+  fi
 fi
 PATCHES="llvmlite-0.28.0-fpic.patch"
 
