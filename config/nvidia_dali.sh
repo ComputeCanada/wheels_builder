6adaa0724b2342a2dca220f634bc38b6fc092e35
Charles Coulombe <charles.coulombe@calculquebec.ca>
2020-10-20 19:33:47 -0400

    Update DALI config for latest and 2020

--- config/nvidia_dali.sh
+++ config/nvidia_dali.sh
@@ -1,4 +1,4 @@
-MODULE_BUILD_DEPS="gcc/7.3.0 cmake/3.16.3 cuda/10.2 opencv/4.2.0 protobuf/3.11.1 boost/1.68.0"
+MODULE_BUILD_DEPS="gcc/9.3.0 cuda/11.0 opencv/4.4.0 protobuf/3.12 boost/1.72.0"
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/NVIDIA/DALI"
 PACKAGE_DOWNLOAD_NAME="$PACKAGE-$VERSION.tar.gz"
 PACKAGE_DOWNLOAD_METHOD="Git"
@@ -15,13 +15,13 @@ PRE_BUILD_COMMANDS=$(cat <<-END
 	cd .. &&
 	mkdir build &&
 	cd build &&
-	cmake -DCMAKE_SKIP_RPATH=ON -DBUILD_NVJPEG=ON -DCMAKE_PREFIX_PATH=\$EBROOTPROTOBUF -DProtobuf_INCLUDE_DIRS=\$EBROOTPROTOBUF/include -DProtobuf_LIBRARIES=\$EBROOTPROTOBUF/lib -DProtobuf_LIBRARY=\$EBROOTPROTOBUF/lib/libprotobuf.so -DBUILD_TEST=OFF -DBUILD_BENCHMARK=OFF -DFFMPEG_ROOT_DIR=\$(realpath ../ffmpeg-4.2.1) -Davformat_INCLUDE_DIRS=\$(realpath ../ffmpeg-4.2.1/include) .. &&
+	cmake -DCMAKE_SKIP_RPATH=ON -DBUILD_NVJPEG=ON -DCMAKE_PREFIX_PATH=\$EBROOTPROTOBUF -DProtobuf_INCLUDE_DIRS=\$EBROOTPROTOBUF/include -DProtobuf_LIBRARIES=\$EBROOTPROTOBUF/lib64 -DProtobuf_LIBRARY=\$EBROOTPROTOBUF/lib64/libprotobuf.so -DBUILD_TEST=OFF -DBUILD_BENCHMARK=OFF -DFFMPEG_ROOT_DIR=\$(realpath ../ffmpeg-4.2.1) -Davformat_INCLUDE_DIRS=\$(realpath ../ffmpeg-4.2.1/include) .. &&
 	make -j 4 &&
 	cd dali/python &&
 	sed -i -e "s#name='nvidia-dali-cuda.*'#name='nvidia-dali'#" setup.py
 END
 ) # This must stay on a separate line!
 # The wheel is not universal
-POST_BUILD_COMMANDS="PYVER=\${EBVERSIONPYTHON::-2}; mv \$WHEEL_NAME \${WHEEL_NAME/py3-none-any/cp\${PYVER//.}-cp\${PYVER//.}m-linux_x86_64} && WHEEL_NAME=\$(ls *.whl)"
+POST_BUILD_COMMANDS="PYVER=\${EBVERSIONPYTHON::-2}; mv \$WHEEL_NAME \${WHEEL_NAME/py3-none-any/cp\${PYVER//.}-cp\${PYVER//.}\$(python3-config --abiflags)-linux_x86_64} && WHEEL_NAME=\$(ls *.whl)"
 # RPATH_TO_ADD="\$ORIGIN"  # Force origin. Equivalent to use `--add_origin`.
 RPATH_ADD_ORIGIN="yes" # any value is good
