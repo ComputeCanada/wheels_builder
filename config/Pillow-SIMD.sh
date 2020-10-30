cab08756c48952569b6cd9b58ba897b5d33fb5ae
Charles Coulombe <charles.coulombe@calculquebec.ca>
2020-10-29 21:37:12 -0400

    Updated pillow-simd recipe with hint for include and libs in Gentoo

--- config/Pillow-SIMD.sh
+++ config/Pillow-SIMD.sh
@@ -1,3 +1,6 @@
-PRE_BUILD_COMMANDS="export CC=\"cc -mavx2\""
+if [[ -z "$EBROOTGENTOO" ]]; then
+	PRE_BUILD_COMMANDS="export CC=\"cc -mavx2\""
+else
+	PRE_BUILD_COMMANDS="export CC=\"cc -mavx2\"; export CFLAGS=\"-I/cvmfs/soft.computecanada.ca/gentoo/2020/usr/include -L/cvmfs/soft.computecanada.ca/gentoo/2020/usr/lib64 \""
+fi
 PYTHON_IMPORT_NAME="PIL"
-
