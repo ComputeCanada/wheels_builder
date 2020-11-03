2904690dc5a57a7e5a1a0166bba8ee7d3528b5ed
Charles Coulombe <charles.coulombe@calculquebec.ca>
2020-11-03 12:24:48 -0500

    Updated torchvision with latest cuda

--- config/torchvision.sh
+++ config/torchvision.sh
@@ -1,5 +1,5 @@
 PYTHON_DEPS="numpy six pillow-simd torch"
-MODULE_BUILD_DEPS="cuda/10.1"
+MODULE_BUILD_DEPS="cuda/11"
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/pytorch/vision/archive/v${VERSION}.tar.gz"
 PACKAGE_DOWNLOAD_NAME="v${VERSION}.tar.gz"
 PACKAGE_FOLDER_NAME="vision-${VERSION}"
