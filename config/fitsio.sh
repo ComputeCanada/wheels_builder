3085d5958caa7da4d1f3ea814097df9c9cbd9809
Oliver Stueker <oliver.stueker@ace-net.ca>
2020-11-23 07:04:59 -0330

    configs for fitsio and psfex

---
+++ config/fitsio.sh
@@ -0,0 +1,6 @@
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/esheldon/fitsio/archive/v${VERSION}.tar.gz"
+PACKAGE_DOWNLOAD_NAME="v${VERSION}.tar.gz"
+PACKAGE_FOLDER_NAME="fitsio-${VERSION}"
+#MODULE_BUILD_DEPS=""
+PYTHON_DEPS="numpy>=1.11"
+
---
+++ config/psfex.sh
@@ -0,0 +1,6 @@
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/esheldon/psfex/archive/v${VERSION}.tar.gz"
+PACKAGE_DOWNLOAD_NAME="v${VERSION}.tar.gz"
+PACKAGE_FOLDER_NAME="psfex-${VERSION}"
+#MODULE_BUILD_DEPS=""
+PYTHON_DEPS="numpy fitsio"
+
