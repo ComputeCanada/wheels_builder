d2eb6e21113a42c7f97cb68bef7be0848034b7f9
Charles Coulombe <charles.coulombe@calculquebec.ca>
2021-03-23 15:05:13 -0400

    Fixed Pillow config to find zlib correctly.

--- config/Pillow.sh
+++ config/Pillow.sh
@@ -3,5 +3,5 @@ PYTHON_IMPORT_NAME="PIL"
 if [[ -z $EBROOTGENTOO ]]; then
 	PRE_BUILD_COMMANDS="sed -i -e 's;/sbin/ldconfig;ldconfig;g' setup.py && CPATH=$NIXUSER_PROFILE/include:$CPATH "
 else
-	PRE_BUILD_COMMANDS="sed -i -e 's;/sbin/ldconfig;ldconfig;g' setup.py && CPATH=$EBROOTGENTOO/include:$CPATH "
+	PRE_BUILD_COMMANDS="sed -i -e 's;/sbin/ldconfig;ldconfig;g' setup.py && CPATH=$EBROOTGENTOO/include:$CPATH LIBRARY_PATH=$EBROOTGENTOO/lib:$LIBRARY_PATH "
 fi
