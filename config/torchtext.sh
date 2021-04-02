5e402a2e53efdfcc95ec6e0b37ac7c447d0ff161
Charles Coulombe <charles.coulombe@calculquebec.ca>
2021-04-02 12:44:17 -0400

    Updated config for torchtext. Now builds from Github since sources are not available on pypi

--- config/torchtext.sh
+++ config/torchtext.sh
@@ -1,8 +1,3 @@
-# torch_cpu, six and numpy are only for testing purposes. They are not in torchtext requirements.
-# torchtext should be installed along with : numpy, six, torch-[cg]pu
-PYTHON_DEPS="certifi urllib3 chardet idna requests tqdm six numpy torch_cpu"
-
-# Remove torch requirements as the user need to either install torch-[cg]pu wheel and not PyPI torch.
-# Remove egg-info artefact as we are rebuilding a wheel.
-PRE_BUILD_COMMANDS="sed -i -e \"s/'torch',//g\" setup.py; rm -r torchtext.egg-info;"
-
+PYTHON_DEPS="torch"
+PRE_BUILD_COMMANDS="export BUILD_VERSION=\$VERSION"
+PACKAGE_DOWNLOAD_ARGUMENT="git+https://github.com/pytorch/text.git@v$VERSION-rc1"
