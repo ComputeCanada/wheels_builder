b86cfb701697c45e8f5301ccbdfc9629c1018e8e
Charles Coulombe <charles.coulombe@calculquebec.ca>
2021-04-02 12:43:12 -0400

    Updated config for preshed and latest spacy

--- config/preshed.sh
+++ config/preshed.sh
@@ -1,2 +1,2 @@
-PYTHON_DEPS="cymem>=1.30,<1.32.0"
+PYTHON_DEPS="cymem>=2.0.2,<2.1.0 murmurhash>=0.28.0,<1.1.0"
 
--- config/spacy.sh
+++ config/spacy.sh
@@ -1,2 +1,1 @@
-PYTHON_DEPS="cymem>=2.0.2,<2.1.0 preshed>=2.0.1,<2.1.0 thinc>=7.0.2,<7.1.0 blis>=0.2.2,<0.3.0 murmurhash>=0.28.0,<1.1.0 wasabi>=0.2.0,<1.1.0 srsly>=0.0.5,<1.1.0 numpy>=1.15.0 requests>=2.13.0,<3.0.0 jsonschema>=2.6.0,<3.1.0 plac<1.0.0,>=0.9.6 pathlib==1.0.1;python_version<'3.4' cython>=0.25 pytest>=4.0.0,<4.1.0 pytest-timeout>=1.3.0,<2.0.0 mock>=2.0.0,<3.0.0 flake8>=3.5.0,<3.6.0"
-
+PYTHON_DEPS="spacy-legacy>=3.0.0,<3.1.0 cymem>=2.0.2,<2.1.0 preshed>=3.0.2,<3.1.0 thinc>=8.0.2,<8.1.0 blis>=0.4.0,<0.8.0 ml_datasets>=0.2.0,<0.3.0 murmurhash>=0.28.0,<1.1.0 wasabi>=0.8.1,<1.1.0 srsly>=2.4.0,<3.0.0 catalogue>=2.0.1,<2.1.0 typer>=0.3.0,<0.4.0 pathy>=0.3.5 numpy>=1.15.0 pydantic>=1.7.1,<1.8.0"
