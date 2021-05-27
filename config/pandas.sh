7488d9a8be2775f9308363343b4434ade5ee1dab
Pawel Pomorski <ppomorsk@sharcnet.ca>
2021-05-27 18:06:40 +0000

    Roll back setting specific numpy version pending further discussion

--- config/pandas.sh
+++ config/pandas.sh
@@ -1,2 +1,2 @@
-PYTHON_DEPS="numpy==1.19.2 python-dateutil pytz Cython numexpr bottleneck scipy tables matplotlib nose pytest moto"
+PYTHON_DEPS="numpy python-dateutil pytz Cython numexpr bottleneck scipy tables matplotlib nose pytest moto"
 
