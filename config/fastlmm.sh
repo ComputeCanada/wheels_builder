feb83c93191dd4bec905a9723e76506af384715b
jshleap <jshleap@sharcnet.ca>
2021-06-02 21:40:36 +0000

    added config for fastlmm

---
+++ config/fastlmm.sh
@@ -0,0 +1,5 @@
+MODULE_RUNTIME_DEPS="scipy-stack rust hdf5"
+PYTHON_DEPS="pybind11 maturin bed_reader"
+BDIST_WHEEL_ARGS=" --no-build-isolation"
+#PACKAGE_DOWNLOAD_CMD="git clone https://github.com/fastlmm/FaST-LMM.git"
+#POST_DOWNLOAD_COMMANDS='cd FaST-LMM'
