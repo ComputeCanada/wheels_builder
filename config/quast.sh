8785a4ece873b3ff22658d963e8b77fb27bd54a7
jshleap <jshleap@sharcnet.ca>
2021-06-29 14:10:38 +0000

    removing debian only options

--- config/quast.sh
+++ config/quast.sh
@@ -1,2 +1,2 @@
-MODULE_RUNTIME_DEPS="rust java boost perl"
-PYTHON_DEPS="simplejson setuptools-rust joblib path"
+MODULE_RUNTIME_DEPS="gcc rust java boost perl"
+PYTHON_DEPS="simplejson setuptools-rust joblib path matplotlib"
