bf92789fba722210a30b2639213acc0e091ea84b
Charles Coulombe <charles.coulombe@calculquebec.ca>
2021-07-07 07:41:36 -0400

    Updated scipy config to latest

--- config/scipy.sh
+++ config/scipy.sh
@@ -1,4 +1,3 @@
-MODULE_BUILD_DEPS="imkl/2019.3.199"
-PYTHON_DEPS="nose numpy pytest"
-PYTHON_DEPS_DEFAULT=""
+MODULE_BUILD_DEPS="imkl/2020.1.217"
+PYTHON_DEPS="numpy pytest pybind11 cython pythran"
 PYTHON_TESTS="scipy.__config__.show(); scipy.test()"
