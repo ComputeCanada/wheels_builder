6f786876442607934962f2b778c0cdff3d09a66d
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2021-07-14 14:46:21 +0000

    first draft of patch_wheel.sh to patch a wheel file's metadata

---
+++ patch_wheel.sh
@@ -0,0 +1,52 @@
+#!/bin/env bash
+
+THIS_SCRIPT=$0
+
+function print_usage
+{
+	echo "Usage: $0 [--wheel <wheel file>]"
+}
+
+TEMP=$(getopt -o h --longoptions help,wheel:,cctag --name $0 -- "$@")
+if [ $? != 0 ] ; then print_usage; exit 1 ; fi
+eval set -- "$TEMP"
+
+while true; do
+	case "$1" in
+		--wheel)
+			ARG_WHEEL=$2; shift 2;;
+		--cctag)
+			ARG_CCTAG=1; shift ;;
+		-h|--help)
+			print_usage; exit 0 ;;
+		--)
+			shift; break ;;
+		*) echo "Unknown parameter $1"; print_usage; exit 1 ;;
+	esac
+done
+
+fullname=$(readlink -f $ARG_WHEEL)
+newname=$fullname
+tmp=$(mktemp --directory)
+pushd $tmp
+unzip -q $fullname
+
+if [[ $ARG_CCTAG -eq 1 ]]; then
+	tag="+computecanada"
+	newname=$(echo $fullname | sed -e "s/\([^-]*\)-\([^-]*\)-\(.*\)\.whl/\1-\2$tag-\3.whl/g")
+
+	# rename the dist-info folder
+	distinfo=*dist-info
+	newdistinfo=$(echo $distinfo | sed -e "s/\([^-]*\)-\([^-]*\)\(.*\).dist-info/\1-\2$tag\3.dist-info/g")
+	mv $distinfo $newdistinfo
+
+	# change the version in the METADATA file
+	sed -i -e "s/^Version:\(.*\)\([^ ]*\)\(.*\)/Version:\1\2$tag\3/g" $newdistinfo/METADATA
+
+fi
+zip -rq $newname .
+
+
+popd
+rm -rf $tmp
+
