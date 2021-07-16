4a5f4d94ce5fffe2eebdf9a75846085f3e906dfb
jshleap <jshleap@sharcnet.ca>
2021-07-16 16:27:22 +0000

    fixing cartopy's config name

--- config/Cartopy.sh
+++ config/Cartopy.sh
@@ -1,8 +1,2 @@
-if [[ "$RSNT_ARCH" == "avx" ]]; then
-	MODULE_BUILD_DEPS="gcc/5.4.0 proj/4.9.3 geos gdal"
-else
-	MODULE_BUILD_DEPS="proj/4.9.3 geos gdal"
-fi
-PYTHON_DEPS="Cython numpy shapely pyshp six Pillow pyepsg pykdtree scipy OWSLib"
-PRE_BUILD_COMMANDS="pip freeze"
-
+MODULE_RUNTIME_DEPS="gcc geos proj/7.0.1 gdal"
+PYTHON_DEPS='pyshp Shapely matplotlib Fiona OWSLib Pillow pyepsg pykdtree'
