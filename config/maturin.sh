3a0e4337d0d6f78ca00e50ec1e05c4bd887e528c
Charles Coulombe <charles.coulombe@calculquebec.ca>
2021-07-28 19:43:17 -0400

    Updated config for maturin with rust 1.53.0. Removed unused deps.

--- config/maturin.sh
+++ config/maturin.sh
@@ -1,3 +1,3 @@
-MODULE_RUNTIME_DEPS="StdEnv/2020 gcc/9.3.0 scipy-stack rust/1.47.0"
-MODULE_BUILD_DEPS="StdEnv/2020 gcc/9.3.0 scipy-stack rust/1.47.0"
+MODULE_RUNTIME_DEPS="rust/1.53.0"
+MODULE_BUILD_DEPS="rust/1.53.0"
 PYTHON_DEPS='toml'
