b46aeb6351c3cddec071bc6bb788f4e40fb8131e
Olivier Fisette <olivier.fisette@usask.ca>
2021-08-27 15:11:54 +0000

    Fix missing version in setup.py

--- config/Cartopy-0.19.0.post1.sh
+++ config/Cartopy-0.19.0.post1.sh
@@ -1,2 +1,3 @@
 PYTHON_VERSIONS="python/3.7 python/3.8 python/3.9"
 MODULE_RUNTIME_DEPS="gcc/9.3.0 geos/3.9.1 proj/7.2.1 gdal/3.2.3 scipy-stack/2021a"
+PRE_BUILD_COMMANDS="sed -i \"s/name='Cartopy'/name='Cartopy', version='${VERSION}'/\" setup.py"
