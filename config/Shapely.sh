546cb82f95c064a5e84476d994c2d2b19fc81e8f
Olivier Fisette <olivier.fisette@usask.ca>
2021-09-21 15:59:07 +0000

    Update build dependencies

--- config/Shapely.sh
+++ config/Shapely.sh
@@ -1,7 +1,3 @@
-if [[ "$RSNT_ARCH" == "avx" ]]; then
-	MODULE_BUILD_DEPS="gcc/5.4.0 geos"
-else
-	MODULE_BUILD_DEPS="gcc geos"
-fi
+MODULE_BUILD_DEPS="gcc geos"
 # need to patch geos.py to find libgeos_c.so based on the module that was loaded at build time
 PRE_BUILD_COMMANDS='sed -i -e "s;path.join([^)]*libgeos_c.so[^)]*);path.join(os.getenv(\"EBROOTGEOS\"), \"lib\", \"libgeos_c.so\");g" -e "s/CONDA_PREFIX/EBROOTGEOS/g" $(find . -name "geos.py")'
