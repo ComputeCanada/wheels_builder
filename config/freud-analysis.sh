ed8c2381f0302cff9f460c6a0478969a7030e847
Oliver Stueker <oliver.stueker@ace-net.ca>
2021-11-23 14:34:10 -0330

    add config for freud-analysis

---
+++ config/freud-analysis.sh
@@ -0,0 +1,4 @@
+# https://freud.readthedocs.io/en/stable/gettingstarted/installation.html#compile-from-source
+MODULE_BUILD_DEPS="tbb"
+PYTHON_DEPS="numpy cython scikit-build"
+PYTHON_IMPORT_NAME="freud"
