0f388f98edaec0c9619f05cb7f2cd8c6902c8538
Bart Oldeman <bart.oldeman@calculquebec.ca>
2021-12-14 18:55:34 +0000

    flexiblascore/.3.0.4 was renamed to flexiblas/3.0.4

--- config/numpy.sh
+++ config/numpy.sh
@@ -1,8 +1,8 @@
-PRE_BUILD_COMMANDS='module load flexiblascore/.3.0.4; echo "[DEFAULT]" > site.cfg; echo "library_dirs = $EBROOTFLEXIBLASCORE/lib" >> site.cfg; echo "include_dirs = $EBROOTFLEXIBLASCORE/include/flexiblas" >> site.cfg; echo "[atlas]" >> site.cfg; echo "atlas_libs = flexiblas" >> site.cfg; echo "[lapack]" >> site.cfg; echo "lapack_libs = flexiblas" >> site.cfg'
+PRE_BUILD_COMMANDS='module load flexiblas/3.0.4; echo "[DEFAULT]" > site.cfg; echo "library_dirs = $EBROOTFLEXIBLAS/lib" >> site.cfg; echo "include_dirs = $EBROOTFLEXIBLAS/include/flexiblas" >> site.cfg; echo "[atlas]" >> site.cfg; echo "atlas_libs = flexiblas" >> site.cfg; echo "[lapack]" >> site.cfg; echo "lapack_libs = flexiblas" >> site.cfg'
 if [[ -z $EBROOTGENTOO ]]; then
 	MODULE_BUILD_DEPS="imkl/2019.2.187"
 else
-	MODULE_BUILD_DEPS="flexiblascore/.3.0.4"
+	MODULE_BUILD_DEPS="flexiblas/3.0.4"
 fi
 PYTHON_DEPS="nose pytest cython hypothesis"
 PYTHON_DEPS_DEFAULT=""
