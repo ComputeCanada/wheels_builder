20b1c350e4db721cffe6f16904f75fe8c5ec7465
Charles Coulombe <charles.coulombe@calculquebec.ca>
2021-12-15 14:39:49 -0500

    Updated medaka for v1.5.0 and tensorflow

--- config/medaka.sh
+++ config/medaka.sh
@@ -1,3 +1,3 @@
 PYTHON_DEPS="cffi"
 MODULE_RUNTIME_DEPS="samtools minimap2"
-PRE_BUILD_COMMANDS="sed -i 's/tensorflow~=2.2.2/tensorflow_cpu==2.2.*/' requirements.txt && export LDFLAGS=-ldeflate"
+PRE_BUILD_COMMANDS="sed -i 's/tensorflow~=2.5.2/tensorflow==2.5.*/' requirements.txt && export LDFLAGS=-ldeflate"
