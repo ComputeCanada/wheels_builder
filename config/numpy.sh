964e63ce9a60be89155ef0f8d7004026ff7f01e8
Bart Oldeman <bart.oldeman@calculquebec.ca>
2021-12-15 18:57:27 +0000

    Add py=1.10.0 to numpy download deps to avoid issues with 1.11.0
    1.11.0 can't be downloaded with --no-use-pep571 and has issues
    with manipulate_wheel as well so we'll defer that to 2022.

--- config/numpy.sh
+++ config/numpy.sh
@@ -1,6 +1,6 @@
 PRE_BUILD_COMMANDS='module load flexiblas/3.0.4; echo "[DEFAULT]" > site.cfg; echo "library_dirs = $EBROOTFLEXIBLAS/lib" >> site.cfg; echo "include_dirs = $EBROOTFLEXIBLAS/include/flexiblas" >> site.cfg; echo "[atlas]" >> site.cfg; echo "atlas_libs = flexiblas" >> site.cfg; echo "[lapack]" >> site.cfg; echo "lapack_libs = flexiblas" >> site.cfg'
 MODULE_BUILD_DEPS="arch/sse3 flexiblas/3.0.4"
-PYTHON_DEPS="nose pytest cython hypothesis"
+PYTHON_DEPS="nose pytest cython hypothesis py=1.10.0"
 PYTHON_DEPS_DEFAULT=""
 PYTHON_TESTS="numpy.__config__.show(); numpy.test()"
 
