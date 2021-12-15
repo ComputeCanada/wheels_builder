16c242065bc601f925884d27fe785c482c92fb92
Charles Coulombe <charles.coulombe@calculquebec.ca>
2021-12-15 14:05:08 -0500

    Medaka 1.4.4 links to libdeflate with htslib.a bundled

--- config/medaka.sh
+++ config/medaka.sh
@@ -1,3 +1,3 @@
 PYTHON_DEPS="cffi"
 MODULE_RUNTIME_DEPS="samtools minimap2"
-PRE_BUILD_COMMANDS="sed -i 's/tensorflow~=2.2.2/tensorflow_cpu==2.2.*/' requirements.txt"
+PRE_BUILD_COMMANDS="sed -i 's/tensorflow~=2.2.2/tensorflow_cpu==2.2.*/' requirements.txt && export LDFLAGS=-ldeflate"
