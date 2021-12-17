391cccb87dffb70dbdde476b5758e43e91e30a1a
Olivier Fisette <olivier.fisette@usask.ca>
2021-12-17 21:13:17 +0000

    Fiona wheel: update cfitsio dependency to recent version

--- config/Fiona.sh
+++ config/Fiona.sh
@@ -1,2 +1,2 @@
-MODULE_RUNTIME_DEPS="gcc gdal cfitsio/3.41 postgresql"
+MODULE_RUNTIME_DEPS="gcc gdal cfitsio/3.48 postgresql"
 POST_BUILD_COMMANDS='setrpaths.sh --path $WHEEL_NAME --add_path $EBROOTCFITSIO/lib:$EBROOTPOSTGRESQL/lib --any_interpreter '
