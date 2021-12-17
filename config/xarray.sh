637f583c7ef55fb05175ef84847217eb8bb0bc0b
Olivier Fisette <olivier.fisette@usask.ca>
2021-12-17 21:12:32 +0000

    xarray wheel: add module dependencies

--- config/xarray.sh
+++ config/xarray.sh
@@ -1,1 +1,3 @@
-PYTHON_VERSIONS="python/3.6 python/3.7 python/3.8"
+PYTHON_VERSIONS="python/3.7 python/3.8 python/3.9"
+MODULE_RUNTIME_DEPS="gcc/9.3.0 eccodes/2.21.0 netcdf/4.7.4"
+PYTHON_DEPS="pandas zarr"
