82c809b6ec1ddc5712b9006b1021aa4387951be6
Oliver Stueker <oliver.stueker@ace-net.ca>
2022-01-31 17:08:08 -0330

    config for mbuild

--- config/mbuild.sh
+++ config/mbuild.sh
@@ -1,3 +1,9 @@
-MODULE_RUNTIME_DEPS="intel/2018.3 openmpi/3.1.2 packmol/18.013"
-PYTHON_DEPS="oset ParmEd mdtraj pandas"
-PATCHES="mbuild-0.10.5.patch"
+if [[ -n $EBROOTGENTOO ]]; then
+    MODULE_RUNTIME_DEPS="packmol"
+else
+    MODULE_RUNTIME_DEPS="intel/2018.3 openmpi/3.1.2 packmol/18.013"
+fi
+PYTHON_DEPS="ele pandas oset ParmEd mdtraj"
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/mosdef-hub/mbuild/archive/refs/tags/${VERSION}.tar.gz"
+#UPDATE_REQUIREMENTS="numpy scipy ele pandas oset ParmEd mdtraj"
+PATCHES="mbuild.patch"
