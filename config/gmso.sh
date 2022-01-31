426cc4d73d9f4a60ed4d5ef74b3fb968b52af272
Oliver Stueker <oliver.stueker@ace-net.ca>
2022-01-31 17:50:22 -0330

    config for GMSO

---
+++ config/gmso.sh
@@ -0,0 +1,8 @@
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/mosdef-hub/gmso/archive/refs/tags/${VERSION}.tar.gz"
+PYTHON_DEPS="numpy sympy unyt boltons lxml pydantic<1.9.0 networkx ele"
+PATCHES="gmso.patch"
+if [[ "$VERSION" == "0.7.3" ]]; then
+  # they forgot to change this flag, causing .dev0 to be added to the version
+  PRE_BUILD_COMMANDS="sed -i 's/ISRELEASED = False/ISRELEASED = True/' setup.py"
+fi
+
