734c3ec846b58ad8b09cc7021c58f0a40b06505d
Oliver Stueker <oliver.stueker@ace-net.ca>
2022-02-02 13:36:40 -0330

    pydantic: remove assumtions abour python version for new releases

--- config/pydantic.sh
+++ config/pydantic.sh
@@ -1,1 +1,4 @@
-PYTHON_VERSIONS="python/3.6 python/3.7 python/3.8"
+if [[ "$VERSION" == "1.2" ]]; then
+  # newer versions should be compatible with python/3.9 and newer
+  PYTHON_VERSIONS="python/3.6 python/3.7 python/3.8"
+fi
