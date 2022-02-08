36d98f43cda931b42e520d52009de6f713f816ba
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2022-02-08 17:06:33 +0000

    correcting import name for cryptography

--- config/cryptography.sh
+++ config/cryptography.sh
@@ -1,3 +1,3 @@
 PYTHON_DEPS="setuptools_rust cffi pycparser"
-PYTHON_IMPORT_NAME="import cryptography.hazmat.bindings._openssl"
+PYTHON_IMPORT_NAME="cryptography.hazmat.bindings._openssl"
 MODULE_BUILD_DEPS="rust/1.47.0"
