4ec675dd226e7a75744959fbfa406a579141a0e5
Charles Coulombe <charles.coulombe@calculquebec.ca>
2022-02-21 15:33:22 -0500

    Updated thinc python deps.

--- config/thinc.sh
+++ config/thinc.sh
@@ -1,2 +1,1 @@
-PYTHON_DEPS="murmurhash>=0.28.0,<1.1.0 cymem>=2.0.2,<2.1.0 preshed>=2.0.1,<2.1.0 blis>=0.2.1,<0.3.0 srsly>=0.0.4,<1.1.0 wasabi>=0.0.9,<1.1.0 numpy>=1.7.0 plac>=0.9.6,<1.0.0 tqdm>=4.10.0,<5.0.0 pathlib==1.0.1;python_version<'3.4' cython>=0.25.0 hypothesis>=2.0.0,<3.0.0 pytest>=3.6.0,<4.0.0 mock>=2.0.0,<3.0.0 flake8>=3.5.0,<3.6.0"
-
+PYTHON_DEPS="cython>=0.25,<3.0 murmurhash>=1.0.2,<1.1.0 cymem>=2.0.2,<2.1.0 preshed>=3.0.2,<3.1.0 blis>=0.4.0,<0.8.0 numpy>=1.15.0"
