2fd50f98457a828d4809af21068f5373fed123d8
Oliver Stueker <oliver.stueker@ace-net.ca>
2022-03-11 16:21:49 -0330

    update recipe for gmx_MMPBSA

--- config/gmx_MMPBSA.sh
+++ config/gmx_MMPBSA.sh
@@ -1,3 +1,5 @@
-MODULE_BUILD_DEPS="gcc/9.3.0 openmpi/4.0.3 ambertools/20 gromacs/2020.4 qt/5.12.8"
+# updated for gmx_MMPBSA 1.5.0.3 by Oliver
+MODULE_BUILD_DEPS="gcc/9.3.0 openmpi/4.0.3 ambertools/21 gromacs/2021.4 qt/5.15.2 mpi4py/3.1.2"
 PRE_BUILD_COMMANDS="source $EBROOTAMBERTOOLS/amber.sh"
+PYTHON_DEPS="ParmEd"
 TEST_COMMAND="gmx_MMPBSA -h"
