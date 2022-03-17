55de291ecfce2dbcc2763d70b6a4b22f9f59f45f
Charles Coulombe <charles.coulombe@calculquebec.ca>
2022-03-17 13:25:57 -0400

    Updated openslide config. Removed intel

--- config/openslide-python.sh
+++ config/openslide-python.sh
@@ -1,3 +1,3 @@
-MODULE_BUILD_DEPS="intel/2016.4 openslide"
+MODULE_BUILD_DEPS="openslide"
 PRE_BUILD_COMMANDS='sed -i -e "/import sys/a import os" -e "s;.libopenslide.so.0.;os.environ.get(\"EBROOTOPENSLIDE\",\"$EBROOTOPENSLIDE\") + \"/lib/libopenslide.so.0\";g" $(find . -name "lowlevel.py")'
 
