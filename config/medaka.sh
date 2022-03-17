b1e5b663bf759d9655b2199d5c537877971ec288
Charles Coulombe <charles.coulombe@calculquebec.ca>
2022-03-17 11:07:07 -0400

    Update medaka config to remove tensorflow fix

--- config/medaka.sh
+++ config/medaka.sh
@@ -1,3 +1,3 @@
 PYTHON_DEPS="cffi"
 MODULE_RUNTIME_DEPS="samtools minimap2"
-PRE_BUILD_COMMANDS="sed -i 's/tensorflow~=2.5.2/tensorflow==2.5.*/' requirements.txt && export LDFLAGS=-ldeflate"
+PRE_BUILD_COMMANDS="export LDFLAGS=-ldeflate"
