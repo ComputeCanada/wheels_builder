ff3a4e81295af7238811f2b2b71d69cd5a752772
Charles Coulombe <charles.coulombe@calculquebec.ca>
2022-03-23 09:05:07 -0400

    PyPrismatic hardcoded version is wrong. Fix it

--- config/PyPrismatic-cpu.sh
+++ config/PyPrismatic-cpu.sh
@@ -1,6 +1,7 @@
 MODULE_BUILD_DEPS="cmake hdf5 fftw boost"
+MODULE_RUNTIME_DEPS="hdf5"
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/prism-em/prismatic/archive/refs/tags/v$VERSION.tar.gz"
 PACKAGE="PyPrismatic"
 PACKAGE_SUFFIX="-cpu"
 PYTHON_IMPORT_NAME="pyprismatic.core"
-PYTHON_DEPS='h5py'
+PRE_BUILD_COMMANDS="sed -i -e 's/1.2.0/$VERSION/' setup.py"
--- config/PyPrismatic-gpu.sh
+++ config/PyPrismatic-gpu.sh
@@ -1,8 +1,9 @@
 MODULE_BUILD_DEPS="cmake cuda/11.4 hdf5 fftw boost"
+MODULE_RUNTIMED_DEPS="hdf5"
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/prism-em/prismatic/archive/refs/tags/v$VERSION.tar.gz"
 PACKAGE="PyPrismatic"
 PACKAGE_SUFFIX="-gpu"
 BDIST_WHEEL_ARGS="--enable-gpu"
 PYTHON_IMPORT_NAME="pyprismatic.core"
-PYTHON_DEPS='h5py'
 PATCHES="PyPrismatic-fix_multiple_arch.patch"
+PRE_BUILD_COMMANDS="sed -i -e 's/1.2.0/$VERSION/' setup.py"
