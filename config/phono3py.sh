ba2d567b218e233514b425cbdaca678cbfcad6ea
Charles Coulombe <charles.coulombe@calculquebec.ca>
2022-04-14 08:20:50 -0400

    Updated phono3py config to use git and flexiblas.

--- config/phono3py.sh
+++ config/phono3py.sh
@@ -1,6 +1,10 @@
-PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/phonopy/phono3py/archive/v$VERSION.tar.gz"
-MODULE_BUILD_DEPS="intel imkl hdf5"
-MODULE_RUNTIME_DEPS="hdf5"
-PATCHES="phono3py-1.22.0-mkl.patch"
-
-export CC="icc"
+# Forced to use git, as pip download try to build to evaluate but the setup.py requires that a human create
+# a site.cfg but the repo do not provide an empty one.
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/phonopy/phono3py -b v$VERSION"
+PACKAGE_DOWNLOAD_NAME="$PACKAGE-$VERSION.tar.gz"
+PACKAGE_DOWNLOAD_METHOD="Git"
+PACKAGE_DOWNLOAD_CMD="git clone --recursive $PACKAGE_DOWNLOAD_ARGUMENT --branch v$VERSION $PACKAGE_FOLDER_NAME"
+POST_DOWNLOAD_COMMANDS="tar -zcf ${PACKAGE}-${VERSION}.tar.gz $PACKAGE_FOLDER_NAME"
+MODULE_BUILD_DEPS="gcc imkl flexiblas hdf5"
+MODULE_RUNTIME_DEPS="gcc hdf5"
+PRE_BUILD_COMMANDS="sed -i 's/with_mkl = False/with_mkl = True/' setup.py; echo -e '[phono3py]\nextra_compile_args = -fopenmp' >> site.cfg"
