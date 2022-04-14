ece05835c87bd3c8cf6c7c80df49f4ce38aff897
Olivier Fisette <olivier.fisette@usask.ca>
2022-04-14 04:56:52 +0000

    Add config for Fiona 1.8.21 with updated OSGeo deps

---
+++ config/Fiona-1.8.21.sh
@@ -0,0 +1,2 @@
+MODULE_RUNTIME_DEPS="gcc/9.3.0 gdal/3.4.1 cfitsio/3.48 postgresql/12.4"
+POST_BUILD_COMMANDS='setrpaths.sh --path $WHEEL_NAME --add_path $EBROOTCFITSIO/lib:$EBROOTPOSTGRESQL/lib --any_interpreter '
