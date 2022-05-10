6a2f7030e9b5322234e622849221d33abca82315
Olivier Fisette <olivier.fisette@usask.ca>
2022-05-10 20:31:52 +0000

    msmbuilder: Update config to build for Gentoo

--- config/msmbuilder.sh
+++ config/msmbuilder.sh
@@ -1,2 +1,5 @@
-PYTHON_DEPS="numpy~=$NUMPY_DEFAULT_VERSION scipy scikit-learn mdtraj pandas cython<0.28 cvxopt nose"
-
+PYTHON_DEPS="numpy~=$NUMPY_DEFAULT_VERSION scipy scikit-learn mdtraj pandas cython<0.28 cvxopt nose tables"
+PACKAGE_DOWNLOAD_METHOD="Git"
+PACKAGE_DOWNLOAD_CMD="wget https://github.com/msmbuilder/msmbuilder/archive/refs/tags/3.8.0.tar.gz"
+PACKAGE_DOWNLOAD_NAME="3.8.0.tar.gz"
+PACKAGE_FOLDER_NAME="msmbuilder-3.8.0"
