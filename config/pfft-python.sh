ca815f81357ed3cd3c0a814b9e2f7736d4f0292c
Charles Coulombe <charles.coulombe@calculquebec.ca>
2022-05-18 11:42:35 -0400

    Updated mpi4py version (3.1.3) for mpsort,pmesh,pfft-python

--- config/pfft-python.sh
+++ config/pfft-python.sh
@@ -1,3 +1,3 @@
-MODULE_BUILD_DEPS="gcc openmpi/4.0.3 mpi4py/3.1.2"
-MODULE_RUNTIME_DEPS="gcc openmpi/4.0.3 mpi4py/3.1.2"
+MODULE_BUILD_DEPS="gcc openmpi/4.0.3 mpi4py/3.1.3"
+MODULE_RUNTIME_DEPS="gcc openmpi/4.0.3 mpi4py/3.1.3"
 PYTHON_IMPORT_NAME="pfft"
--- config/pmesh.sh
+++ config/pmesh.sh
@@ -1,2 +1,2 @@
-MODULE_BUILD_DEPS="gsl openmpi/4.0.3 mpi4py/3.1.2"
-MODULE_RUNTIME_DEPS="gsl openmpi/4.0.3 mpi4py/3.1.2"
+MODULE_BUILD_DEPS="gsl openmpi/4.0.3 mpi4py/3.1.3"
+MODULE_RUNTIME_DEPS="gsl openmpi/4.0.3 mpi4py/3.1.3"
