0bae1dcff15bcb5c55ff80872989d19a95bea508
Moise Rousseau <moise.rousseau@calculquebec.ca>
2022-06-15 14:55:57 +0000

    add CUDA_ARCH 6.0 and load cmake/3.22.1

--- config/qiskit-aer-gpu-0.9.1.sh
+++ config/qiskit-aer-gpu-0.9.1.sh
@@ -5,6 +5,7 @@ PACKAGE_SUFFIX="-gpu"
 PRE_BUILD_COMMANDS="export QISKIT_AER_PACKAGE_NAME=qiskit-aer-gpu"
 PACKAGE_DOWNLOAD_ARGUMENT="$PACKAGE==$VERSION"
 MODULE_RUNTIME_DEPS="
+    cmake/3.22.1
     symengine/0.8.1
     cuda/11.4
     flexiblas/3.0.4
@@ -15,7 +16,7 @@ PYTHON_DEPS="cmake"
 BDIST_WHEEL_ARGS="--
     -DBLA_VENDOR=FlexiBLAS
     -DAER_THRUST_BACKEND=CUDA
-    -DAER_CUDA_ARCH='7.0;7.5;8.0;8.6;8.6+PTX'
+    -DAER_CUDA_ARCH='6.0;7.0;7.5;8.0;8.6;8.6+PTX'
     -DAER_MPI=True
     -DDISABLE_CONAN=OFF
     -DSymEngine_DIR=$EBROOTSYMENGINE/lib/cmake/symengine/
