3b0149cc6818ab6029469eaf4a4bf79a2f8aac1f
Charles Coulombe <charles.coulombe@calculquebec.ca>
2022-06-30 11:40:54 -0400

    Updated scilpy requirements.

--- config/scilpy.sh
+++ config/scilpy.sh
@@ -1,1 +1,4 @@
 PACKAGE_DOWNLOAD_ARGUMENT="git+https://github.com/scilus/scilpy.git@$VERSION"
+MODULE_RUNTIME_DEPS="vtk"
+# Relax constraints as they are incompatible
+PRE_BUILD_COMMANDS="sed -i -e 's/[=>]=.*$//' requirements.txt"
