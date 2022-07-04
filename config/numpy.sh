14f60fbd7064027b1f49d40acf70623199c72312
Charles Coulombe <charles.coulombe@calculquebec.ca>
2022-07-04 12:57:07 -0400

    Updated Numpy config to let distutils.system_config know about blas. Updated site.cfg to use libraries as does upstream.

--- config/numpy.sh
+++ config/numpy.sh
@@ -1,6 +1,17 @@
-PRE_BUILD_COMMANDS='module load flexiblas/3.0.4; echo "[DEFAULT]" > site.cfg; echo "library_dirs = $EBROOTFLEXIBLAS/lib" >> site.cfg; echo "include_dirs = $EBROOTFLEXIBLAS/include/flexiblas" >> site.cfg; echo "[atlas]" >> site.cfg; echo "atlas_libs = flexiblas" >> site.cfg; echo "[lapack]" >> site.cfg; echo "lapack_libs = flexiblas" >> site.cfg'
+# One could also set NPY_BLAS_LIBS, NPY_LAPACK_LIBS and NPY_CBLAS_LIBS since numpy 1.21.
+PRE_BUILD_COMMANDS='module load flexiblas/3.0.4;
+echo "[DEFAULT]" > site.cfg;
+echo "library_dirs = $EBROOTFLEXIBLAS/lib" >> site.cfg;
+echo "include_dirs = $EBROOTFLEXIBLAS/include/flexiblas" >> site.cfg;
+echo "[atlas]" >> site.cfg;
+echo "libraries = flexiblas" >> site.cfg;
+echo "[blis]" >> site.cfg;
+echo "libraries = flexiblas" >> site.cfg;
+echo "[lapack]" >> site.cfg;
+echo "libraries = flexiblas" >> site.cfg;
+echo "[blas]" >> site.cfg;
+echo "libraries = flexiblas" >> site.cfg;'
 MODULE_BUILD_DEPS="arch/sse3 flexiblas/3.0.4"
 PYTHON_DEPS="nose pytest cython hypothesis py==1.10.0 importlib_metadata==4.8.1 typing_extensions==4.0.0"
 PYTHON_DEPS_DEFAULT=""
 PYTHON_TESTS="numpy.__config__.show(); numpy.test()"
-
