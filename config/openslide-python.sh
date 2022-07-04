277cece2ab09e67e4f155de489e4c783800b2dd7
Charles Coulombe <charles.coulombe@calculquebec.ca>
2022-07-04 09:58:41 -0400

    Update openslide-python config to match latest version (and backward compatible).

--- config/openslide-python.sh
+++ config/openslide-python.sh
@@ -1,3 +1,3 @@
 MODULE_BUILD_DEPS="openslide"
-PRE_BUILD_COMMANDS='sed -i -e "/import sys/a import os" -e "s;.libopenslide.so.0.;os.environ.get(\"EBROOTOPENSLIDE\",\"$EBROOTOPENSLIDE\") + \"/lib/libopenslide.so.0\";g" $(find . -name "lowlevel.py")'
-
+PRE_BUILD_COMMANDS='sed -i -e "/import platform/a import os" -e "s;.libopenslide.so.0.;os.environ.get(\"EBROOTOPENSLIDE\",\"$EBROOTOPENSLIDE\") + \"/lib/libopenslide.so.0\";g" $(find . -name "lowlevel.py")'
+PYTHON_IMPORT_NAME='openslide'
