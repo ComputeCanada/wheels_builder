cde1aed60f6cd93bb9c00e5e451766a7eb3b0fe6
Charles Coulombe <charles.coulombe@calculquebec.ca>
2022-07-04 17:12:05 -0400

    Cupy also uses cutensor

--- config/cupy.sh
+++ config/cupy.sh
@@ -1,5 +1,4 @@
-
 PYTHON_DEPS="numpy~=$NUMPY_DEFAULT_VERSION scipy optuna"
-MODULE_BUILD_DEPS="gcc/9.3.0 cuda/11.4 cudnn nccl"
+MODULE_BUILD_DEPS="gcc/9.3.0 cuda/11.4 cudnn nccl cutensor"
 # needed otherwise it does not find libcuda.so
 PRE_DOWNLOAD_COMMANDS='export LDFLAGS="-L$EBROOTCUDA/lib64/stubs -L$EBROOTNCCL/lib" ; export CFLAGS="-I$EBROOTNCCL/include/"'
