2b2e75ad16e6ab059433d8681c108449d9b61631
Charles Coulombe <charles.coulombe@calculquebec.ca>
2022-07-20 09:48:14 -0400

    Added jaxlib config. Refuses to build from source and defer to unmanylinuxize instead.

---
+++ config/jaxlib.sh
@@ -0,0 +1,13 @@
+# jaxlib is a pain to build
+
+MODULE_RUNTIME_DEPS="cuda/11.4 cudnn"
+
+if [[ $THIS_SCRIPT == 'build_wheel.sh' ]]; then
+        echo "Thanks Bazel..for nothing! Using the following to add jaxlib wheels:"
+        echo ""
+        echo "module load gcc ${MODULE_RUNTIME_DEPS}"
+        echo "bash unmanylinuxize.sh --package jaxlib --add_path \$CUDA_HOME/lib64:\$EBROOTCUDNN/lib --version ${VERSION:-X.Y.Z} --find_links=https://storage.googleapis.com/jax-releases/jax_releases.html"
+        module load gcc $MODULE_RUNTIME_DEPS
+        bash unmanylinuxize.sh --package jaxlib --add_path $CUDA_HOME/lib64:$EBROOTCUDNN/lib --version ${VERSION:?A version is required} --find_links=https://storage.googleapis.com/jax-releases/jax_releases.html
+        exit 1;
+fi
