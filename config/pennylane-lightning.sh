2d7321bb83b40a0b0eaf6952be4a4eaca2318a95
Charles Coulombe <charles.coulombe@calculquebec.ca>
2022-07-21 18:57:10 -0400

    Added PennyLane-Lightning config.

---
+++ config/pennylane-lightning.sh
@@ -0,0 +1,4 @@
+MODULE_BUILD_DEPS="arch/avx2 cmake flexiblas"
+PYTHON_DEPS="pybind11 ninja flaky pennylane>=0.15 pybind11 pytest pytest-cov pytest-mock"
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/PennyLaneAI/pennylane-lightning/archive/refs/tags/v${VERSION:?version required}.tar.gz"
+PRE_BUILD_COMMANDS="python setup.py build_ext --define='ENABLE_NATIVE=OFF;ENABLE_BLAS=ON;ENABLE_OPENMP=ON;BLA_VENDOR=FlexiBLAS;ENABLE_AVX=ON;ENABLE_AVX2=ON'"
