870dc5bc1e4240ac3028975871b35068034db891
Charles Coulombe <charles.coulombe@calculquebec.ca>
2022-07-27 13:01:44 -0400

    Updated torch_geometric and friends to torch 1.12
    Pinned torch version.
    Added 8.6 compute capability.
    Beautified pre_build_commands.

--- config/torch_sparse.sh
+++ config/torch_sparse.sh
@@ -1,3 +1,9 @@
-PYTHON_DEPS="torch==1.10.0 torch-scatter"
+TORCH_VERSION=1.12.0
+PYTHON_DEPS="torch==$TORCH_VERSION torch-scatter"
 MODULE_BUILD_DEPS="gcc/9.3.0 cuda/11.4 metis-64idx/5.1.0" # Must use 64 bits index module
-PRE_BUILD_COMMANDS="export TORCH_CUDA_ARCH_LIST='6.0;7.0;7.5;8.0'; export FORCE_CUDA=1; export WITH_METIS=1;"
+PRE_BUILD_COMMANDS="
+	export TORCH_CUDA_ARCH_LIST='6.0;7.0;7.5;8.0;8.6';
+	export FORCE_CUDA=1;
+	export WITH_METIS=1;
+	sed -i -E \"s#'scipy',#'scipy','torch==$TORCH_VERSION']#' setup.py;
+"
--- config/torch_spline_conv.sh
+++ config/torch_spline_conv.sh
@@ -1,3 +1,8 @@
-PYTHON_DEPS="torch==1.10.0"
+TORCH_VERSION=1.12.0
+PYTHON_DEPS="torch==$TORCH_VERSION"
 MODULE_BUILD_DEPS="gcc/9.3.0 cuda/11.4"
-PRE_BUILD_COMMANDS="export TORCH_CUDA_ARCH_LIST='6.0;7.0;7.5;8.0'; export FORCE_CUDA=1;"
+PRE_BUILD_COMMANDS="
+	export TORCH_CUDA_ARCH_LIST='6.0;7.0;7.5;8.0;8.6';
+	export FORCE_CUDA=1;
+	sed -i \"s#install_requires = \[\]#install_requires = \['torch==$TORCH_VERSION'\]#\" setup.py;
+"
