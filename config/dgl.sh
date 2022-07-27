2f0556a8a82d846b08af126f6452040be8e10980
Charles Coulombe <charles.coulombe@calculquebec.ca>
2022-07-27 09:36:32 -0400

    Updated DGL config.
    Use any recent torch.
    Added compute 8.6.
    Beautified config.

--- config/dgl.sh
+++ config/dgl.sh
@@ -5,11 +5,17 @@ PACKAGE_DOWNLOAD_NAME="$PACKAGE-$VERSION.tar.gz"
 PYTHON_IMPORT_NAME=$PACKAGE
 PACKAGE_FOLDER_NAME=$PACKAGE
 MODULE_BUILD_DEPS="cmake cuda/11.4 nccl cudnn"
-PYTHON_DEPS="cython numpy>=1.21.2 scipy networkx torch==1.10.0"
+PYTHON_DEPS="cython numpy>=1.21.2 scipy networkx torch"
 PACKAGE_DOWNLOAD_METHOD="Git"
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/dmlc/dgl"
 PACKAGE_DOWNLOAD_CMD="git clone --recursive $PACKAGE_DOWNLOAD_ARGUMENT --branch ${VERSION:?version required} $PACKAGE_FOLDER_NAME"
 POST_DOWNLOAD_COMMANDS="tar -zcf $PACKAGE_DOWNLOAD_NAME $PACKAGE_FOLDER_NAME"
-PRE_BUILD_COMMANDS="mkdir build && cd build && cmake -DUSE_OPENMP=ON -DUSE_CUDA=ON -DUSE_NCCL=ON -DUSE_SYSTEM_NCCL=ON -DCUDA_ARCH_BIN='60;70;75;80' -DCUDA_ARCH_NAME=Manual .. && make -j 4 && cd ../python "
+PRE_BUILD_COMMANDS="
+	mkdir build &&
+	cd build &&
+	cmake -DUSE_OPENMP=ON -DUSE_CUDA=ON -DUSE_NCCL=ON -DUSE_SYSTEM_NCCL=ON -DCUDA_ARCH_BIN='60;70;75;80;86' -DCUDA_ARCH_NAME=Manual .. &&
+	make -j 4 &&
+	cd ../python
+"
 # libdgl is not found, patch runpath
 POST_BUILD_COMMANDS='setrpaths.sh --path $WHEEL_NAME --add_path \$ORIGIN/../.. --any_interpreter'
