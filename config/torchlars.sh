8bfb7acf5c9d7b360ddc274c438eb695f8dd94c8
Charles Coulombe <charles.coulombe@calculquebec.ca>
2022-07-28 20:04:28 -0400

    Updated torchlars to pin torch version.

--- config/torchlars.sh
+++ config/torchlars.sh
@@ -1,3 +1,8 @@
+TORCH_VERSION=1.12.0
 MODULE_BUILD_DEPS="cuda/11.4"
-PYTHON_DEPS="torch"
-PRE_BUILD_COMMANDS="export TORCH_CUDA_ARCH_LIST='6.0;7.0;7.5;8.0'"
+PYTHON_DEPS="torch==$TORCH_VERSION"
+PRE_BUILD_COMMANDS="
+	export TORCH_CUDA_ARCH_LIST='6.0;7.0;7.5;8.0;8.6';
+	sed -i -e 's#${VERSION:?version required}#$VERSION+torch${TORCH_VERSION//./}#' torchlars/__version__.py;
+"
+UPDATE_REQUIREMENTS="'torch (==$TORCH_VERSION)'"
