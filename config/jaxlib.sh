13263c56ac44538550ebded05f802ad3cc62c1c6
lucasn42 <16ln6@queensu.ca>
2022-09-08 16:43:10 +0000

    Fixing jax_rleases repo URL to point to where CUDA-enabled wheels are

--- config/jaxlib.sh
+++ config/jaxlib.sh
@@ -8,6 +8,6 @@ if [[ $THIS_SCRIPT == 'build_wheel.sh' ]]; then
         echo "module load gcc ${MODULE_RUNTIME_DEPS}"
         echo "bash unmanylinuxize.sh --package jaxlib --add_path \$CUDA_HOME/lib64:\$EBROOTCUDNN/lib --version ${VERSION:-X.Y.Z} --find_links=https://storage.googleapis.com/jax-releases/jax_releases.html"
         module load gcc $MODULE_RUNTIME_DEPS
-        bash unmanylinuxize.sh --package jaxlib --add_path $CUDA_HOME/lib64:$EBROOTCUDNN/lib --version ${VERSION:?version required} --find_links=https://storage.googleapis.com/jax-releases/jax_releases.html
+        bash unmanylinuxize.sh --package jaxlib --add_path $CUDA_HOME/lib64:$EBROOTCUDNN/lib --version ${VERSION:?version required} --find_links=https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
         exit 1;
 fi
