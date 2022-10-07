1bbff8b84941bcecd9e7759de0603538074e9bbc
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2022-10-07 18:37:00 +0000

    added recipe for pore_c

---
+++ config/pore_c.sh
@@ -0,0 +1,3 @@
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/nanoporetech/pore-c/archive/refs/tags/${VERSION:?version required}.tar.gz"
+MODULE_BUILD_DEPS="gcc/9.3.0 cuda/11.4 arrow/1.0.0"
+UPDATE_REQUIREMENTS="'pairtools (<1.0)'"
