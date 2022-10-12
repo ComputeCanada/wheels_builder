d4a0dbba50029d6fd8d9464ab581f430ecf5fd5f
Charles Coulombe <charles.coulombe@calculquebec.ca>
2022-10-12 11:17:48 -0400

    Updated Alphafold recipe to be compatible with v2.2.3

--- config/alphafold.sh
+++ config/alphafold.sh
@@ -5,14 +5,14 @@ PACKAGE_DOWNLOAD_METHOD="Git"
 PATCHES="alphafold.patch" # Bundles scripts and run_alphafold.py
 # Use repo instead of release archive so we can prepare the wheel.
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/deepmind/alphafold.git"
-PACKAGE_DOWNLOAD_CMD="git clone --recursive $PACKAGE_DOWNLOAD_ARGUMENT --branch ${VERSION:?version required} $PACKAGE_FOLDER_NAME"
+PACKAGE_DOWNLOAD_CMD="git clone --recursive $PACKAGE_DOWNLOAD_ARGUMENT --branch v${VERSION:?version required} $PACKAGE_FOLDER_NAME"
 POST_DOWNLOAD_COMMANDS="tar -zcf $PACKAGE_DOWNLOAD_NAME $PACKAGE_FOLDER_NAME"
 # As Alphafold does not know how to distribute a software, we will use soft requirements from
 # setup.py. https://github.com/deepmind/alphafold/issues/511
 PRE_BUILD_COMMANDS=$(cat <<-END
 	sed -i -e 's/version=.*/version="$VERSION",/' setup.py;
 	sed -i -e 's/tensorflow-cpu/tensorflow/' requirements.txt setup.py;
-	sed -i -e 's/pandas==.*/pandas~=1.1.0/' -e 's/scipy==.*/scipy~=1.5.0/' requirements.txt setup.py;
+	sed -i -e 's/pandas==.*/pandas/' requirements.txt setup.py;
 	sed -i -e 's/aria2c/wget/g' -e 's/--dir=/-P /g' -e 's/--preserve-permissions//g' scripts/*.sh;
 	wget https://git.scicore.unibas.ch/schwede/openstructure/-/raw/7102c63615b64735c4941278d92b554ec94415f8/modules/mol/alg/src/stereo_chemical_props.txt -P alphafold/common/;
 END
