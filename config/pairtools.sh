f0bc05731e6451e70d46efa5208c8f5511376458
Charles Coulombe <charles.coulombe@calculquebec.ca>
2022-10-25 08:21:59 -0400

    Added pairtools config and patch. Makes it usable and installable.

---
+++ config/pairtools.sh
@@ -0,0 +1,3 @@
+MODULE_BUILD_DEPS='samtools'
+PYTHON_DEPS="cython numpy>=1.10 click>=6.6 scipy>=1.7.0 pandas>=1.3.4 pysam>=0.15.0 pyyaml bioframe>=0.3.3"
+PATCHES='pairtools-1.0.1-fix_pysam_libs.patch'
