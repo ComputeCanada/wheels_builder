4915947d7d19ca054ae6473577359faa0bf31620
Charles Coulombe <charles.coulombe@calculquebec.ca>
2022-11-11 10:56:30 -0500

    Updated config for Scipy with meson build

--- config/scipy.sh
+++ config/scipy.sh
@@ -1,4 +1,17 @@
-MODULE_BUILD_DEPS="arch/sse3 flexiblas/3.0.4"
+# pip download cannot be used as it tries to use meson build configuration with OpenBlas when trying to determine metadata, and fails.
+# we cannot patch the source dist before downloading it, so avoid pip download
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/scipy/scipy.git"
+PACKAGE_DOWNLOAD_NAME="$PACKAGE-$VERSION.tar.gz"
+PACKAGE_DOWNLOAD_METHOD="Git"
+PACKAGE_DOWNLOAD_CMD="git clone --recursive $PACKAGE_DOWNLOAD_ARGUMENT --branch v${VERSION:?version required} $PACKAGE_FOLDER_NAME"
+POST_DOWNLOAD_COMMANDS="tar -zcf ${PACKAGE}-${VERSION}.tar.gz $PACKAGE_FOLDER_NAME"
+
+MODULE_BUILD_DEPS="arch/sse3 flexiblas/3.0.4 cmake"
 PYTHON_DEPS_DEFAULT=""
-PYTHON_DEPS="numpy~=$NUMPY_DEFAULT_VERSION pytest pybind11 cython pythran"
+PYTHON_DEPS="numpy~=$NUMPY_DEFAULT_VERSION pytest pybind11 cython pythran meson-python"
+
+# Test test_x0_equals_Mb fail on avx512 capable processor:
+# https://github.com/scipy/scipy/issues/17075#issuecomment-1255594078
+# https://github.com/scipy/scipy/issues/15533
 PYTHON_TESTS="scipy.__config__.show(); scipy.test()"
+PRE_BUILD_COMMANDS="sed -i -e 's/openblas/flexiblas/' meson.build"
