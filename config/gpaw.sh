3b9b4ea2f0e2af6c6d51e48d255d988b156d2f70
Oliver Stueker <oliver.stueker@ace-net.ca>
2022-11-25 15:04:07 -0330

    updated for gpaw 22.8.0

--- config/gpaw.sh
+++ config/gpaw.sh
@@ -1,6 +1,8 @@
-# current GPAW (21.6.0) requires LibXC 3.x or 4.x.  Using module libxc/4.3.4
+# current GPAW (22.8.0) requires LibXC 3.x or 4.x or 5.1+.  Using module libxc/5.1.3
 # https://wiki.fysik.dtu.dk/gpaw/install.html#requirements
-MODULE_BUILD_DEPS="gcc openmpi blacs scalapack fftw libxc/4.3.4"
-# need a siteconfig.py for installation settings. Storing it within wheels_builder repo.
+MODULE_BUILD_DEPS="gcc openmpi flexiblas blacs scalapack fftw libxc/5.2.3"
+# Need a siteconfig.py for installation settings. Storing it within wheels_builder repo.
+# That file contains a list of libraries to link as well as compiler-/linker flags.
+# See also: https://wiki.fysik.dtu.dk/gpaw/install.html#customizing-installation
 export GPAW_CONFIG="${CONFIGDIR}/gpaw_siteconfig.py"
 # TEST_COMMAND="gpaw info "
--- config/gpaw_siteconfig-21.6.0.py
+++ config/gpaw_siteconfig-21.6.0.py
@@ -1,4 +1,4 @@
-libraries = [ 'flexiblas', 'scalapack', 'fftw3', 'xcf90', 'xc' ]
+libraries = [ 'openblas', 'scalapack', 'fftw3', 'xcf90', 'xc' ]
 extra_compile_args += ['-fopenmp']
 extra_link_args += ['-fopenmp']
 scalapack = True
