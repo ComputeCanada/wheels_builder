7712380b1f99036c7d3edf24ba326d35eaf04b9c
Charles Coulombe <charles.coulombe@calculquebec.ca>
2022-12-02 16:20:49 -0500

    Updated torchtext to support specific torch version

--- config/torchtext.sh
+++ config/torchtext.sh
@@ -1,4 +1,8 @@
 MODULE_BUILD_DEPS="cuda/11.4 cudnn cmake protobuf/3.21.3"
-PYTHON_DEPS="torch protobuf==4.21.3"
-PRE_BUILD_COMMANDS="export BUILD_VERSION=$VERSION; export PYTORCH_VERSION=\$(python -c 'import torch; print(torch.__version__)'); "
+# Set TORCH_VERSION in your shell to build with a specific version or latest/current by default.
+PYTHON_DEPS="torch${TORCH_VERSION:+==$TORCH_VERSION} protobuf==4.21.3"
+PRE_BUILD_COMMANDS="
+	export BUILD_VERSION=$VERSION;
+	export PYTORCH_VERSION=\$(python -c 'import torch; print(torch.__version__)');
+"
 PACKAGE_DOWNLOAD_ARGUMENT="git+https://github.com/pytorch/text@v${VERSION:?version required}"
