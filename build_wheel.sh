dd573fa5cadf77c1b8eb8870c7704c88bd54fd59
Charles Coulombe <charles.coulombe@calculquebec.ca>
2022-12-06 07:29:31 -0500

    Better find wheels to be tagged

--- build_wheel.sh
+++ build_wheel.sh
@@ -475,7 +475,8 @@ function adjust_torch_requirements_based_on_link_info()
 			log_command $SCRIPT_DIR/manipulate_wheels.py --print_req --wheels $TMP_WHEELHOUSE/$WHEEL_NAME
 
 			# Does it need to be tagged, would it override an existing wheel of the same version?
-			if [[ $(find /cvmfs/soft.computecanada.ca/custom/python/wheelhouse/ -name $WHEEL_NAME | wc -l) -gt 0 ]]; then
+			local wheel_pattern=$(echo ${WHEEL_NAME//+computecanada/-} | sed -e 's/--/\*/')
+			if [[ $(find /cvmfs/soft.computecanada.ca/custom/python/wheelhouse/ -name "${wheel_pattern}" | wc -l) -gt 0 ]]; then
 				echo "Found existing wheel that would have been overriden. Tagging the wheel."
 				local tag="torch${torch_build_version//./}"
 				log_command $SCRIPT_DIR/manipulate_wheels.py --inplace --force --wheels $TMP_WHEELHOUSE/$WHEEL_NAME --add_tag $tag
