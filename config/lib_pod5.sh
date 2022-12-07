a48f8bfe91fad5a316a9473222f9d3b64c34e2d4
Charles Coulombe <charles.coulombe@calculquebec.ca>
2022-12-07 10:34:40 -0500

    pod5-format-file was renamed for lib-pod5. Updated config for 0.1.0

--- config/lib_pod5.sh
+++ config/lib_pod5.sh
@@ -5,8 +5,9 @@ PACKAGE_DOWNLOAD_NAME="$PACKAGE-$VERSION.tar.gz"
 PACKAGE_DOWNLOAD_METHOD="Git"
 PACKAGE_DOWNLOAD_CMD="git clone --recursive $PACKAGE_DOWNLOAD_ARGUMENT --branch ${VERSION:?version required} $PACKAGE_FOLDER_NAME"
 POST_DOWNLOAD_COMMANDS="tar -zcf ${PACKAGE}-${VERSION}.tar.gz $PACKAGE_FOLDER_NAME"
+PYTHON_DEPS='build '
 PRE_BUILD_COMMANDS="
 	cmake -B build . &&
 	cmake --build build -- -j 4 &&
-	cd python/pod5_format
+	cd python/lib_pod5
 "
