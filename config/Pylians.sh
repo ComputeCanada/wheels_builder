a16b4f6bf38ae33cd0f9b432c4d687ee46d892d4
Charles Coulombe <charles.coulombe@calculquebec.ca>
2022-12-14 09:22:28 -0500

    Added Pylians config

---
+++ config/Pylians.sh
@@ -0,0 +1,23 @@
+PYTHON_IMPORT_NAME=""
+# From https://raw.githubusercontent.com/franciscovillaescusa/Pylians3/master/Tests/import_libraries.py
+TEST_COMMAND="python -c 'import bias_library; \
+	import cosmology_library; \
+	import density_field_library; \
+	import HI_clusters_library; \
+	import HOD_library; \
+	import integration_library; \
+	import MAS_library; \
+	import mass_function_library; \
+	import Pk_library; \
+	import plotting_library; \
+	import readfof; \
+	import readsnap; \
+	import readgadget; \
+	import readsubf; \
+	import redshift_space_library; \
+	import routines; \
+	import smoothing_library; \
+	import sorting_library; \
+	import units_library; \
+	import void_library;'
+"
