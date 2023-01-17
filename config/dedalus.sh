93bb6097a1d95f9455a479e7034f61376cb2c5d1
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-01-17 13:40:15 -0500

    Use mpicc

--- config/dedalus.sh
+++ config/dedalus.sh
@@ -1,5 +1,5 @@
 MODULE_RUNTIME_DEPS='openmpi mpi4py fftw-mpi hdf5-mpi'
 PYTHON_DEPS="h5py pytest mercurial"
-PRE_BUILD_COMMANDS='export MPI_PATH=$EBROOTOPENMPI; export FFTW_PATH=$EBROOTFFTW; export FFTW_STATIC=1; export CC=gcc;'
+PRE_BUILD_COMMANDS='export MPI_PATH=$EBROOTOPENMPI; export FFTW_PATH=$EBROOTFFTW; export FFTW_STATIC=1; export CC=mpicc;'
 # pip download runs the setup in order to build the metadata, which requires the paths to be known...
 PRE_DOWNLOAD_COMMANDS=$PRE_BUILD_COMMANDS
