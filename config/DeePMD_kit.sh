5d5883f6f9fe94857994bf4050dd504549e80ef0
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-01-27 17:07:55 -0500

    Added DeePMD-kit config

---
+++ config/DeePMD_kit.sh
@@ -0,0 +1,6 @@
+MODULE_BUILD_DEPS="cuda/11.4"
+MODULE_RUNTIME_DEPS="openmpi mpi4py"
+PYTHON_DEPS="tensorflow scikit-build"
+PRE_BUILD_COMMANDS="export DP_VARIANT=cuda;"
+RPATH_TO_ADD="'\$ORIGIN/../../tensorflow'"
+UPDATE_REQUIREMENTS='tensorflow-cpu -> tensorflow'
