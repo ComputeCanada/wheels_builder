8bdc76f173e1670355f1a2f274143b2740492107
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-01-27 17:13:29 -0500

    Updated Pillow-SIMD config to use avx2 by default, and correctly download

--- config/Pillow-SIMD.sh
+++ config/Pillow-SIMD.sh
@@ -4,3 +4,5 @@ else
 	PRE_BUILD_COMMANDS="export CC=\"cc -mavx2\"; export CFLAGS=\"-I/cvmfs/soft.computecanada.ca/gentoo/2020/usr/include -L/cvmfs/soft.computecanada.ca/gentoo/2020/usr/lib64 \""
 fi
 PYTHON_IMPORT_NAME="PIL"
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/uploadcare/pillow-simd/archive/refs/tags/${VERSION:?version required}.tar.gz"
+MODULE_BUILD_DEPS="arch/avx2"
