00577a11c1cfbef95e7f4de548f6ad06c822072d
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-02-08 06:07:01 -0500

    Updated DGL config for v1.0 which nows depends on torch.

--- config/dgl.sh
+++ config/dgl.sh
@@ -4,8 +4,8 @@ PACKAGE="dgl"
 PACKAGE_DOWNLOAD_NAME="$PACKAGE-$VERSION.tar.gz"
 PYTHON_IMPORT_NAME=$PACKAGE
 PACKAGE_FOLDER_NAME=$PACKAGE
-MODULE_BUILD_DEPS="cmake cuda/11.4 nccl cudnn"
-PYTHON_DEPS="cython numpy>=1.21.2 scipy networkx torch"
+MODULE_BUILD_DEPS="cmake cuda/11.4 nccl cudnn protobuf"
+PYTHON_DEPS="cython numpy>=1.21.2 scipy networkx torch${TORCH_VERSION:+==$TORCH_VERSION}"
 PACKAGE_DOWNLOAD_METHOD="Git"
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/dmlc/dgl"
 PACKAGE_DOWNLOAD_CMD="git clone --recursive $PACKAGE_DOWNLOAD_ARGUMENT --branch ${VERSION:?version required} $PACKAGE_FOLDER_NAME"
@@ -13,9 +13,10 @@ POST_DOWNLOAD_COMMANDS="tar -zcf $PACKAGE_DOWNLOAD_NAME $PACKAGE_FOLDER_NAME"
 PRE_BUILD_COMMANDS="
 	mkdir build &&
 	cd build &&
+	export TORCH_CUDA_ARCH_LIST='6.0;7.0;7.5;8.0;8.6' &&
 	cmake -DUSE_OPENMP=ON -DUSE_CUDA=ON -DUSE_NCCL=ON -DUSE_SYSTEM_NCCL=ON -DCUDA_ARCH_BIN='60;70;75;80;86' -DCUDA_ARCH_NAME=Manual .. &&
 	make -j 4 &&
 	cd ../python
 "
 # libdgl is not found, patch runpath
-POST_BUILD_COMMANDS='setrpaths.sh --path $WHEEL_NAME --add_path \$ORIGIN/../.. --any_interpreter'
+POST_BUILD_COMMANDS='setrpaths.sh --path $WHEEL_NAME --add_path \$ORIGIN/../..:\$ORIGIN/..:\$ORIGIN/../../torch/lib --any_interpreter'
