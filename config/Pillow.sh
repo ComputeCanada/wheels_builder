d3eb524df875be0f47a75bfa19b1a335cdcd364a
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-03-30 09:15:08 -0400

    Added raqm support to Pillow

--- config/Pillow.sh
+++ config/Pillow.sh
@@ -1,7 +1,13 @@
-PYTHON_DEPS="olefile"
 PYTHON_IMPORT_NAME="PIL"
 if [[ -z $EBROOTGENTOO ]]; then
 	PRE_BUILD_COMMANDS="sed -i -e 's;/sbin/ldconfig;ldconfig;g' setup.py && CPATH=$NIXUSER_PROFILE/include:$CPATH "
 else
-	PRE_BUILD_COMMANDS="sed -i -e 's;/sbin/ldconfig;ldconfig;g' setup.py && CPATH=$EBROOTGENTOO/include:$CPATH LIBRARY_PATH=$EBROOTGENTOO/lib:$LIBRARY_PATH "
+	PRE_BUILD_COMMANDS="
+		sed -i -e 's;/sbin/ldconfig;ldconfig;g' setup.py;
+		CPATH=$EBROOTGENTOO/include:$CPATH;
+		LIBRARY_PATH=$EBROOTGENTOO/lib:$LIBRARY_PATH;
+		python setup.py build_ext --inplace --enable-raqm;
+	"
 fi
+MODULE_BUILD_DEPS="raqm/0.9.0"
+TEST_COMMAND="python -c 'import PIL; from PIL import features; assert features.check(\"freetype2\"); assert features.check(\"raqm\")'"
