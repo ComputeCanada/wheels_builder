cc49f9db0ac942d4407a3d9cf03853aa06bc66b8
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-03-30 09:25:32 -0400

    Updated cupy config

--- config/cupy.sh
+++ config/cupy.sh
@@ -1,4 +1,4 @@
-PYTHON_DEPS=" scipy optuna"
+PYTHON_DEPS="optuna"
 MODULE_BUILD_DEPS="gcc/9.3.0 cuda/11.4 cudnn nccl cutensor"
 # needed otherwise it does not find libcuda.so
 PRE_DOWNLOAD_COMMANDS='export LDFLAGS="-L$EBROOTCUDA/lib64/stubs -L$EBROOTNCCL/lib" ; export CFLAGS="-I$EBROOTNCCL/include/"'
