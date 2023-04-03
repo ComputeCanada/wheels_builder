1893c71e1ffedc459033329c81fb8cc54bf6f310
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-04-03 09:04:09 -0400

    Fixed quoting in Pillow* configs

--- config/Pillow-SIMD.sh
+++ config/Pillow-SIMD.sh
@@ -1,13 +1,13 @@
 if [[ -z "$EBROOTGENTOO" ]]; then
 	PRE_BUILD_COMMANDS="export CC=\"cc -mavx2\""
 else
-	PRE_BUILD_COMMANDS="
-		export CC=\"cc -mavx2\";
-		sed -i -e 's;/sbin/ldconfig;ldconfig;g' setup.py;
+	PRE_BUILD_COMMANDS='
+		export CC="cc -mavx2";
+		sed -i -e "s;/sbin/ldconfig;ldconfig;g" setup.py;
 		CPATH=$EBROOTGENTOO/include:$CPATH;
 		LIBRARY_PATH=$EBROOTGENTOO/lib:$LIBRARY_PATH;
 		python setup.py build_ext --inplace --enable-raqm;
-	"
+	'
 fi
 PYTHON_IMPORT_NAME="PIL"
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/uploadcare/pillow-simd/archive/refs/tags/v${VERSION:?version required}.tar.gz"
--- config/Pillow.sh
+++ config/Pillow.sh
@@ -2,12 +2,12 @@ PYTHON_IMPORT_NAME="PIL"
 if [[ -z $EBROOTGENTOO ]]; then
 	PRE_BUILD_COMMANDS="sed -i -e 's;/sbin/ldconfig;ldconfig;g' setup.py && CPATH=$NIXUSER_PROFILE/include:$CPATH "
 else
-	PRE_BUILD_COMMANDS="
-		sed -i -e 's;/sbin/ldconfig;ldconfig;g' setup.py;
+	PRE_BUILD_COMMANDS='
+		sed -i -e "s;/sbin/ldconfig;ldconfig;g" setup.py;
 		CPATH=$EBROOTGENTOO/include:$CPATH;
 		LIBRARY_PATH=$EBROOTGENTOO/lib:$LIBRARY_PATH;
 		python setup.py build_ext --inplace --enable-raqm;
-	"
+	'
 fi
 MODULE_BUILD_DEPS="raqm/0.9.0"
 TEST_COMMAND="python -c 'import PIL; from PIL import features; assert features.check(\"freetype2\"); assert features.check(\"raqm\")'"
