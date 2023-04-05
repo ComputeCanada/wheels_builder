b6206498b58e726e4d2607849dab7b41f4d6afe9
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-04-05 07:53:22 -0400

    Fixed protobuf compilation

--- config/protobuf.sh
+++ config/protobuf.sh
@@ -12,17 +12,13 @@ PACKAGE_DOWNLOAD_CMD="git clone --recursive $PACKAGE_DOWNLOAD_ARGUMENT --branch 
 POST_DOWNLOAD_COMMANDS="tar -zcf $PACKAGE_DOWNLOAD_NAME $PACKAGE_FOLDER_NAME"
 MODULE_BUILD_DEPS="cmake"
 PYTHON_IMPORT_NAME="google.protobuf"
-# In order to be compatible across different versions, add symlinks to current directory
-# but also keep old hardcoded directory. Versions since 4.22 can use:
-# python setup.py build_ext -v --cpp_implementation --compile_static_extension --link-objects='../src/.libs/libprotobuf.a ../src/.libs/libprotobuf-lite.a';
-PRE_BUILD_COMMANDS="
-	cmake -B build -S cmake -DBUILD_SHARED_LIBS=OFF -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=\$PWD/src -DCMAKE_CXX_FLAGS='-fPIC' -DCMAKE_VERBOSE_MAKEFILE=ON &&
-	cmake --build build --parallel 4 &&
-	cmake --install build &&
-	mv src/lib64 src/.libs &&
-	ln -s src/.libs/libprotobuf.a libprotobuf.a &&
-	ln -s src/.libs/libprotobuf-lite.a libprotobuf_lite.a &&
-	export PROTOC=\$PWD/src/bin/protoc &&
-	cd python;
-"
+PRE_BUILD_COMMANDS='
+	cmake -B . -S cmake -DBUILD_SHARED_LIBS=OFF -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=$PWD -DCMAKE_CXX_FLAGS="-fPIC" -DCMAKE_VERBOSE_MAKEFILE=ON &&
+	cmake --build . --parallel 4 &&
+	cmake --install . &&
+	export PROTOC=$PWD/bin/protoc &&
+	cd python &&
+	sed -i -e "s#/src/.libs##" setup.py;
+'
 BDIST_WHEEL_ARGS="-v --cpp_implementation --compile_static_extension"
+TEST_COMMAND="python -c 'import $PYTHON_IMPORT_NAME; from $PYTHON_IMPORT_NAME.pyext import _message'"
