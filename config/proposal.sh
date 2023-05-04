56dc92c541efb4c3abc0021d1ca26d6a2b4676e1
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-05-04 08:18:15 -0400

    Updated PROPOSAL config with available dependencies

--- config/proposal.sh
+++ config/proposal.sh
@@ -1,1 +1,6 @@
-PYTHON_DEPS="scikit-build"
+MODULE_BUILD_DEPS='gcc cmake cubic_interpolation spdlog eigen boost'
+PRE_BUILD_COMMANDS='
+	export BUILD_CORES=4;
+	export NO_CONAN=1;
+	sed -i "/'\''-DBUILD_TESTING=OFF'\'',/a '\''-DBoost_NO_BOOST_CMAKE=ON'\''," setup.py;
+'
