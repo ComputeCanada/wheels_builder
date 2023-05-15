8d84f37fdba699d29999a29bda498bb88e049dab
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-05-15 08:11:27 -0400

    Updated astropy config

--- config/astropy.sh
+++ config/astropy.sh
@@ -1,6 +1,6 @@
 PACKAGE="astropy"
 PYTHON_IMPORT_NAME=$PACKAGE
 PACKAGE_FOLDER_NAME=$PACKAGE
-PYTHON_DEPS="scipy networkx extension_helpers jinja2 pytest-astropy"
+PYTHON_DEPS="extension_helpers pytest-astropy"
 #TEST_COMMAND="python -c import astropy;astropy.test()"
 PYTHON_TESTS="astropy.test()"
