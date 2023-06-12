8f5b95aca3bf28c92dd40452bdee6e43b4bcab0a
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-06-12 16:06:33 -0400

    Added symengine and opencv_* config to avoid automatic addition

---
+++ config/opencv_python.sh
@@ -0,0 +1,4 @@
+echo "OpenCV is a module with Python bindings. https://docs.alliancecan.ca/wiki/OpenCV#Python_bindings"
+echo "Please set MODULE_RUNTIME_DEPS='opencv'"
+echo "Not building wheel."
+exit 1
---
+++ config/symengine.sh
@@ -0,0 +1,4 @@
+echo "Symengine is a module with Python bindings."
+echo "Please add MODULE_RUNTIME_DEPS='symengine'"
+echo "Not building wheel."
+exit 1
