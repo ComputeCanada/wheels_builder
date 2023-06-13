7e2266a35afcc68152af48bf1be79fc733019367
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-06-13 07:33:06 -0400

    Updated llvmlite and numba config. Now uses LLVM 14

--- config/llvmlite.sh
+++ config/llvmlite.sh
@@ -1,13 +1,2 @@
-
-if [[ -z "$EBROOTGENTOO" ]]; then
-  PYTHON_DEPS="enum34"
-  MODULE_BUILD_DEPS="llvm cuda/10.1 tbb"
-else
-  if [[ "$VERSION" == "0.32.1" ]]; then
-    MODULE_BUILD_DEPS="llvm/8 cuda/11.0 tbb"
-  else
-    MODULE_BUILD_DEPS="llvm/11 cuda/11.4 tbb"
-  fi
-fi
+MODULE_BUILD_DEPS="llvm/14 cuda/11.4 tbb"
 PATCHES="llvmlite-0.28.0-fpic.patch"
-
--- config/numba.sh
+++ config/numba.sh
@@ -1,1 +1,2 @@
 PYTHON_DEPS=" enum34 llvmlite"
+MODULE_RUNTIME_DEPS='arch/avx2' # llvmlite is now under avx2
