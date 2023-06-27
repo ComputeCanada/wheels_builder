c075026b0fc15843a76a031fbcc4e89facb8ae0b
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-06-27 11:34:47 -0400

    Updated lib_pod5 to work the version bodge

--- config/lib_pod5.sh
+++ config/lib_pod5.sh
@@ -5,9 +5,13 @@ PACKAGE_DOWNLOAD_NAME="$PACKAGE-$VERSION.tar.gz"
 PACKAGE_DOWNLOAD_METHOD="Git"
 PACKAGE_DOWNLOAD_CMD="git clone --recursive $PACKAGE_DOWNLOAD_ARGUMENT --branch ${VERSION:?version required} $PACKAGE_FOLDER_NAME"
 POST_DOWNLOAD_COMMANDS="tar -zcf ${PACKAGE}-${VERSION}.tar.gz $PACKAGE_FOLDER_NAME"
-PYTHON_DEPS='build '
+PYTHON_DEPS="build setuptools setuptools_scm[toml]"
+# The root pyproject is for pod5version that creates a version file when built, which then requires a custom script to create a version file
+# in order for the cmake to run without error.........................
 PRE_BUILD_COMMANDS="
-	cmake -B build . &&
+	python -m build . -x -n -s &&
+	python pod5_make_version.py &&
+	cmake -B build . -DPOD5_BUILD_EXAMPLES=OFF &&
 	cmake --build build -- -j 4 &&
 	cd python/lib_pod5
 "
