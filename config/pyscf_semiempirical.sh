27c6c08ea14b012175c73fa4bf6dfe7b167cbd9f
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-06-29 10:40:16 -0400

    Added pyscf-semiempirical config

---
+++ config/pyscf_semiempirical.sh
@@ -0,0 +1,3 @@
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/pyscf/semiempirical/archive/refs/tags/v${VERSION:?version required}.tar.gz"
+PYTHON_DEPS='pyscf'
+PYTHON_IMPORT_NAME='pyscf.semiempirical'
