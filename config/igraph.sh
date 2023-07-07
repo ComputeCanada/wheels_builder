194629e8922949a5588af7d8b54375baabbd7c48
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-07-07 08:05:26 -0400

    Updated igraph config to abort and refer to module instead

--- config/igraph.sh
+++ config/igraph.sh
@@ -1,2 +1,4 @@
-MODULE_BUILD_DEPS='flexiblas cmake arpack-ng gmp glpk'
-
+echo "igraph is a module with Python bindings."
+echo "Please set MODULE_RUNTIME_DEPS='igraph'"
+echo "Not building wheel."
+exit 1
