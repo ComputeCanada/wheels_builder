651874aebb753b8ef9f82abc98350386ae0ec3c1
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-08-10 08:05:30 -0400

    Added pyg-lib config

---
+++ config/pyg-lib.sh
@@ -0,0 +1,13 @@
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/pyg-team/pyg-lib"
+PACKAGE_DOWNLOAD_NAME="$PACKAGE-$VERSION.tar.gz"
+PACKAGE_DOWNLOAD_METHOD="Git"
+PACKAGE_DOWNLOAD_CMD="git clone --recursive $PACKAGE_DOWNLOAD_ARGUMENT --branch ${VERSION:?version required} $PACKAGE_FOLDER_NAME"
+POST_DOWNLOAD_COMMANDS="tar -zcf ${PACKAGE}-${VERSION}.tar.gz $PACKAGE_FOLDER_NAME"
+PYTHON_DEPS="torch${TORCH_VERSION:+==$TORCH_VERSION}"
+MODULE_BUILD_DEPS="gcc/9.3.0 cuda/11.7 protobuf cudnn"
+PRE_BUILD_COMMANDS="
+	export TORCH_CUDA_ARCH_LIST='6.0;7.0;7.5;8.0;8.6';
+	export FORCE_CUDA=1;
+    export USE_CUDNN=1;
+	sed -i \"s#install_requires = \[\]#install_requires = \['torch'\]#\" setup.py;
+"
