b5721027879420e38a7d699d3a1e1c076f902973
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2023-08-30 20:35:47 +0000

    new version of pandas require newer version of numpy than oldest-supported-numpy

--- config/pandas.sh
+++ config/pandas.sh
@@ -1,4 +1,9 @@
 # Pandas does not support the oldest-supported-numpy version
-PYTHON_DEPS='versioneer numpy==1.21;python_version=="3.9" meson-python'
+if [[ ${VERSION} =~ 2.0.* ]]; then
+	PYTHON_DEPS='versioneer numpy==1.21;python_version=="3.9" meson-python'
+else
+#elif [[ ${VERSION} =~ 2.1.* ]]; then
+	PYTHON_DEPS='versioneer numpy~=1.22.4;python_version=="3.9" numpy~=1.22.4;python_version=="3.10" meson-python'
+fi
 PRE_BUILD_COMMANDS="python setup.py clean --all"
 
