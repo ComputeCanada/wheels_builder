ec6dad663782436bd9a726afbc49c0efb32fce45
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2023-08-31 13:03:42 +0000

    adjust cryptography config file for version >40

--- config/cryptography.sh
+++ config/cryptography.sh
@@ -1,3 +1,3 @@
-PYTHON_DEPS="setuptools_rust cffi pycparser"
-PYTHON_IMPORT_NAME="cryptography.hazmat.bindings._openssl"
+PYTHON_DEPS="setuptools_rust cffi pycparser jaraco.classes keyring"
+PYTHON_IMPORT_NAME="cryptography.hazmat.bindings.openssl.binding"
 MODULE_BUILD_DEPS="rust"
