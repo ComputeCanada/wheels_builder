6b104621555f88d7e0e41cd9c633f053c2987595
Helene Gingras <helene.gingras@calculquebec.ca>
2023-09-20 15:19:19 +0000

    Ajout du fichier config idr.sh avec les quotes

--- config/idr.sh
+++ config/idr.sh
@@ -1,1 +1,1 @@
-PACKAGE_DOWNLOAD_ARGUMENT=https://github.com/nboley/idr/archive/refs/tags/${VERSION:?version required}.tar.gz
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/nboley/idr/archive/refs/tags/${VERSION:?version required}.tar.gz"
