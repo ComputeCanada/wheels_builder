5a10b9cb1460a5ecd646f067b99307147125064b
Bart Oldeman <bart.oldeman@calculquebec.ca>
2023-10-10 12:25:30 +0000

    Adjust numpy config for 2023
    This corrects the regexp to use .* instead of *, and uses
    PYTHON_DEPS="$PYTHON_DEPS setuptools==63.4.3" instead of
    using the older python-build-bundle module for older numpy.

--- config/numpy.sh
+++ config/numpy.sh
@@ -1,5 +1,5 @@
 # One could also set NPY_BLAS_LIBS, NPY_LAPACK_LIBS and NPY_CBLAS_LIBS since numpy 1.21.
-PRE_BUILD_COMMANDS='module load flexiblas/3.0.4;
+PRE_BUILD_COMMANDS="module load flexiblas/$FLEXIBLAS_VERSION;"'
 echo "[DEFAULT]" > site.cfg;
 echo "library_dirs = $EBROOTFLEXIBLAS/lib" >> site.cfg;
 echo "include_dirs = $EBROOTFLEXIBLAS/include/flexiblas" >> site.cfg;
@@ -11,15 +11,19 @@ echo "[lapack]" >> site.cfg;
 echo "libraries = flexiblas" >> site.cfg;
 echo "[blas]" >> site.cfg;
 echo "libraries = flexiblas" >> site.cfg;'
-MODULE_BUILD_DEPS="arch/sse3 flexiblas/3.0.4"
+MODULE_BUILD_DEPS="flexiblas/$FLEXIBLAS_VERSION"
+if [[ $EBVERSIONGENTOO != 2023 ]]; then
+	MODULE_BUILD_DEPS="arch/sse3 $MODULE_BUILD_DEPS"
+fi
 PYTHON_DEPS="nose hypothesis"
 PYTHON_DEPS_DEFAULT=""
+MODULE_BUILD_DEPS_DEFAULT="cython/.0.29.36 pytest python-build-bundle"
 # old versions don't build with most recent python-build-bundle (recent setuptools)
-if [[ ${VERSION} =~ 1.1* ]]; then
-	MODULE_BUILD_DEPS_DEFAULT="cython/.0.29.36 pytest python-build-bundle/2023a"
-elif [[ ${VERSION} =~ 1.2[0123]* ]]; then
-	MODULE_BUILD_DEPS_DEFAULT="cython/.0.29.36 pytest python-build-bundle/2023a"
-else
-	MODULE_BUILD_DEPS_DEFAULT="cython/.0.29.36 pytest python-build-bundle"
+if [[ ${VERSION} =~ 1.1.* || ${VERSION} =~ 1.2[0123].* ]]; then
+	if [[ $EBVERSIONGENTOO == 2023 ]]; then
+		PYTHON_DEPS="$PYTHON_DEPS setuptools==63.4.3"
+	else
+		MODULE_BUILD_DEPS_DEFAULT="cython/.0.29.36 pytest python-build-bundle/2023a"
+	fi
 fi
 PYTHON_TESTS="numpy.__config__.show(); numpy.test()"
