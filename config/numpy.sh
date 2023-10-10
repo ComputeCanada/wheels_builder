2df3b7980dcba33a2cf3e3c231642bd5fd5c835a
Bart Oldeman <bart.oldeman@calculquebec.ca>
2023-10-10 12:56:47 +0000

    Use default version for flexiblas module

--- config/numpy.sh
+++ config/numpy.sh
@@ -1,5 +1,5 @@
 # One could also set NPY_BLAS_LIBS, NPY_LAPACK_LIBS and NPY_CBLAS_LIBS since numpy 1.21.
-PRE_BUILD_COMMANDS="module load flexiblas/$FLEXIBLAS_VERSION;"'
+PRE_BUILD_COMMANDS='module load flexiblas;
 echo "[DEFAULT]" > site.cfg;
 echo "library_dirs = $EBROOTFLEXIBLAS/lib" >> site.cfg;
 echo "include_dirs = $EBROOTFLEXIBLAS/include/flexiblas" >> site.cfg;
@@ -11,7 +11,7 @@ echo "[lapack]" >> site.cfg;
 echo "libraries = flexiblas" >> site.cfg;
 echo "[blas]" >> site.cfg;
 echo "libraries = flexiblas" >> site.cfg;'
-MODULE_BUILD_DEPS="flexiblas/$FLEXIBLAS_VERSION"
+MODULE_BUILD_DEPS="flexiblas"
 if [[ $EBVERSIONGENTOO != 2023 ]]; then
 	MODULE_BUILD_DEPS="arch/sse3 $MODULE_BUILD_DEPS"
 fi
