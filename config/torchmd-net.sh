ade2947932cc0dc800dd8a3ddc8940a589490dd4
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-10-17 08:12:19 -0400

    Added torchmd-net config

---
+++ config/torchmd-net.sh
@@ -0,0 +1,4 @@
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/torchmd/torchmd-net/archive/refs/tags/${VERSION:?version required}.tar.gz"
+MODULE_BUILD_DEPS="cuda/11.7"
+PYTHON_DEPS="torch${TORCH_VERSION:+==$TORCH_VERSION}"
+PYTHON_IMPORT_NAME='torchmdnet'
