1c2843afe1988b69dec50a18756c652abf686dcd
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-10-17 10:33:45 -0400

    Updated torchmd-net, fixed incorrectly set version

--- config/torchmd-net.sh
+++ config/torchmd-net.sh
@@ -2,3 +2,4 @@ PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/torchmd/torchmd-net/archive/refs/t
 MODULE_BUILD_DEPS="cuda/11.7"
 PYTHON_DEPS="torch${TORCH_VERSION:+==$TORCH_VERSION}"
 PYTHON_IMPORT_NAME='torchmdnet'
+PRE_BUILD_COMMANDS="sed -i -e \"s/version=version/version='$VERSION'/\" setup.py"
