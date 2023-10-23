dfddd33ad0edec5858f759b8b70d450017e3a91b
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-10-23 11:55:05 -0400

    Updated llvmlite config for 2023

--- config/llvmlite.sh
+++ config/llvmlite.sh
@@ -1,2 +1,6 @@
-MODULE_BUILD_DEPS="llvm/14 cuda/11.4 tbb"
-PATCHES="llvmlite-0.28.0-fpic.patch"
+if [[ "$EBVERSIONGENTOO" == "2023" ]]; then
+	MODULE_BUILD_DEPS="llvm/14 cuda"
+else
+	MODULE_BUILD_DEPS="llvm/14 cuda/11.4 tbb"
+	PATCHES="llvmlite-0.28.0-fpic.patch"
+fi
