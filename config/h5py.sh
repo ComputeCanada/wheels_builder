e8b8288e2481913941ae5e28c22ddf78006feb35
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-10-27 14:40:16 -0400

    Updated h5py for 2023

--- config/h5py.sh
+++ config/h5py.sh
@@ -5,9 +5,8 @@ if [[ -z $EBROOTGENTOO ]]; then
 	MODULE_BUILD_DEPS="gcc/7.3.0 hdf5"
   fi
 else
-	MODULE_BUILD_DEPS="gcc/9.3.0 hdf5"
+	MODULE_BUILD_DEPS="hdf5"
 fi
-PYTHON_DEPS="nose  six unittest2"
+PYTHON_DEPS="nose six unittest2 pytest"
 PYTHON_TESTS="h5py.run_tests()"
-PRE_SETUP_COMMANDS="unset EBPYTHONPREFIXES"
 
