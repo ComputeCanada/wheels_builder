965812a068bdda55b1cc052e548f3f5f0b9c61b9
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-10-27 07:44:52 -0400

    Updated protobuf config for 4.21.0 and use upb now on

--- config/protobuf.sh
+++ config/protobuf.sh
@@ -3,22 +3,29 @@
 
 # When possible, build the module, but provide this config other version for an optimized wheel.
 # Wheel will be avx2
-PACKAGE="protobuf"
-PACKAGE_DOWNLOAD_NAME="$PACKAGE-$VERSION.tar.gz"
-PACKAGE_FOLDER_NAME=$PACKAGE
-PACKAGE_DOWNLOAD_METHOD="Git"
-PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/protocolbuffers/protobuf"
-PACKAGE_DOWNLOAD_CMD="git clone --recursive $PACKAGE_DOWNLOAD_ARGUMENT --branch v${VERSION:?version required} $PACKAGE_FOLDER_NAME"
-POST_DOWNLOAD_COMMANDS="tar -zcf $PACKAGE_DOWNLOAD_NAME $PACKAGE_FOLDER_NAME"
-MODULE_BUILD_DEPS="cmake"
+
 PYTHON_IMPORT_NAME="google.protobuf"
-PRE_BUILD_COMMANDS='
-	cmake -B . -S cmake -DBUILD_SHARED_LIBS=OFF -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=$PWD -DCMAKE_CXX_FLAGS="-fPIC" -DCMAKE_VERBOSE_MAKEFILE=ON &&
-	cmake --build . --parallel 4 &&
-	cmake --install . &&
-	export PROTOC=$PWD/bin/protoc &&
-	cd python &&
-	sed -i -e "s#/src/.libs##" setup.py;
-'
-BDIST_WHEEL_ARGS="-v --cpp_implementation --compile_static_extension"
-TEST_COMMAND="python -c 'import $PYTHON_IMPORT_NAME; from $PYTHON_IMPORT_NAME.pyext import _message'"
+
+# New upb implemantation is faster, and c++ api was deprecated in 4.21
+if [[ $(translate_version $VERSION) -le $(translate_version '4.21.0')  ]]; then
+	PACKAGE="protobuf"
+	PACKAGE_DOWNLOAD_NAME="$PACKAGE-$VERSION.tar.gz"
+	PACKAGE_FOLDER_NAME=$PACKAGE
+	PACKAGE_DOWNLOAD_METHOD="Git"
+	PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/protocolbuffers/protobuf"
+	PACKAGE_DOWNLOAD_CMD="git clone --depth 1 --recursive $PACKAGE_DOWNLOAD_ARGUMENT --branch v${VERSION:?version required} $PACKAGE_FOLDER_NAME"
+	POST_DOWNLOAD_COMMANDS="tar -zcf $PACKAGE_DOWNLOAD_NAME $PACKAGE_FOLDER_NAME"
+	MODULE_BUILD_DEPS="cmake"
+	PRE_BUILD_COMMANDS='
+		cmake -B . -S cmake -DBUILD_SHARED_LIBS=OFF -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=$PWD -DCMAKE_CXX_FLAGS="-fPIC" -DCMAKE_VERBOSE_MAKEFILE=ON &&
+		cmake --build . --parallel 4 &&
+		cmake --install . &&
+		export PROTOC=$PWD/bin/protoc &&
+		cd python &&
+		sed -i -e "s#/src/.libs##" setup.py;
+	'
+	BDIST_WHEEL_ARGS="-v --cpp_implementation --compile_static_extension"
+	TEST_COMMAND="python -c 'import $PYTHON_IMPORT_NAME; from $PYTHON_IMPORT_NAME.pyext import _message'"
+else
+	TEST_COMMAND="python -c 'import $PYTHON_IMPORT_NAME; from google import _upb'"
+fi
