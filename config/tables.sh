5e8740b4dccf032985ee9f4cb6e42eae1e28f81e
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-10-30 06:36:01 -0400

    Updated tables config

--- config/tables.sh
+++ config/tables.sh
@@ -1,5 +1,8 @@
-MODULE_BUILD_DEPS="gcc hdf5"
-PYTHON_DEPS="h5py  numexpr six nose mock"
+MODULE_BUILD_DEPS="hdf5 blosc2"
+PYTHON_DEPS="h5py numexpr six nose mock packaging blosc2"
 PYTHON_TESTS="tables.test()"
-PRE_SETUP_COMMANDS="unset EBPYTHONPREFIXES"
-
+PRE_BUILD_COMMANDS='
+	rm -r c-blosc/{blosc,internal-complibs};
+	sed -i "s@/usr/local@$EBROOTGENTOO@" setup.py;
+'
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/PyTables/PyTables/archive/refs/tags/v${VERSION:?version required}.tar.gz"
