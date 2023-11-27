b2b3f46c9f51586895e38226aa5da4af47a3e3f9
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-11-27 09:03:35 -0500

    Added torchtuples config

---
+++ config/torchtuples.sh
@@ -0,0 +1,1 @@
+PYTHON_DEPS='torch --find-links=/cvmfs/soft.computecanada.ca/custom/python/wheelhouse/gentoo/avx2/ --find-links=/cvmfs/soft.computecanada.ca/custom/python/wheelhouse/gentoo2023/x86-64-v3'
