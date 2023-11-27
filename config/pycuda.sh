ff1d0462c609fd7caef993c1762f4cc2ceeff49f
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-11-27 08:51:53 -0500

    Updated pycuda for 2023

--- config/pycuda.sh
+++ config/pycuda.sh
@@ -1,2 +1,6 @@
-MODULE_BUILD_DEPS="gcc/9.3.0 cuda/11.4 boost"
+if [[ "$EBVERSIONGENTOO" == "2023" ]]; then
+    MODULE_BUILD_DEPS="gcc cuda/12.2 boost"
+else
+    MODULE_BUILD_DEPS="gcc/9.3.0 cuda/11.4 boost"
+fi
 PRE_BUILD_COMMANDS="sed -i '/\"mako\",/a \"numpy\"' setup.py"
