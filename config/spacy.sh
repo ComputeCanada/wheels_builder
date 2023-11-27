c8593820f02ebe98a09787272dc84687d11cd787
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-11-27 08:46:57 -0500

    Updated spacy for 2023

--- config/spacy.sh
+++ config/spacy.sh
@@ -1,1 +1,8 @@
-PYTHON_DEPS="cymem>=2.0.2,<2.1.0 preshed>=3.0.2,<3.1.0 murmurhash>=0.28.0,<1.1.0 thinc>=8.1.8,<8.2.0 blis>=0.4.0,<0.8.0 pathy numpy>=1.15.0" 
+PYTHON_DEPS="
+    cymem>=2.0.2,<2.1.0
+    preshed>=3.0.2,<3.1.0
+    murmurhash>=0.28.0,<1.1.0
+    thinc>=8.1.8,<8.3.0
+    pathy
+    numpy>=1.25.0;python_version>='3.9'
+"
