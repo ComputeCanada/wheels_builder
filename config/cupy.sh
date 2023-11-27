2edd8d637aab621cd4464f308a9c305c139e4da2
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-11-27 08:51:15 -0500

    Updated cupy for 2023

--- config/cupy.sh
+++ config/cupy.sh
@@ -1,4 +1,9 @@
 PYTHON_DEPS="optuna"
-MODULE_BUILD_DEPS="gcc/9.3.0 cuda/11.4 cudnn nccl cutensor"
+# TODO: check if cusparselt works
+if [[ "$EBVERSIONGENTOO" == "2023" ]]; then
+	MODULE_BUILD_DEPS="cuda/12.2 cudnn nccl cutensor/1.7"
+else
+    MODULE_BUILD_DEPS="gcc/9.3.0 cuda/11.4 cudnn nccl cutensor"
+fi
 # needed otherwise it does not find libcuda.so
 PRE_DOWNLOAD_COMMANDS='export LDFLAGS="-L$EBROOTCUDA/lib64/stubs -L$EBROOTNCCL/lib" ; export CFLAGS="-I$EBROOTNCCL/include/"'
