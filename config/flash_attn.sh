f7df94bbc636c63c178b33c2e756a7f54bdd5106
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-11-27 08:52:57 -0500

    Updated flash_attn for 2023

--- config/flash_attn.sh
+++ config/flash_attn.sh
@@ -3,10 +3,14 @@ PRE_BUILD_COMMANDS="
 	export TORCH_CUDA_ARCH_LIST='6.0;7.0;7.5;8.0;8.6';
 	export MAX_JOBS=2
 "
-# torch 2.0.1 uses 11.7, but previous versions uses 11.4
-if [[ "$TORCH_VERSION" =~ "2." ]]; then
-	MODULE_BUILD_DEPS="gcc/9.3.0 cmake cuda/11.7"
+if [[ "$EBVERSIONGENTOO" == "2023" ]]; then
+	MODULE_BUILD_DEPS="gcc cmake cuda/12.2"
 else
-	MODULE_BUILD_DEPS="gcc/9.3.0 cmake cuda/11.4"
+	# torch 2.0.1 uses 11.7, but previous versions uses 11.4
+	if [[ "$TORCH_VERSION" =~ "2." ]]; then
+		MODULE_BUILD_DEPS="gcc/9.3.0 cmake cuda/11.7"
+	else
+		MODULE_BUILD_DEPS="gcc/9.3.0 cmake cuda/11.4"
+	fi
 fi
 PYTHON_TESTS="from flash_attn import flash_attn_qkvpacked_func, flash_attn_func"
