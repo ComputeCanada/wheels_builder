9007a51f2978f696206de659e8496c3839569b25
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-11-27 08:52:22 -0500

    Updated mmdet and mmcv for 2023

--- config/mmcv.sh
+++ config/mmcv.sh
@@ -1,5 +1,9 @@
 PYTHON_DEPS="torch>=1.10.0 ninja rich termcolor"
-MODULE_BUILD_DEPS="cuda/11.4"
+if [[ "$EBVERSIONGENTOO" == "2023" ]]; then
+    MODULE_BUILD_DEPS="cuda/12.2"
+else
+    MODULE_BUILD_DEPS="cuda/11.4"
+fi
 MODULE_RUNTIME_DEPS='opencv'
 PRE_BUILD_COMMANDS="export MMCV_WITH_OPS=1; export FORCE_CUDA=1; export TORCH_CUDA_ARCH_LIST='6.0;7.0;7.5;8.0;8.6'; export MAX_JOBS=4;"
 PYTHON_IMPORT_NAME='mmcv'
--- config/mmdet.sh
+++ config/mmdet.sh
@@ -1,4 +1,8 @@
 MODULE_RUNTIME_DEPS="opencv"
-MODULE_BUILD_DEPS="cuda/11.4"
+if [[ "$EBVERSIONGENTOO" == "2023" ]]; then
+    MODULE_BUILD_DEPS="cuda/12.2"
+else
+    MODULE_BUILD_DEPS="cuda/11.4"
+fi
 PRE_BUILD_COMMANDS="export FORCE_CUDA=1;"
-PYTHON_DEPS="torch>=1.10.0"
+PYTHON_DEPS="torch>=1.10.0 mmcv rich termcolor"
