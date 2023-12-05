2e3879b4026e058a90a3f08726040bf0872bdb94
Oliver Stueker <oliver.stueker@ace-net.ca>
2023-12-05 15:02:20 -0330

    update config/rasterio.sh for StdEnv/2023

--- config/rasterio.sh
+++ config/rasterio.sh
@@ -1,2 +1,11 @@
+if [[ "$EBVERSIONGENTOO" -eq "2020" ]] ; then
+    MODULE_BUILD_DEPS="gcc/9.3.0 gdal"
+elif [[ "$EBVERSIONGENTOO" -eq "2023" ]] ; then
+    MODULE_BUILD_DEPS="gcc/12.3 gdal/3.7.2"
+else
+    MODULE_BUILD_DEPS="gcc gdal"
+fi
+
 PYTHON_DEPS=" affine snuggs cligj click-plugins enum34"
-MODULE_BUILD_DEPS="gcc/9.3.0 gdal/3.2.3"
+
+
