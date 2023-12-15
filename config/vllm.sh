61f286109e62aa7b1db73093b549d44087cdc065
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-12-15 17:10:24 -0500

    Added vllm config

---
+++ config/vllm.sh
@@ -0,0 +1,11 @@
+# Use GH sources, https://github.com/vllm-project/vllm/issues/1922
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/vllm-project/vllm/archive/refs/tags/v${VERSION:?version required}.tar.gz"
+MODULE_RUNTIME_DEPS='arrow'
+MODULE_BUILD_DEPS='cuda'
+PYTHON_DEPS='torch>=2.1.0'
+# nvcc uses os.cpu_count() hardcoded threads which returns 16
+PRE_BUILD_COMMANDS='
+	export TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6";
+	export MAX_JOBS=1;
+	sed -i -e "0,/MAIN_CUDA_VERSION/{s/MAIN_CUDA_VERSION.*/MAIN_CUDA_VERSION=\"$EBVERSIONCUDA\"/}" setup.py;
+'
