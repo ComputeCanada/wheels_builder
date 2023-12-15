a2a7a721caebe3082467e94264c53d1c6f6f2b4a
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-12-15 17:11:18 -0500

    Added acids-rave config

---
+++ config/acids-rave.sh
@@ -0,0 +1,3 @@
+PYTHON_DEPS='torch==1.13.1 llvmlite==0.41.0 --find-links=/cvmfs/soft.computecanada.ca/custom/python/wheelhouse/gentoo/avx2/'
+UPDATE_REQUIREMENTS="'scipy~=1.10.0'"
+PYTHON_IMPORT_NAME='rave'
