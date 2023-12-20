821798682c046701d8da67b28720dc2737ed5822
Bart Oldeman <bart.oldeman@calculquebec.ca>
2023-12-20 15:19:21 +0000

    Build OpenEXR wheels on 2023 without needing the module.

--- config/OpenEXR.sh
+++ config/OpenEXR.sh
@@ -1,2 +1,6 @@
-MODULE_BUILD_DEPS='openexr'
+if [[ "$EBVERSIONGENTOO" == "2023" ]]; then
+	PRE_BUILD_COMMANDS="CPATH=$CPATH:$EBROOTGENTOO/include/OpenEXR:$EBROOTGENTOO/include/Imath "
+else
+	MODULE_BUILD_DEPS='openexr'
+fi
 PATCHES='OpenEXR-fix-lib.patch'
