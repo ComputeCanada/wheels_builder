d067b4d4117e9f678c5a0f6ecedb0176b5864d98
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-01-18 16:37:43 -0500

    Added torchsparse config

---
+++ config/torchsparse.sh
@@ -0,0 +1,8 @@
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/mit-han-lab/torchsparse/archive/refs/tags/v${VERSION:?version required}.tar.gz"
+PRE_BUILD_COMMANDS='
+	export TORCH_CUDA_ARCH_LIST="6.0;7.0;7.5;8.0;8.6";
+	export FORCE_CUDA=1;
+	sed -i -e "s/__version__.*/__version__ = \"$VERSION\"/" torchsparse/version.py
+'
+PYTHON_DEPS="torch${TORCH_VERSION:+==$TORCH_VERSION} tqdm"
+MODULE_BUILD_DEPS='sparsehash cuda'
