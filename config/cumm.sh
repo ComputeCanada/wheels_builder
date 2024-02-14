9f72f58b79330b848256e154f94c39769f2f4b4a
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-02-14 09:09:01 -0500

    Updated cumm config for 2023

--- config/cumm.sh
+++ config/cumm.sh
@@ -1,6 +1,10 @@
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/FindDefinition/cumm/archive/refs/tags/v${VERSION:?version required}.tar.gz"
 PYTHON_DEPS="pccm>=0.4.2"
-MODULE_BUILD_DEPS='cuda/11.4'
+if [[ "$EBVERSIONGENTOO" == "2023" ]]; then
+	MODULE_BUILD_DEPS='cuda/12'
+else
+	MODULE_BUILD_DEPS='cuda/11.7'
+fi
 PRE_BUILD_COMMANDS="
 	export CUMM_CUDA_ARCH_LIST='6.0;7.0;7.5;8.0;8.6';
 	export CUMM_DISABLE_JIT="1";
