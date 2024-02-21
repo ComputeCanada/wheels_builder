f5362030b8e54bae398ffaff0177ab508fdeedea
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-02-21 10:13:56 -0500

    Updated openslide-python for 2023 and latest version

--- config/openslide-python.sh
+++ config/openslide-python.sh
@@ -1,3 +1,8 @@
 MODULE_BUILD_DEPS="openslide"
-PRE_BUILD_COMMANDS='sed -i -e "/import platform/a import os" -e "s;.libopenslide.so.0.;os.environ.get(\"EBROOTOPENSLIDE\",\"$EBROOTOPENSLIDE\") + \"/lib/libopenslide.so.0\";g" $(find . -name "lowlevel.py")'
+# Match `'libopenslide.so.[0-9]` to the actual library path, by inserting the path
+# to the library from the environment variable `EBROOTOPENSLIDE` before the library name.
+# or use the path from the current loaded module.
+PRE_BUILD_COMMANDS='
+    sed -i -e "/import platform/a import os" -e "s;[[:punct:]]libopenslide.so.[0-9];os.environ.get(\"EBROOTOPENSLIDE\",\"$EBROOTOPENSLIDE\") + \"/lib/\" + &;g" openslide/lowlevel.py
+'
 PYTHON_IMPORT_NAME='openslide'
