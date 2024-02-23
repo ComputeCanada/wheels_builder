b1c781643c3b56caa8304a7e22eec44e31a4d83b
Helene Gingras <helene.gingras@calculquebec.ca>
2024-02-23 18:03:16 +0000

    Adding python requirement to setup.py

--- config/anvio.sh
+++ config/anvio.sh
@@ -2,7 +2,7 @@ PACKAGE_DOWNLOAD_ARGUMENT=https://github.com/merenlab/anvio/archive/refs/tags/v$
 PRE_BUILD_COMMANDS=$(cat <<-END
         sed -i -e 's/pandas==1.4.4/pandas~=1.4.1/' requirements.txt;
         sed -i -e 's/scikit-learn==1.2.2/scikit-learn~=1.2.1/' requirements.txt;
-        python_requires="==3.10";
+        sed -i -e '29i \  \  python_requires="==3.10",' setup.py;
 END
 ) # This must stay on a separate line!
 
