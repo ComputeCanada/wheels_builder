6cffd2d02a8c505c0d37269940c86ebeaa9c07c3
Helene Gingras <helene.gingras@calculquebec.ca>
2024-02-23 13:09:50 +0000

    Removed numpy requirement

--- config/anvio.sh
+++ config/anvio.sh
@@ -2,7 +2,8 @@ PACKAGE_DOWNLOAD_ARGUMENT=https://github.com/merenlab/anvio/archive/refs/tags/v$
 PRE_BUILD_COMMANDS=$(cat <<-END
         sed -i -e 's/pandas==1.4.4/pandas~=1.4.1/' requirements.txt;
         sed -i -e 's/scikit-learn==1.2.2/scikit-learn~=1.2.1/' requirements.txt;
-        sed -i -e 's/numpy<=1.24/numpy~=1.24/' requirements.txt;
         python_requires="==3.10";
 END
 ) # This must stay on a separate line!
+
+
