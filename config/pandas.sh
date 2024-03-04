a418c57be5975d5e2e67a1ee8273de2f6101e5ed
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-03-04 07:38:20 -0500

    Updated pandas config. Avoid double building

--- config/pandas.sh
+++ config/pandas.sh
@@ -3,7 +3,8 @@ if [[ ${VERSION} =~ 2.0.* ]]; then
 	PYTHON_DEPS='versioneer numpy==1.21;python_version=="3.9" meson-python'
 else
 #elif [[ ${VERSION} =~ 2.1.* ]]; then
-	PYTHON_DEPS='versioneer numpy~=1.22.4;python_version=="3.9" numpy~=1.22.4;python_version=="3.10" meson-python'
+	PYTHON_DEPS='versioneer numpy~=1.22.4;python_version=="3.9" numpy~=1.22.4;python_version=="3.10" meson-python Cython~=3.0.5'
 fi
 PRE_BUILD_COMMANDS="python setup.py clean --all"
-
+# Avoid double build because `pip download` stupidity
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/pandas-dev/pandas/archive/refs/tags/v${VERSION:?version required}.tar.gz"
