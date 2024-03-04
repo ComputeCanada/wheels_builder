6e53646b563ee0b89e572340ed3a2debfd4aebcd
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-03-04 07:37:29 -0500

    Updated beir config

--- config/beir.sh
+++ config/beir.sh
@@ -1,2 +1,2 @@
-MODULE_RUNTIME_DEPS="cuda faiss"
+MODULE_RUNTIME_DEPS="cuda/11.4 faiss arrow"
 PATCH_WHEEL_COMMANDS="$SCRIPT_DIR/manipulate_wheels.py -v --inplace --force -w \$ARCHNAME -u 'faiss-cpu->faiss'"
