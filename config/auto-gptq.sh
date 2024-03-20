693563afd14a7934411f7ace40f765fe745c7c8b
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-03-20 12:04:19 -0400

    Added auto-gptq config

---
+++ config/auto-gptq.sh
@@ -0,0 +1,10 @@
+PYTHON_DEPS="torch${TORCH_VERSION:+==$TORCH_VERSION}"
+MODULE_BUILD_DEPS="cuda/12"
+MODULE_RUNTIME_DEPS="arrow"
+PRE_BUILD_COMMANDS="
+    export TORCH_CUDA_ARCH_LIST='6.0;7.0;7.5;8.0;8.6';
+    export BUILD_CUDA_EXT=1;
+    export MAX_JOBS=8;
+    export PYPI_RELEASE=1;
+"
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/AutoGPTQ/AutoGPTQ/archive/refs/tags/v${VERSION:?version required}.tar.gz"
