202bf9fa4496aacc771c1f624921cdc083304876
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-03-20 12:02:26 -0400

    Updated lib_pod5 for 2023

--- config/lib_pod5.sh
+++ config/lib_pod5.sh
@@ -1,5 +1,11 @@
-MODULE_BUILD_DEPS="arrow/8 cmake boost flatbuffers thrift"
-MODULE_RUNTIME_DEPS="arrow/8"
+MODULE_BUILD_DEPS="cmake boost flatbuffers thrift"
+if [[ "$EBVERSIONGENTOO" == "2023" ]]; then
+	MODULE_BUILD_DEPS="arrow/14 $MODULE_BUILD_DEPS"
+	MODULE_RUNTIME_DEPS="arrow/14"
+else
+	MODULE_BUILD_DEPS="arrow/8 $MODULE_BUILD_DEPS"
+	MODULE_RUNTIME_DEPS="arrow/8"
+fi
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/nanoporetech/pod5-file-format.git"
 PACKAGE_DOWNLOAD_NAME="$PACKAGE-$VERSION.tar.gz"
 PACKAGE_DOWNLOAD_METHOD="Git"
@@ -11,7 +17,7 @@ PYTHON_DEPS="build setuptools setuptools_scm[toml]"
 PRE_BUILD_COMMANDS="
 	python -m build . -x -n -s &&
 	python pod5_make_version.py &&
-	cmake -B build . -DPOD5_BUILD_EXAMPLES=OFF &&
+	cmake -B build . -DPOD5_BUILD_EXAMPLES=OFF -DARROW_HOME=$EBROOTARROW &&
 	cmake --build build -- -j 4 &&
 	cd python/lib_pod5
 "
