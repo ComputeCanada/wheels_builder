d0328ad1b69f93aa8459607a783087bf0f581d99
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-04-03 16:34:48 -0400

    Updated pyscf for 2023

--- config/pyscf.sh
+++ config/pyscf.sh
@@ -1,3 +1,7 @@
-MODULE_BUILD_DEPS="flexiblas libxc/5.1.3 qcint"
+MODULE_BUILD_DEPS="flexiblas libxc/6 qcint/6"
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/pyscf/pyscf/archive/refs/tags/v${VERSION:?version required}.zip"
-PRE_BUILD_COMMANDS='export VERBOSE=1; export CMAKE_CONFIGURE_ARGS="-DBLA_VENDOR=Flexiblas -DBLAS_LIBRARIES=-lflexiblas -DBUILD_LIBCINT=0 -DBUILD_LIBXC=0" '
+# Trick build into avx2 or avx512 since we patch march=native flag.
+PRE_BUILD_COMMANDS='
+	export VERBOSE=1;
+	export CMAKE_CONFIGURE_ARGS="-DBLA_VENDOR=Flexiblas -DBLAS_LIBRARIES=-lflexiblas -DBUILD_MARCH_NATIVE=1 -DBUILD_LIBCINT=0 -DBUILD_LIBXC=0 -DUSE_QCINT=1"
+'
