e15d71a2ac3887dd20cea2d2c4dbc53c58fcc7a7
Helene Gingras <helene.gingras@calculquebec.ca>
2024-04-09 19:55:37 +0000

    Adding restriction for python dependency

--- config/CITE-seq-Count.sh
+++ config/CITE-seq-Count.sh
@@ -1,1 +1,5 @@
-PRE_BUILD_COMMANDS="sed -i -e 's/pytest==4.1.0/pytest~=4.1.0/' setup.py"
+PRE_BUILD_COMMANDS=$(cat <<-END
+        sed -i -e 's/pytest==4.1.0/pytest~=4.1.0/' setup.py;
+        sed -i -e 's/python_requires=">=3.6"/python_requires=">=3.6, >=3.10.13"/' setup.py;
+END
+) # This must stay on a separate line!
