6580dd78cd124b8ba78f3b0f19197d39f27e5a9a
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-04-18 11:47:20 -0400

    Updated qutip for 2023

--- config/qutip.sh
+++ config/qutip.sh
@@ -1,4 +1,2 @@
 # Latest scipy forces numpy 1.24 and for python 3.9 this fails so force numpy 1.21
 PYTHON_DEPS="scipy matplotlib numpy~=1.21.0;python_version<'3.10' packaging"
-PRE_BUILD_COMMANDS='rm -v pyproject.toml; python setup.py clean --all;' # force using setup.py to pass arguments
-BDIST_WHEEL_ARGS='--with-openmp'
