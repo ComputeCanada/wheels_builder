e39bd9c7cba31562d444d6c0cbc7c0d3d3ce533d
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-04-23 15:55:59 -0400

    Fixed and updated config for vllm 0.4.1. Ensure ops imports as well.

--- config/vllm.sh
+++ config/vllm.sh
@@ -1,12 +1,14 @@
 # Use GH sources, https://github.com/vllm-project/vllm/issues/1922
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/vllm-project/vllm/archive/refs/tags/v${VERSION:?version required}.tar.gz"
 MODULE_RUNTIME_DEPS='arrow'
-MODULE_BUILD_DEPS='cuda protobuf abseil'
-PYTHON_DEPS='torch==2.1.2'
+MODULE_BUILD_DEPS='cuda protobuf abseil nccl'
+PYTHON_DEPS='torch==2.2.1'
 # nvcc uses os.cpu_count() hardcoded threads which returns 16
 PRE_BUILD_COMMANDS='
 	export TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6;9.0";
-	export MAX_JOBS=1;
+	export MAX_JOBS=${SLURM_CPUS_PER_TASK:-1};
 	sed -i -e "0,/MAIN_CUDA_VERSION/{s/MAIN_CUDA_VERSION.*/MAIN_CUDA_VERSION=\"$EBVERSIONCUDA\"/}" setup.py;
-	sed -i -e "s/cupy-cuda12x/cupy/" requirements.txt;
+	sed -i -e "s/cupy-cuda12x/cupy/" -e "/vllm-nccl-.*/d" -e "/^cmake/d" -e "$ a triton" requirements*.txt;
+	rm -f requirements-build.txt;
 '
+PYTHON_TESTS='from vllm._C import ops'
