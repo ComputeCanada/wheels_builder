fc9a602c9a59dc9270e3093bd92fc950d7c8cbd9
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-06-10 14:05:11 -0400

    Updated parasail config for 2023. Now included in the module.

--- config/parasail.sh
+++ config/parasail.sh
@@ -1,2 +1,8 @@
-MODULE_RUNTIME_DEPS="parasail"
-PRE_BUILD_COMMANDS="export PARASAIL_SKIP_BUILD=1"
+if [[ "$EBVERSIONGENTOO" == "2023" ]]; then
+	    echo "Parasail python wheel is now bundled under the module of the same name since it depends on it at runtime."
+        echo "use with : module load parasail"
+        exit 1;
+else
+	MODULE_RUNTIME_DEPS="parasail"
+	PRE_BUILD_COMMANDS="export PARASAIL_SKIP_BUILD=1"
+fi
