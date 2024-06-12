23606cc69873002a3bdb1e9d7ad98b5768523b6f
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-06-12 09:34:51 -0400

    Run scipy test suite faster

--- config/scipy.sh
+++ config/scipy.sh
@@ -8,7 +8,7 @@ POST_DOWNLOAD_COMMANDS="tar -zcf ${PACKAGE}-${VERSION}.tar.gz $PACKAGE_FOLDER_NA
 
 MODULE_BUILD_DEPS="flexiblas cmake"
 PYTHON_DEPS_DEFAULT=""
-PYTHON_DEPS=" pythran meson-python pooch hypothesis"
+PYTHON_DEPS=" pythran meson-python pooch hypothesis pytest-xdist"
 if [[ ${VERSION} =~ 1.10.* ]]; then
 	PYTHON_DEPS="$PYTHON_DEPS numpy==1.19.5;python_version<'3.10' "
 fi
@@ -16,5 +16,5 @@ fi
 # Test test_x0_equals_Mb fail on avx512 capable processor:
 # https://github.com/scipy/scipy/issues/17075#issuecomment-1255594078
 # https://github.com/scipy/scipy/issues/15533
-PYTHON_TESTS="scipy.__config__.show(); scipy.test()"
+PYTHON_TESTS="scipy.__config__.show(); scipy.test(parallel=${SLURM_CPUS_PER_TASK:-1})"
 PRE_BUILD_COMMANDS="sed -i -e 's/openblas/flexiblas/' meson.build"
