b4d768e02464d0aabd0e67677a75617cfeac4dd7
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-06-14 06:51:27 -0400

    Updated qiskit-aer config to build correctly

--- config/qiskit-aer.sh
+++ config/qiskit-aer.sh
@@ -1,18 +1,12 @@
-MODULE_BUILD_DEPS="scipy-stack" #for dateutil
-MODULE_RUNTIME_DEPS="
-    symengine/0.9.0
-    flexiblas/3.0.4
-    openmpi/4.0.3
-    cuda/11.4
-"
-PYTHON_DEPS="cmake"
-PRE_BUILD_COMMANDS="
-    export CMAKE_ARGS=\"
-        -DBLA_VENDOR=FlexiBLAS
-        -DAER_THRUST_BACKEND=CUDA
-        -DAER_CUDA_ARCH='6.0;7.0;7.5;8.0;8.6;8.6+PTX'
-        -DAER_MPI=True
-        -DDISABLE_CONAN=OFF
-        -DSymEngine_DIR=$EBROOTSYMENGINE/lib/cmake/symengine/
-    \"
-"
+# could use cuQuantum but a pain to install
+MODULE_RUNTIME_DEPS="symengine flexiblas openmpi cuda/12 spdlog muparserx"
+PRE_BUILD_COMMANDS='rm -f pyproject.toml'
+BDIST_WHEEL_ARGS='
+    -DBLA_VENDOR=FlexiBLAS
+    -DDISABLE_CONAN=ON
+    -DAER_MPI=ON
+    -DAER_CUDA_ARCH="6.0;7.0;7.5;8.0;8.6;8.6+PTX"
+    -DAER_THRUST_BACKEND=CUDA
+    -DCUDA_NVCC_FLAGS="-D_AVX512BF16VLINTRIN_H_INCLUDED -D_AVX512BF16INTRIN_H_INCLUDED"
+'
+PYTHON_IMPORT_NAME='qiskit.Aer'
