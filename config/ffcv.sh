1bb633ea740a49356447d0af40d54f1e49420aa4
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-06-14 06:45:00 -0400

    Updated ffcv for 2023

--- config/ffcv.sh
+++ config/ffcv.sh
@@ -1,2 +1,7 @@
-MODULE_RUNTIME_DEPS="cuda/11.4 opencv/4.5.5"
+if [[ "$EBVERSIONGENTOO" == "2023" ]]; then
+	MODULE_BUILD_DEPS="cuda/12"
+	MODULE_RUNTIME_DEPS='opencv'
+else
+	MODULE_RUNTIME_DEPS="cuda/11.4 opencv/4.5.5"
+fi
 PYTHON_IMPORT_DEPS="torch cupy numba"
