11b73416908170e54ce6d5f317540ec34e079e04
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-06-14 06:34:10 -0400

    Updated editdistance config

--- config/editdistance.sh
+++ config/editdistance.sh
@@ -1,1 +1,3 @@
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/roy-ht/editdistance/archive/refs/tags/v${VERSION:?version required}.tar.gz"
+RECURSIVE=0
+PYTHON_DEPS='pdm pdm.backend' # python build deps, yet another package manager
