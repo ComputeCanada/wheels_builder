601f26e4d52b64d53e8ee79d11db422ac7ba1d42
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-06-20 12:19:06 -0400

    Updated gpaw config.
    blacs is integrated with scalapack and any libxc can be used.

--- config/gpaw.sh
+++ config/gpaw.sh
@@ -1,6 +1,6 @@
 # current GPAW (22.8.0) requires LibXC 3.x or 4.x or 5.1+.  Using module libxc/5.1.3
 # https://wiki.fysik.dtu.dk/gpaw/install.html#requirements
-MODULE_BUILD_DEPS="gcc openmpi flexiblas blacs scalapack fftw libxc/5.2.3"
+MODULE_BUILD_DEPS="gcc openmpi flexiblas scalapack fftw libxc"
 # Need a siteconfig.py for installation settings. Storing it within wheels_builder repo.
 # That file contains a list of libraries to link as well as compiler-/linker flags.
 # See also: https://wiki.fysik.dtu.dk/gpaw/install.html#customizing-installation
