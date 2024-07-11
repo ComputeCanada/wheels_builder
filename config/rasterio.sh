aa7f1c6db3ca8532949e1e766d88f5a5b7c4c257
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-07-11 08:28:23 -0400

    Updated rasterio config to use available gdal

--- config/rasterio.sh
+++ config/rasterio.sh
@@ -1,7 +1,7 @@
 if [[ "$EBVERSIONGENTOO" -eq "2020" ]] ; then
     MODULE_BUILD_DEPS="gcc/9.3.0 gdal"
 elif [[ "$EBVERSIONGENTOO" -eq "2023" ]] ; then
-    MODULE_BUILD_DEPS="gcc/12.3 gdal/3.7.2"
+    MODULE_BUILD_DEPS="gcc/12.3 gdal"
 else
     MODULE_BUILD_DEPS="gcc gdal"
 fi
