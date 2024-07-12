74ea593a4b4cee1eb16130d792e84be54de23871
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-07-12 10:37:13 -0400

    Updated pennylane-ligthning to specify backend

--- config/pennylane-lightning.sh
+++ config/pennylane-lightning.sh
@@ -1,13 +1,15 @@
 MODULE_BUILD_DEPS="arch/avx2 cmake flexiblas"
 PYTHON_DEPS="pybind11 scipy"
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/PennyLaneAI/pennylane-lightning/archive/refs/tags/v${VERSION:?version required}.tar.gz"
-PRE_BUILD_COMMANDS="python setup.py build_ext --define='
-	ENABLE_NATIVE=OFF;
-	ENABLE_BLAS=ON;
-	ENABLE_OPENMP=ON;
-	BLA_VENDOR=FlexiBLAS;
-	ENABLE_AVX=ON;
-	ENABLE_AVX2=ON;
-	ENABLE_LAPACK=ON'
+PRE_BUILD_COMMANDS="
+	export PL_BACKEND='lightning_qubit';
+	python setup.py build_ext --define='
+		ENABLE_NATIVE=OFF;
+		ENABLE_BLAS=ON;
+		ENABLE_OPENMP=ON;
+		BLA_VENDOR=FlexiBLAS;
+		ENABLE_AVX=ON;
+		ENABLE_AVX2=ON;
+		ENABLE_LAPACK=ON'
 "
 PATCHES='pennylane_lightning-fix_blas.patch'
