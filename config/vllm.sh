397dbcd9c9aa7b2c8b0721c5a3729fd405931c16
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-07-25 07:33:49 -0400

    Updated vllm config

--- config/vllm.sh
+++ config/vllm.sh
@@ -2,7 +2,7 @@
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/vllm-project/vllm/archive/refs/tags/v${VERSION:?version required}.tar.gz"
 MODULE_RUNTIME_DEPS='arrow'
 MODULE_BUILD_DEPS='cuda protobuf abseil nccl'
-PYTHON_DEPS='torch==2.2.1'
+PYTHON_DEPS='torch==2.3.1'
 # nvcc uses os.cpu_count() hardcoded threads which returns 16
 PRE_BUILD_COMMANDS='
 	export TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6;9.0";
@@ -11,4 +11,4 @@ PRE_BUILD_COMMANDS='
 	sed -i -e "s/cupy-cuda12x/cupy/" -e "/vllm-nccl-.*/d" -e "/^cmake/d" -e "$ a triton" requirements*.txt;
 	rm -f requirements-build.txt;
 '
-PYTHON_TESTS='from vllm._C import ops'
+PYTHON_TESTS='from vllm._C import *'
