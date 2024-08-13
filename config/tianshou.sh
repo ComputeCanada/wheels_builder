a13c18a43d73f20a565e81ef7416452bf69e0d81
Helene Gingras <helene.gingras@calculquebec.ca>
2024-08-13 12:11:32 +0000

    Adding module name to tianshou.sh

--- config/tianshou.sh
+++ config/tianshou.sh
@@ -1,2 +1,2 @@
 PYTHON_VERSIONS="python/3.11"
-PRE_BUILD_COMMANDS='sed -i -E "s/\^0\.57\.1/\^0\.60\.0/" pyproject.toml'
+PRE_BUILD_COMMANDS="sed -i -e 's/numba\s=\s\"\^0\.57\.1\"/numba = \"\^0\.60\.0\"/' pyproject.toml"
