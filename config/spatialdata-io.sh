9e0274d82376f8fd7dbfe606c66cadc478de2f3f
Collin Wilson <collin.wilson@sharcnet.ca>
2024-08-23 12:42:33 +0000

    added configs for spatialdata, spatialdata-io, spatialdata-plot

---
+++ config/spatialdata-io.sh
@@ -0,0 +1,3 @@
+MODULE_RUNTIME_DEPS="gcc/12.3 h5py/3.6.0 arrow/17.0.0 scipy-stack/2023b proj/9.4.1"
+#PYTHON_DEPS="xarray==2024.5.0 dask==2024.5.1 distributed==2024.5.1 dask_image==2021.12.0 anndata==0.10.7 geopandas==0.14.4 s3fs==2021.6.1 imageio==2.34.2 param==1.13.0 array_api_compat==1.6 colorcet==3.0.1 fsspec==2024.6.1"
+PYTHON_DEPS="dask_image==2021.12.0 anndata==0.10.7"
