1a20a38c72596520ddb65ed87fdda82f4e2a75f6
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-10-04 11:17:33 -0400

    Added graph-attention-student config

---
+++ config/graph-attention-student.sh
@@ -0,0 +1,4 @@
+MODULE_RUNTIME_DEPS='openbabel rdkit'
+MODULE_BUILD_DEPS='cython/.0.29.36'
+# per https://github.com/aimat-lab/graph_attention_student/issues/3 relax TF requirement
+UPDATE_REQUIREMENTS='tensorflow'
