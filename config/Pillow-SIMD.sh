8de5a85cba4fe2c4fc73bfb020779064ca878c4d
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-10-04 11:15:42 -0400

    Updated pillow-simd config
    Remove hardcoded sse instruction

--- config/Pillow-SIMD.sh
+++ config/Pillow-SIMD.sh
@@ -2,8 +2,7 @@ if [[ -z "$EBROOTGENTOO" ]]; then
 	PRE_BUILD_COMMANDS="export CC=\"cc -mavx2\""
 else
 	PRE_BUILD_COMMANDS='
-		export CC="cc -mavx2";
-		sed -i -e "s;/sbin/ldconfig;ldconfig;g" setup.py;
+		sed -i -e "s;\"-msse4\";;" setup.py;
 		CPATH=$EBROOTGENTOO/include:$CPATH;
 		LIBRARY_PATH=$EBROOTGENTOO/lib:$LIBRARY_PATH;
 		python setup.py build_ext --inplace --enable-raqm;
@@ -13,3 +12,5 @@ PYTHON_IMPORT_NAME="PIL"
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/uploadcare/pillow-simd/archive/refs/tags/v${VERSION:?version required}.tar.gz"
 MODULE_BUILD_DEPS="arch/avx2 raqm"
 TEST_COMMAND="python -c 'import PIL; from PIL import features; assert features.check(\"freetype2\"); assert features.check(\"raqm\")'"
+
+#
