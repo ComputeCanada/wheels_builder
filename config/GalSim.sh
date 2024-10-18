d4731c558226b84dd3a22217359abe8117ca9868
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-10-18 16:34:48 -0400

    Fixed quoting in GalSim config

--- config/GalSim.sh
+++ config/GalSim.sh
@@ -1,8 +1,8 @@
 MODULE_BUILD_DEPS='fftw eigen'
-PRE_BUILD_COMMANDS="
+PRE_BUILD_COMMANDS='
 	export FFTW_DIR=$EBROOTFFTW;
 	export EIGEN_DIR=$EBROOTEIGEN;
-	sed -i -e '839,840d' setup.py
-"
+	sed -i -e "839,840d" setup.py
+'
 PYTHON_DEPS='setuptools<72'
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/GalSim-developers/GalSim/archive/refs/tags/v${VERSION:?version required}.tar.gz"
