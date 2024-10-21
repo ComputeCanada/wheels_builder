e453a5a5ae9fd8bfe6c0bdbd5fd2685f5f5d4459
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-10-21 10:27:29 -0400

    Updated gensim config. Must build with older numpy.

--- config/gensim.sh
+++ config/gensim.sh
@@ -1,2 +1,2 @@
-PYTHON_DEPS="numpy>=1.11.3 scipy>=0.18.1 six>=1.5.0 smart_open>=1.2.1"
-
+MODULE_BUILD_DEPS='cython/.0.29.36 oldest-supported-numpy/.2024a -numpy/.2.1.1'
+PYTHON_DEPS="numpy>=1.11.3 scipy<1.14.0 six>=1.5.0 smart_open>=1.2.1"
