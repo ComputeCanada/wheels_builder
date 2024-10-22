f331e56468ff74d896d22c9e2285985a6aba5fb3
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-10-22 11:18:07 -0400

    Added dbcan config

---
+++ config/dbcan.sh
@@ -0,0 +1,2 @@
+TEST_COMMAND='run_dbcan -h'
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/linnabrown/run_dbcan/releases/download/${VERSION:?version required}/dbcan-${VERSION:?version required}.tar.gz"
