c477821ff8f8717ef90d995193516fc39f0137a1
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-10-23 11:34:25 -0400

    Fixed Ray + S3 issues

--- config/ray.sh
+++ config/ray.sh
@@ -1,5 +1,11 @@
 # Unfortunately, Ray developers have turned to use the evil Bazel, and is now impossible to build.
 
+PATCH_WHEEL_COMMANDS="
+    unzip -q -o \$ARCHNAME ray/train/_internal/storage.py;
+    patch -p2 -i \$SCRIPT_DIR/patches/ray-fix_s3.patch
+    zip \$ARCHNAME ray/train/_internal/storage.py;
+"
+
 if [[ $THIS_SCRIPT == 'build_wheel.sh' ]]; then
         echo "Thanks Bazel..for nothing! Using the following to add ray wheels:"
         echo ""
