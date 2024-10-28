d21765b59cddf5ccc56ecf5096451edc8985970d
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-10-28 09:40:19 -0400

    Updated PySCF config with fftw

--- config/pyscf.sh
+++ config/pyscf.sh
@@ -1,7 +1,7 @@
-MODULE_BUILD_DEPS="flexiblas libxc/6 qcint/6"
+MODULE_BUILD_DEPS="flexiblas libxc/6 qcint/6 fftw"
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/pyscf/pyscf/archive/refs/tags/v${VERSION:?version required}.zip"
 # Trick build into avx2 or avx512 since we patch march=native flag.
 PRE_BUILD_COMMANDS='
 	export VERBOSE=1;
-	export CMAKE_CONFIGURE_ARGS="-DBLA_VENDOR=Flexiblas -DBLAS_LIBRARIES=-lflexiblas -DBUILD_MARCH_NATIVE=1 -DBUILD_LIBCINT=0 -DBUILD_LIBXC=0 -DUSE_QCINT=1"
+	export CMAKE_CONFIGURE_ARGS="-DBLA_VENDOR=Flexiblas -DBLAS_LIBRARIES=-lflexiblas -DBUILD_MARCH_NATIVE=1 -DBUILD_LIBCINT=0 -DBUILD_LIBXC=0 -DUSE_QCINT=1 -DENABLE_FFTW=1 -DBUILD_FFTW=0"
 '
