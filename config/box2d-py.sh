ffcfafb87cc3b8619dc27bdcf9671e04f6e9ebe0
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-11-12 08:33:42 -0500

    Fixed box2d-py. It is not pybox2d while the names are confusing.

--- config/box2d-py.sh
+++ config/box2d-py.sh
@@ -1,1 +1,3 @@
 PYTHON_IMPORT_NAME=Box2D
+MODULE_BUILD_DEPS="swig cython/.0.29.36"
+TEST_COMMAND='python -c "from Box2D import (b2CircleShape, b2FixtureDef, b2Random, b2Vec2); from Box2D.b2 import circleShape;"'
