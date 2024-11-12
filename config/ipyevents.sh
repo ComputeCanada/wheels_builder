e61f42f767bffeeb384c5aaa64e0cef0d8e2e964
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-11-12 08:44:00 -0500

    Updated ipyevents config for 2023

--- config/ipyevents.sh
+++ config/ipyevents.sh
@@ -1,3 +1,2 @@
-MODULE_RUNTIME_DEPS="StdEnv/2020 scipy-stack"
-MODULE_BUILD_DEPS="StdEnv/2020 scipy-stack nodejs/15.2.1"
+MODULE_BUILD_DEPS="nodejs"
 PYTHON_DEPS='npm branca yarn jupyter-packaging jupyterlab ipywidgets maturin'
