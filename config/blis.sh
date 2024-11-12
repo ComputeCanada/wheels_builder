1cc7c2395025eb4738609110e13974d77d414d1f
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-11-12 08:41:51 -0500

    Updated blis config

--- config/blis.sh
+++ config/blis.sh
@@ -1,8 +1,1 @@
-PYTHON_DEPS="  hypothesis wheel"
-if [[ "$RSNT_ARCH" == "sse3" ]]; then
-	PRE_BUILD_COMMANDS="export BLIS_ARCH='generic'"
-elif [[ "$RSNT_ARCH" == "avx2" ]]; then
-	PRE_BUILD_COMMANDS="export BLIS_ARCH='haswell'"
-elif [[ "$RSNT_ARCH" == "avx512" ]]; then
-	PRE_BUILD_COMMANDS="export BLIS_ARCH='skx'"
-fi
+PRE_BUILD_COMMANDS="export BLIS_ARCH='x86_64'"
