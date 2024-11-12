e45c0e0f394315f066a82670c3c103078dbac930
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-11-12 08:41:29 -0500

    Added thinc-8.2.5 and updated thinc config for latest

---
+++ config/thinc-8.2.5.sh
@@ -0,0 +1,8 @@
+PYTHON_DEPS="
+    cython>=0.25,<3.0
+    murmurhash>=1.0.2,<1.1.0
+    cymem>=2.0.2,<2.1.0
+    preshed>=3.0.2,<3.1.0
+    blis>=0.7.8,<0.8.0
+"
+MODULE_BUILD_DEPS='cython/.0.29.36'
--- config/thinc.sh
+++ config/thinc.sh
@@ -1,1 +1,8 @@
-PYTHON_DEPS="murmurhash>=1.0.2,<1.1.0 cymem>=2.0.2,<2.1.0 preshed>=3.0.2,<3.1.0 blis>=0.4.0,<0.8.0 numpy>=1.15.0"
+PYTHON_DEPS="
+	murmurhash>=1.0.2,<1.1.0
+	cymem>=2.0.2,<2.1.0
+	preshed>=3.0.2,<3.1.0
+	blis>=1.0.0,<1.1.0
+"
+MODULE_BUILD_DEPS='cython/.0.29.36'
+UPDATE_REQUIREMENTS='numpy>=2.0.1'
