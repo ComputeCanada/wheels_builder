20cf775dc775fae0b2d8dc8c14f715c1195eabc0
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-11-12 08:42:21 -0500

    Added spacy-3.7.5 and updated spacy config for latest

--- config/spacy-3.7.5.sh
+++ config/spacy-3.7.5.sh
@@ -3,6 +3,5 @@ PYTHON_DEPS="
     cymem>=2.0.2,<2.1.0
     preshed>=3.0.2,<3.1.0
     murmurhash>=0.28.0,<1.1.0
-    thinc>=8.3.0,<8.4.0
+    thinc>=8.2.2,<8.3.0
 "
-UPDATE_REQUIREMENTS='numpy>=2.0.1'
