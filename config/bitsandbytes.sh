40fd9cbcfcd69de4dd4bbd27f8ad7a9cc3c1a01d
Charles Coulombe <charles.coulombe@calculquebec.ca>
2024-12-16 06:48:39 -0500

    Updated bitsandbytes config with build for 12.2 and 12.6

--- config/bitsandbytes.sh
+++ config/bitsandbytes.sh
@@ -4,14 +4,17 @@ PACKAGE_DOWNLOAD_METHOD="Git"
 PACKAGE_DOWNLOAD_CMD="git clone --jobs 16 --recursive $PACKAGE_DOWNLOAD_ARGUMENT $PACKAGE_FOLDER_NAME && cd $PACKAGE_FOLDER_NAME && git checkout ${VERSION:?version required} && cd .."
 POST_DOWNLOAD_COMMANDS="tar -zcf ${PACKAGE}-${VERSION}.tar.gz $PACKAGE_FOLDER_NAME"
 
-MODULE_BUILD_DEPS='cuda/12'
+MODULE_BUILD_DEPS='cuda/12.2'
 MODULE_RUNTIME_DEPS='cuda/12'
 # Builds the differents SOs into the source directory
 PRE_BUILD_COMMANDS='
-	cmake -B _build -S . -DCOMPUTE_BACKEND=cpu;
-	cmake --build _build --parallel 16;
-	cmake -B _build -S . -DCOMPUTE_BACKEND=cuda -DCOMPUTE_CAPABILITY="60;70;75;80;86;90" -DNO_CUBLASLT=OFF;
-	cmake --build _build --parallel 16;
-	cmake -B _build -S . -DCOMPUTE_BACKEND=cuda -DCOMPUTE_CAPABILITY="60;70;75;80;86;90" -DNO_CUBLASLT=ON;
-	cmake --build _build --parallel 16;
+	cmake -G Ninja  -B _build_cpu -S . -DCOMPUTE_BACKEND=cpu -DCMAKE_BUILD_TYPE=Release;
+	cmake --build _build_cpu --parallel ${SLURM_CPUS_PER_TASK:-4} --config Release;
+
+	for cudaver in 12.2 12.6; do
+		module load cuda/$cudaver && cmake -G Ninja  -B _build-$EBVERSIONCUDACORE -S . -DCOMPUTE_BACKEND=cuda -DCOMPUTE_CAPABILITY="60;70;75;80;86;90" -DCMAKE_BUILD_TYPE=Release;
+		cmake --build _build-$EBVERSIONCUDACORE --parallel ${SLURM_CPUS_PER_TASK:-4} --config Release;
+	done
 '
+# pypi wheel is py3
+POST_BUILD_COMMANDS='wheel tags --remove --abi-tag=none --python-tag=py3 --platform-tag=linux_x86_64 $WHEEL_NAME && WHEEL_NAME=$(ls *.whl)'
