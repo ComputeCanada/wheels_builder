0b7a934aad6c96d77bec3ab5c70e9dd5ceb1145a
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-01-30 14:18:05 -0500

    Updated quast config. Use GH url

--- config/quast.sh
+++ config/quast.sh
@@ -1,2 +1,3 @@
 MODULE_RUNTIME_DEPS="gcc rust java boost perl"
 PYTHON_DEPS="simplejson setuptools-rust joblib path matplotlib"
+PACKAGE_DOWNLOAD_URL="https://github.com/ablab/quast/releases/download/quast_$VERSION/quast-${VERSION:?version required}.tar.gz"
