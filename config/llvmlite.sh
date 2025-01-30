6603aa6b268921c179ce8646d5cd422e6aaffb5a
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-01-30 08:18:05 -0500

    Updated numba and llvmlite for 0.61

--- config/llvmlite.sh
+++ config/llvmlite.sh
@@ -1,5 +1,5 @@
 if [[ "$EBVERSIONGENTOO" == "2023" ]]; then
-	MODULE_BUILD_DEPS="llvm/14 cuda"
+	MODULE_BUILD_DEPS="llvm cuda"
 else
 	MODULE_BUILD_DEPS="llvm/14 cuda/11.4 tbb"
 	PATCHES="llvmlite-0.28.0-fpic.patch"
--- config/numba.sh
+++ config/numba.sh
@@ -1,3 +1,2 @@
-PYTHON_DEPS=" enum34 llvmlite"
+PYTHON_DEPS="enum34 llvmlite"
 MODULE_RUNTIME_DEPS='arch/avx2' # llvmlite is now under avx2
-PYTHON_DEPS="numpy~=1.22.0; python_version=='3.11'"
