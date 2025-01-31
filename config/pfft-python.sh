36ac2f5f218c7860f85c846b87ba7b55a4f4301e
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-01-31 11:25:23 -0500

    Updated pfft-python config

--- config/pfft-python.sh
+++ config/pfft-python.sh
@@ -1,3 +1,4 @@
-MODULE_BUILD_DEPS="gcc openmpi/4.0.3 mpi4py/3.1.3"
-MODULE_RUNTIME_DEPS="gcc openmpi/4.0.3 mpi4py/3.1.3"
+MODULE_BUILD_DEPS="cython/.0.29.36"
+MODULE_RUNTIME_DEPS="gcc openmpi mpi4py"
 PYTHON_IMPORT_NAME="pfft"
+POST_BUILD_COMMANDS='$SCRIPT_DIR/manipulate_wheels.py --inplace --force --remove_req cython -w $WHEEL_NAME'
