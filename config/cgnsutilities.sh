c130782461869719850d8dceab92f4ee25dcd090
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-02-03 15:40:30 -0500

    Added cgnsutilities config

---
+++ config/cgnsutilities.sh
@@ -0,0 +1,12 @@
+# MACH-AERO cgnsutilities
+
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/mdolab/cgnsutilities/archive/refs/tags/v${VERSION:?version required}.tar.gz"
+MODULE_BUILD_DEPS='openmpi cgns'
+PRE_BUILD_COMMANDS='
+	cp config/defaults/config.LINUX_GFORTRAN.mk config/config.mk;
+	sed -i -e "s/-O2/-O2 -ftree-vectorize -march=x86-64-v3 -fno-math-errno/" config/config.mk;
+	export CGNS_HOME=$EBROOTCGNS;
+	make -j ;
+'
+# Fix tags
+POST_BUILD_COMMANDS='wheel tags --remove --abi-tag=none --python-tag=py3 --platform-tag=linux_x86_64 $WHEEL_NAME && WHEEL_NAME=$(ls *.whl)'
