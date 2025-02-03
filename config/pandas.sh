d6fb6ff4c1079d4920286ac5d6c0957d1ee1a753
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-02-03 10:28:36 -0500

    Updated pandas config

--- config/pandas.sh
+++ config/pandas.sh
@@ -1,4 +1,2 @@
 PYTHON_DEPS='versioneer meson-python cython>3.0.10'
-PRE_BUILD_COMMANDS="python setup.py clean --all"
-# Avoid double build because `pip download` stupidity
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/pandas-dev/pandas/archive/refs/tags/v${VERSION:?version required}.tar.gz"
