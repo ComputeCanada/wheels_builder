ee487c36a47b69b5e2bf8f8184fdf582ca575601
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-02-06 20:42:56 -0500

    Ensure lightgbm uses GPU

--- config/LightGBM.sh
+++ config/LightGBM.sh
@@ -2,5 +2,5 @@ MODULE_BUILD_DEPS='cuda boost eigen openmpi'
 PYTHON_DEPS='scikit_build_core pyproject_metadata'
 PRE_BUILD_COMMANDS='
 	sed -i "s#../../../external_libs/fmt/include/##" include/LightGBM/utils/common.h;
-	export CMAKE_ARGS="-DUSE_CUDA=1 -DUSE_MPI=ON -DBoost_NO_BOOST_CMAKE=ON";
+	export CMAKE_ARGS="-DUSE_CUDA=1 -DUSE_MPI=ON -DBoost_NO_BOOST_CMAKE=ON -DUSE_GPU=1";
 '
