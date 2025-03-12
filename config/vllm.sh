e7529dd335b178a4adbe7b55eb25e06f7df5439f
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-03-12 09:55:07 -0400

    Updated vllm config for v0.7.3

--- config/vllm.sh
+++ config/vllm.sh
@@ -7,15 +7,17 @@ POST_DOWNLOAD_COMMANDS="tar -zcf ${PACKAGE}-${VERSION}.tar.gz $PACKAGE_FOLDER_NA
 
 MODULE_RUNTIME_DEPS='arrow'
 MODULE_BUILD_DEPS='cuda protobuf abseil nccl'
-PYTHON_DEPS='torch==2.4.1'
+PYTHON_DEPS='torch==2.5.1'
 # nvcc uses os.cpu_count() hardcoded threads which returns 16
 PRE_BUILD_COMMANDS='
 	export SETUPTOOLS_SCM_PRETEND_VERSION=${VERSION:?version required};
 	export TORCH_CUDA_ARCH_LIST="7.0;7.5;8.0;8.6;9.0";
-	export MAX_JOBS=8;
+    export USE_SCCACHE=1;
+	export MAX_JOBS=2;
 	export NVCC_THREADS=2;
 	sed -i -e "0,/MAIN_CUDA_VERSION/{s/MAIN_CUDA_VERSION.*/MAIN_CUDA_VERSION=\"$EBVERSIONCUDA\"/}" setup.py;
-	sed -i -e "s/torchvision\s*==\s*0.19/torchvision~=0.19/" -e "s/2.4.0/2.4.1/" -e "s/cupy-cuda12x/cupy/" -e "/vllm-nccl-.*/d" -e "/^cmake/d" -e "$ a triton" requirements*.txt;
-	rm -f requirements-build.txt pyproject.toml;
+	sed -i -e "s/cupy-cuda12x/cupy/" -e "/vllm-nccl-.*/d" -e "/^cmake/d" -e "$ a triton" requirements*.txt;
+	rm -f requirements-build.txt;
 '
+POST_BUILD_COMMANDS='wheel tags --remove --abi-tag=abi3 --python-tag=cp38 --platform-tag=linux_x86_64 $WHEEL_NAME && WHEEL_NAME=$(ls *.whl)'
 PYTHON_TESTS='from vllm._C import *'
