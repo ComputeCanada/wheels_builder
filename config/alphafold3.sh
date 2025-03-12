006609957ffa181a22f03f4d0cae2b9d8f67ea1a
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-03-12 09:50:36 -0400

    Updated AF3 config

--- config/alphafold3.sh
+++ config/alphafold3.sh
@@ -6,6 +6,7 @@ PYTHON_IMPORT_NAME=$PACKAGE
 PACKAGE_FOLDER_NAME=$PACKAGE
 PACKAGE_DOWNLOAD_METHOD="Git"
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/google-deepmind/alphafold3.git"
-# As of Nov. 20 2024, this commit contains some fixes and support 3.12
-PACKAGE_DOWNLOAD_CMD="git clone --recursive $PACKAGE_DOWNLOAD_ARGUMENT --branch v${VERSION:?version required} $PACKAGE_FOLDER_NAME && cd $PACKAGE_FOLDER_NAME && git checkout 23e3d46d4ca126e8731e8c0cbb5673e9a848ceb5 && cd .."
+PACKAGE_DOWNLOAD_CMD="git clone --recursive $PACKAGE_DOWNLOAD_ARGUMENT --branch v${VERSION:?version required} $PACKAGE_FOLDER_NAME"
 POST_DOWNLOAD_COMMANDS="tar -zcf $PACKAGE_DOWNLOAD_NAME $PACKAGE_FOLDER_NAME"
+PYTHON_VERSION='python/3.11 python/3.12'
+PRE_BUILD_COMMANDS="sed -i -e 's/3.0.0/${VERSION}/' pyproject.toml"
