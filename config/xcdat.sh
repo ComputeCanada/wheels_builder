6e57d6acec8c83a77ed614b45c10944b6135e9e9
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-03-12 09:52:11 -0400

    Added xcdat config

---
+++ config/xcdat.sh
@@ -0,0 +1,3 @@
+MODULE_BUILD_DEPS='geos esmf'
+PACKAGE_DOWNLOAD_ARGUMENT="git+https://github.com/xCDAT/xcdat.git@v${VERSION:?version required}"
+PYTHON_DEPS='-U trove-classifiers setuptools setuptools-scm'
