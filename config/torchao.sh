babb127ca1efd585bb08904f72732b74515003a0
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-03-13 10:54:09 -0400

    Added torchao config and patch to allow build for cuda

---
+++ config/torchao.sh
@@ -0,0 +1,12 @@
+MODULE_BUILD_DEPS='cuda'
+PYTHON_DEPS="torch${TORCH_VERSION:+==$TORCH_VERSION} triton"
+PRE_BUILD_COMMANDS='
+	export USE_CPP=1;
+	export USE_SYSTEM_LIBS=1;
+	export FORCE_CUDA=1;
+	export TORCH_CUDA_ARCH_LIST="8.0;8.6;9.0";
+	export MAX_JOBS=2;
+	export VERSION_SUFFIX="";
+'
+PACKAGE_DOWNLOAD_ARGUMENT="git+https://github.com/pytorch/ao.git@v${VERSION:?version required}"
+PATCHES="torchao-forcecuda.patch"
