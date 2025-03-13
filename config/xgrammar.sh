18d52a84236bbe404fc32ac89dc0f6b1dae8dac2
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-03-13 10:57:09 -0400

    Updated xgrammar config. No pre-build needed

--- config/xgrammar.sh
+++ config/xgrammar.sh
@@ -1,11 +1,4 @@
-PRE_BUILD_COMMANDS='
-	cmake -B _build;
-	cmake --build _build --parallel ${SLURM_CPUS_PER_TASK:-1};
-	cd python;
-'
 PYTHON_DEPS="torch"
-POST_BUILD_COMMANDS='$SCRIPT_DIR/manipulate_wheels.py --inplace --force --add_req triton -w $WHEEL_NAME'
-
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/mlc-ai/xgrammar.git"
 PACKAGE_DOWNLOAD_NAME="$PACKAGE-$VERSION.tar.gz"
 PACKAGE_DOWNLOAD_METHOD="Git"
