4b1078f18cf96826a4b72190c27c1c8f754ed50c
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-03-13 10:55:16 -0400

    Updated cuda-python config. Set import name

--- config/cuda-python.sh
+++ config/cuda-python.sh
@@ -1,3 +1,4 @@
 MODULE_BUILD_DEPS='cuda'
 PYTHON_DEPS='pyclibrary'
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/NVIDIA/cuda-python/archive/refs/tags/v${VERSION:?version required}.tar.gz"
+PYTHON_IMPORT_NAME='cuda'
