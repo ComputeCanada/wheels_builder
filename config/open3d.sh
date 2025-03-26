93233593fc8e745ca6e8ed6b1acddceb950699a1
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-03-25 20:15:07 -0400

    Updated Open3D config for v0.19.
    Added patches for v0.19

--- config/open3d.sh
+++ config/open3d.sh
@@ -13,7 +13,7 @@ PACKAGE_DOWNLOAD_CMD="git clone --depth 1 --recursive $PACKAGE_DOWNLOAD_ARGUMENT
 POST_DOWNLOAD_COMMANDS="tar -zcf $PACKAGE_DOWNLOAD_NAME $PACKAGE_FOLDER_NAME"
 
 if [[ "$EBVERSIONGENTOO" == "2023" ]]; then
-	MODULE_BUILD_DEPS="vtk/9 eigen clang cuda/12 protobuf flexiblas abseil"
+	MODULE_BUILD_DEPS="eigen clang cuda/12 protobuf flexiblas abseil"
 else
 	MODULE_BUILD_DEPS="vtk/9 eigen clang cuda/11.7 protobuf flexiblas"
 fi
@@ -22,28 +22,29 @@ PYTHON_DEPS="torch${TORCH_VERSION:+==$TORCH_VERSION} yapf==0.30.0"
 # Build for CPU then GPU, order is important.
 	# export PYTHONPATH=$VIRTUAL_ENV/lib/python3.*/site-packages:$PYTHONPATH;
 PRE_BUILD_COMMANDS="
-	export TORCH_CUDA_ARCH_LIST='6.0;7.0;7.5;8.0;8.6;9.0';
+	export TORCH_CUDA_ARCH_LIST='7.0;7.5;8.0;8.6;9.0';
 
 	sed -i -e "/obsoletes=/d" python/setup.py;
 
-	cmake -B _build -S . -DDEVELOPER_BUILD=OFF -DBUILD_EXAMPLES=OFF -DUSE_SYSTEM_VTK=ON -DUSE_SYSTEM_EIGEN3=ON \
+	cmake -B _build -S . -DDEVELOPER_BUILD=OFF -DBUILD_EXAMPLES=OFF -DUSE_SYSTEM_VTK=OFF -DUSE_SYSTEM_EIGEN3=ON \
 	                     -DBUILD_ISPC_MODULE=OFF -DCMAKE_VERBOSE_MAKEFILE=ON -DBUILD_GUI=OFF \
 	                     -DCMAKE_CXX_FLAGS=-Wno-error=deprecated-declarations \
 	                     -DBUILD_CUDA_MODULE=OFF \
 	                     -DBUILD_PYTORCH_OPS=ON -DUSE_BLAS=ON -DUSE_SYSTEM_BLAS=ON -DBLA_VENDOR=FlexiBLAS;
-	cmake --build _build --config Release --parallel 16;
+	cmake --build _build --config Release --parallel;
 
-	cmake -B _build -S . -DDEVELOPER_BUILD=OFF -DBUILD_EXAMPLES=OFF -DUSE_SYSTEM_VTK=ON -DUSE_SYSTEM_EIGEN3=ON \
+	cmake -B _build -S . -DDEVELOPER_BUILD=OFF -DBUILD_EXAMPLES=OFF -DUSE_SYSTEM_VTK=OFF -DUSE_SYSTEM_EIGEN3=ON \
 	                     -DBUILD_ISPC_MODULE=OFF -DCMAKE_VERBOSE_MAKEFILE=ON -DBUILD_GUI=OFF \
 	                     -DCMAKE_CXX_FLAGS=-Wno-error=deprecated-declarations \
 	                     -DBUILD_CUDA_MODULE=ON -DCMAKE_CUDA_ARCHITECTURES=${TORCH_CUDA_ARCH_LIST//.} \
 	                     -DBUILD_PYTORCH_OPS=ON -DUSE_BLAS=ON -DUSE_SYSTEM_BLAS=ON -DBLA_VENDOR=FlexiBLAS;
-	cmake --build _build --config Release --parallel 16;
+	cmake --build _build --config Release --parallel;
 
 	cmake --build _build --config Release -- python-package;
 	cd _build/lib/python_package;
 "
 PATCHES="
-	open3d-fix_platname.patch
-	open3d-build_torch_ops.patch
+	open3d-0.19.0-fix_stdgpu.patch
+	open3d-0.19.0-fix_wayland.patch
 "
+POST_BUILD_COMMANDS='wheel tags --remove --platform-tag=linux_x86_64 $WHEEL_NAME && WHEEL_NAME=$(ls *.whl)'
