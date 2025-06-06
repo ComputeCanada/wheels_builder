0bf543dc8b8063d7a52ad5cd92b60d3cd477d662
Olivier Fisette <olivier.fisette@usask.ca>
2025-06-06 21:23:37 +0000

    Pin nlohmann_json version

--- config/qiskit-aer.sh
+++ config/qiskit-aer.sh
@@ -1,4 +1,7 @@
-MODULE_BUILD_DEPS='flexiblas openmpi spdlog muparserx'
+# nlohmann_json 3.10.2 is the latest that works properly with qiskit-aer
+# https://github.com/Qiskit/qiskit-aer/issues/1742
+# https://github.com/Qiskit/qiskit-aer/pull/2283
+MODULE_BUILD_DEPS='flexiblas openmpi spdlog muparserx nlohmann_json/3.10.2'
 MODULE_RUNTIME_DEPS="symengine"
 PRE_BUILD_COMMANDS='
     rm -f pyproject.toml;
