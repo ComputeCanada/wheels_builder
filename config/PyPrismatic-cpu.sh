146d6386da98f4b57e2a871e7ce1dc4a605cb112
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-06-10 07:06:39 -0400

    Updated PyPrismatic configs for 2023

--- config/PyPrismatic-cpu.sh
+++ config/PyPrismatic-cpu.sh
@@ -1,7 +1,7 @@
 MODULE_BUILD_DEPS="cmake hdf5 fftw boost"
-MODULE_RUNTIME_DEPS="hdf5"
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/prism-em/prismatic/archive/refs/tags/v${VERSION:?version required}.tar.gz"
 PACKAGE="PyPrismatic"
 PACKAGE_SUFFIX="-cpu"
 PYTHON_IMPORT_NAME="pyprismatic.core"
 PRE_BUILD_COMMANDS="sed -i -e 's/1.2.0/$VERSION/' setup.py"
+PYTHON_DEPS='h5py scipy'
