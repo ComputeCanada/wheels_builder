3eb0c3e4bd2c63b14690901727875d428ed160ac
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-06-19 14:40:57 -0400

    Added climlab configs and its fortran dependencies

---
+++ config/climlab-emanuel-convection.sh
@@ -0,0 +1,1 @@
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/climlab/climlab-emanuel-convection/archive/refs/tags/v${VERSION:?version required}.tar.gz"
---
+++ config/climlab-sbm-convection.sh
@@ -0,0 +1,1 @@
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/climlab/climlab-sbm-convection/archive/refs/tags/v${VERSION:?version required}.tar.gz"
