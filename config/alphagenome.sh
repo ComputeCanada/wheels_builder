63bdc9e7a42c8747127972dfa08cde8ff4479125
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-07-15 06:18:04 -0400

    Added alphagenome config

---
+++ config/alphagenome.sh
@@ -0,0 +1,3 @@
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/google-deepmind/alphagenome/archive/refs/tags/v${VERSION:?version required}.tar.gz"
+PYTHON_DEPS='grpcio-tools'
+MODULE_RUNTIME_DEPS='arrow'
