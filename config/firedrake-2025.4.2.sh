9c405e10b090a67a539c849113526f7d97467513
Oliver Stueker <oliver.stueker@ace-net.ca>
2025-07-22 15:35:33 -0230

    firedrake-2025.4.2.sh

---
+++ config/firedrake-2025.4.2.sh
@@ -0,0 +1,6 @@
+MODULE_RUNTIME_DEPS='mpi4py/4.0.3 symengine libspatialindex petsc/3.23.4'
+
+PYTHON_DEPS="rtree>=1.2 cachetools Cython>=3.0 decorator<=4.4.2 fenics-ufl>=2025.1.0 firedrake-fiat>=2025.4.0 h5py>3.12.1 libsupermesh loopy>2024.1 mpi-pytest packaging pkgconfig progress pyadjoint-ad>=2025.04.1 pybind11 pycparser pytest pytools[siphash] requests scipy setuptools>=77.0.3 sympy"
+
+PATCHES="firedrake-2025.4.2_pyproject_toml.patch"
+
