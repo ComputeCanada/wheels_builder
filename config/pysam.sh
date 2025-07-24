8bef84c5e5c8551e42b2f9c9523f6a10b7a3698d
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-07-24 10:29:21 -0400

    Updated pysam config to add htslib rpath

--- config/pysam.sh
+++ config/pysam.sh
@@ -1,2 +1,7 @@
 MODULE_BUILD_DEPS="htslib"
-PRE_BUILD_COMMANDS="export HTSLIB_LIBRARY_DIR=$EBROOTHTSLIB/lib; export HTSLIB_INCLUDE_DIR=$EBROOTHTSLIB/include; export HTSLIB_MODE=external;"
+PRE_BUILD_COMMANDS="
+        export HTSLIB_LIBRARY_DIR=$EBROOTHTSLIB/lib;
+        export HTSLIB_INCLUDE_DIR=$EBROOTHTSLIB/include;
+        export HTSLIB_MODE=external;
+        LDFLAGS='-Wl,-rpath,$EBROOTHTSLIB/lib';
+"
