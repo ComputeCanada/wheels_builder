4115701d1c14da20f0f89154a3748358d889b42a
Olivier Fisette <olivier.fisette@usask.ca>
2025-08-05 13:53:23 +0000

    Update config for recent PyQCM releases

--- config/pyqcm.sh
+++ config/pyqcm.sh
@@ -1,20 +1,22 @@
 MODULE_RUNTIME_DEPS="
-    scipy-stack/2023b
+    scipy-stack/2025a
+    nlopt/2
 "
 MODULE_BUILD_DEPS="
-    cmake/3
     cuba/4.2.2
     eigen/3.4.0
     flexiblas/3
     primme/3.2
+    nlopt/2
 "
-PRE_BUILD_COMMANDS="
-    export CMAKE_ARGS=\"
+PRE_BUILD_COMMANDS='
+    sed -e s/\${PROJECT_VERSION}/$VERSION/ -i CMakeLists.txt;
+    export CMAKE_ARGS="
         -DBLA_VENDOR=FlexiBLAS
         -DEIGEN_HAMILTONIAN=1
         -DWITH_PRIMME=1
         -DPRIMME_DIR=$EBROOTPRIMME
         -DCUBA_DIR=$EBROOTCUBA
         -DWITH_GF_OPT_KERNEL=1
-    \"
-"
+    "
+'
