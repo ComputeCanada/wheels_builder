a9b1b131042f20550ffa30004218499f0e705f7d
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-08-27 09:39:01 -0400

    Updated transcript-transformer for download url

--- config/transcript_transformer.sh
+++ config/transcript_transformer.sh
@@ -2,4 +2,4 @@ MODULE_RUNTIME_DEPS='arrow'
 POST_BUILD_COMMANDS='
     $SCRIPT_DIR/manipulate_wheels.py --inplace --force --add_req torch -w $WHEEL_NAME
 '
-
+PACKAGE_DOWNLOAD_ARGUMENT="git+https://github.com/TRISTAN-ORF/TRISTAN@v${VERSION:?version required}"
