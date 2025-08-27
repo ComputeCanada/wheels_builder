22641dcb078bb1f6af0a267fe7c40993220ac267
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-08-27 09:31:38 -0400

    Updated xformers to use cuda/12.6 and cc 8,9

--- config/xformers.sh
+++ config/xformers.sh
@@ -1,11 +1,11 @@
 PYTHON_DEPS="torch${TORCH_VERSION:+==$TORCH_VERSION}"
 if [[ "$EBVERSIONGENTOO" == "2023" ]]; then
-	MODULE_BUILD_DEPS="cuda/12.2"
+	MODULE_BUILD_DEPS="cuda/12.6"
 else
 	MODULE_BUILD_DEPS="gcc/9.3.0 cuda/11.7"
 fi
 PRE_BUILD_COMMANDS="
-	export TORCH_CUDA_ARCH_LIST='7.0;7.5;8.0;8.6;9.0';
+	export TORCH_CUDA_ARCH_LIST='8.0;9.0';
 	export FORCE_CUDA=1;
 	export MAX_JOBS=2;
 "
