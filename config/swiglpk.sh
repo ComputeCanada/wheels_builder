1f72209683e3a6c3a0031333f0177228c7f31912
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-08-27 09:36:17 -0400

    Updated swiglpk config for 2023

--- config/swiglpk.sh
+++ config/swiglpk.sh
@@ -1,10 +1,3 @@
-if [[ -n $EBROOTGENTOO ]]; then
-    # Gentoo-based stack (StdEnv/2020)
-    MODULE_BUILD_DEPS="glpk/5.0"
-else
-    # NIX-based stack (StdEnv/2018.3)
-    MODULE_BUILD_DEPS="gcc/7.3.0 glpk/4.65"
-fi
-
+MODULE_BUILD_DEPS='swig'
 
 
