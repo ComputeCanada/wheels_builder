d24c10c77ddd92e7e675e1362bbebd87f79609ab
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-09-17 12:59:26 -0400

    Updated transcript-transformer config. Fix polars reqs. Remove github link as now available on PyPI

--- config/transcript_transformer.sh
+++ config/transcript_transformer.sh
@@ -1,5 +1,8 @@
 MODULE_RUNTIME_DEPS='arrow'
+PRE_BUILD_COMMANDS='
+   sed -i "s/polars[[:space:]]*==[[:space:]]*1\.28/polars ~= 1.28.0/" pyproject.toml;
+'
 POST_BUILD_COMMANDS='
     $SCRIPT_DIR/manipulate_wheels.py --inplace --force --add_req torch -w $WHEEL_NAME
 '
-PACKAGE_DOWNLOAD_ARGUMENT="git+https://github.com/TRISTAN-ORF/TRISTAN@v${VERSION:?version required}"
+
