7ea28f12afc43f27fcde5596f1736f4926d3869e
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-09-29 08:24:02 -0400

    Updated torchcodec config

--- config/torchcodec.sh
+++ config/torchcodec.sh
@@ -1,6 +1,6 @@
 MODULE_BUILD_DEPS='cuda/12.6 protobuf abseil'
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/pytorch/torchcodec/archive/refs/tags/v${VERSION:?version required}.tar.gz"
-PYTHON_DEPS='torch>=2.7.0'
+PYTHON_DEPS='torch>=2.7.0 setuptools>77.0.0'
 PRE_BUILD_COMMANDS='
         export ENABLE_CUDA=1;
         export I_CONFIRM_THIS_IS_NOT_A_LICENSE_VIOLATION=1;
