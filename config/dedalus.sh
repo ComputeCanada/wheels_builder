008f6bdc97deed58a56cc2f307025924d9a6f35f
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-10-15 07:52:49 -0400

    Updated dedalus config, uses openmpi 5

--- config/dedalus.sh
+++ config/dedalus.sh
@@ -1,5 +1,6 @@
-MODULE_RUNTIME_DEPS='openmpi mpi4py fftw-mpi hdf5-mpi'
-PYTHON_DEPS="h5py cython==3.0.8"
+MODULE_BUILD_DEPS='gcc/13 openmpi/5 mpi4py fftw-mpi hdf5-mpi'
+MODULE_RUNTIME_DEPS='gcc openmpi mpi4py'
+PYTHON_DEPS="h5py"
 PRE_BUILD_COMMANDS='export MPI_PATH=$EBROOTOPENMPI; export FFTW_PATH=$EBROOTFFTWMPI; export FFTW_STATIC=1; export CC=mpicc;'
 # pip download runs the setup in order to build the metadata, which requires the paths to be known...
 PRE_DOWNLOAD_COMMANDS=$PRE_BUILD_COMMANDS
