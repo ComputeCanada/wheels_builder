80aeecac8571e77b8f980cf91ea4c3902031dbcc
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-10-15 07:50:36 -0400

    Added voyager config

---
+++ config/voyager.sh
@@ -0,0 +1,8 @@
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/spotify/voyager.git"
+PACKAGE_DOWNLOAD_NAME="$PACKAGE-$VERSION.tar.gz"
+PACKAGE_DOWNLOAD_METHOD="Git"
+PACKAGE_DOWNLOAD_CMD="git clone --jobs 16 --depth 1 --recursive $PACKAGE_DOWNLOAD_ARGUMENT --branch v${VERSION:?version required} $PACKAGE_FOLDER_NAME"
+POST_DOWNLOAD_COMMANDS="tar -zcf ${PACKAGE}-${VERSION}.tar.gz $PACKAGE_FOLDER_NAME"
+PRE_BUILD_COMMANDS='cd python; sed -i "s/^version = \".*\"/version = \"'"$VERSION"'\"/" pyproject.toml;'
+
+
