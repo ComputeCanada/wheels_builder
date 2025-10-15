32fbb82567e0818b22a2268aba6f2d66701a6012
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-10-15 08:14:31 -0400

    Updated pylate config

--- config/pylate.sh
+++ config/pylate.sh
@@ -1,1 +1,4 @@
-PRE_BUILD_COMMANDS="sed -i -e \"s/fast-plaid>=${VERSION}\.[0-9]\+,<=${VERSION}\.[0-9]\+/fast-plaid==${VERSION}/\" pyproject.toml"
+PRE_BUILD_COMMANDS="
+	sed -i -e \"s/fast-plaid>=${VERSION}\.[0-9]\+,<=${VERSION}\.[0-9]\+/fast-plaid==${VERSION}/\" pyproject.toml;
+	sed -i -e '/ninja==.*/d' pyproject.toml;
+"
