2c1c82411d1dc930d197f3abae21c68eaf474f71
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-10-15 07:55:26 -0400

    Updated numpy config.
    Uses openmp and cython 3.1.2

--- config/numpy.sh
+++ config/numpy.sh
@@ -6,16 +6,17 @@ fi
 MODULE_BUILD_DEPS="arch/$GENERIC_ARCH flexiblas"
 
 # From 1.26, strictly use meson.
-PYTHON_DEPS="pytest hypothesis -U pip==24.0.0 meson-python setuptools<=60.0"
+PYTHON_DEPS="pytest hypothesis meson-python setuptools"
 PIP_WHEEL_ARGS='
     -Csetup-args=-Dblas=flexiblas
     -Csetup-args=-Dlapack=flexiblas
     -Csetup-args=-Dblas-order=flexiblas
     -Csetup-args=-Dlapack-order=flexiblas
     -Csetup-args=-Dallow-noblas=false
+    -Csetup-args=-Denable-openmp=true
 '
 # cython is not picked up in one test, and it fail. Install cython in the virtual env.
 PRE_TEST_COMMANDS="pip install --ignore-installed 'cython>3.0.0'"
-MODULE_BUILD_DEPS_DEFAULT="cython/.3.0.11"
+MODULE_BUILD_DEPS_DEFAULT="cython/.3.1.2"
 
 PYTHON_TESTS="numpy.__config__.show(); numpy.test()"
