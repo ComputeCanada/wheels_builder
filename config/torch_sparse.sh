17b4a91d91b1d8bb4450675e72d562ef101046dc
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-10-22 09:59:13 -0400

    Updated torch-geo and friends with 12.6

--- config/torch_sparse.sh
+++ config/torch_sparse.sh
@@ -1,11 +1,11 @@
 PYTHON_DEPS="torch${TORCH_VERSION:+==$TORCH_VERSION} torch-scatter"
 if [[ "$EBVERSIONGENTOO" == "2023" ]]; then
-	MODULE_BUILD_DEPS="cuda/12.2 metis-64idx/5.1.0" # Must use 64 bits index module
+	MODULE_BUILD_DEPS="cuda/12.6 metis-64idx/5.1.0" # Must use 64 bits index module
 else
 	MODULE_BUILD_DEPS="gcc/9.3.0 cuda/11.7 metis-64idx/5.1.0" # Must use 64 bits index module
 fi
 PRE_BUILD_COMMANDS="
-	export TORCH_CUDA_ARCH_LIST='6.0;7.0;7.5;8.0;8.6;9.0';
+	export TORCH_CUDA_ARCH_LIST='7.0;8.0;9.0';
 	export FORCE_CUDA=1;
 	export WITH_METIS=1;
 	sed -i -E \"s#'scipy',#'scipy','torch'#\" setup.py;
--- config/torch_spline_conv.sh
+++ config/torch_spline_conv.sh
@@ -1,11 +1,11 @@
 PYTHON_DEPS="torch${TORCH_VERSION:+==$TORCH_VERSION}"
 if [[ "$EBVERSIONGENTOO" == "2023" ]]; then
-	MODULE_BUILD_DEPS="cuda/12.2"
+	MODULE_BUILD_DEPS="cuda/12.6"
 else
 	MODULE_BUILD_DEPS="gcc/9.3.0 cuda/11.7"
 fi
 PRE_BUILD_COMMANDS="
-	export TORCH_CUDA_ARCH_LIST='6.0;7.0;7.5;8.0;8.6;9.0';
+	export TORCH_CUDA_ARCH_LIST='7.0;8.0;9.0';
 	export FORCE_CUDA=1;
 	sed -i -e \"s#install_requires = \[\]#install_requires = \['torch'\]#\" setup.py;
 "
