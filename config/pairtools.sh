44a0932eedd2eb7475bc9ba6872c44bbb6d102d9
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-12-16 16:40:41 -0500

    Updated pairtools config

--- config/pairtools.sh
+++ config/pairtools.sh
@@ -1,3 +1,4 @@
-MODULE_BUILD_DEPS='samtools'
+MODULE_BUILD_DEPS='samtools htslib'
 PYTHON_DEPS="numpy>=1.10 click>=6.6 scipy>=1.7.0 pandas>=1.3.4 pysam>=0.15.0 pyyaml bioframe>=0.3.3"
 PATCHES='pairtools-1.0.1-fix_pysam_libs.patch'
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/open2c/pairtools/archive/refs/tags/v${VERSION:?version required}.tar.gz"
