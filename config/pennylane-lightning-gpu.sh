08bd97ce1bb85f2b13e289ace8d32ee7076a7e41
Charles Coulombe <charles.coulombe@calculquebec.ca>
2026-01-13 14:13:04 -0500

    Updated pennylane-lightning to remove hard scipy-openblas32 dep

--- config/pennylane-lightning-gpu.sh
+++ config/pennylane-lightning-gpu.sh
@@ -1,12 +1,18 @@
-MODULE_BUILD_DEPS="arch/avx2 cmake cuda cuquantum flexiblas openmpi"
-MODULE_RUNTIME_DEPS='cuda cuquantum'
-PYTHON_DEPS="pybind11 scipy"
+MODULE_BUILD_DEPS="cmake cuda/12.9 cuquantum flexiblas openmpi"
+MODULE_RUNTIME_DEPS='cuda/12.9 cuquantum'
+PYTHON_DEPS="pybind11 nanobind scipy"
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/PennyLaneAI/pennylane-lightning/archive/refs/tags/v${VERSION:?version required}.tar.gz"
 PRE_BUILD_COMMANDS="
 	export CUQUANTUM_SDK=\$EBROOTCUQUANTUM/lib;
-	PL_BACKEND="lightning_gpu" python scripts/configure_pyproject_toml.py;
-	export CMAKE_ARGS='-DENABLE_MPI=ON -DENABLE_NATIVE=OFF -DENABLE_BLAS=ON -DENABLE_OPENMP=ON -DBLA_VENDOR=FlexiBLAS -DENABLE_AVX=ON -DENABLE_AVX2=ON -DENABLE_LAPACK=ON';
+	export PL_BACKEND="lightning_gpu";
+	export CMAKE_ARGS='-DENABLE_MPI=ON -DENABLE_NATIVE=OFF -DENABLE_BLAS=OFF -DENABLE_OPENMP=ON -DBLA_VENDOR=FlexiBLAS -DENABLE_AVX=ON -DENABLE_AVX2=ON -DENABLE_LAPACK=ON -DENABLE_SCIPY_OPENBLAS=OFF';
+	sed -i -e '/scipy-openblas32/d' -e '/nvidia-.*-cu12/d' -e '/nvidia-.*{package_suffix}/d' scripts/configure_pyproject_toml.py;
+	python scripts/configure_pyproject_toml.py;
 "
-#PATCHES='pennylane_lightning-fix_blas.patch'
+
 PYTHON_IMPORT_NAME='pennylane_lightning'
 # `pennylane_lightning.lightning_gpu` requires a device to import correctly.
+
+PATCHES='
+    pennylane_lightning-toggle_scipy_openblas.patch
+'
--- config/pennylane-lightning.sh
+++ config/pennylane-lightning.sh
@@ -1,4 +1,4 @@
-MODULE_BUILD_DEPS="arch/avx2 cmake flexiblas openmpi"
+MODULE_BUILD_DEPS="cmake flexiblas openmpi"
 PYTHON_DEPS="pybind11 nanobind scipy"
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/PennyLaneAI/pennylane-lightning/archive/refs/tags/v${VERSION:?version required}.tar.gz"
 
@@ -7,7 +7,7 @@ PRE_BUILD_COMMANDS='
 	export ENABLE_NANOBIND=1;
 	export PL_BACKEND="lightning_qubit";
 	export CMAKE_ARGS="-DENABLE_NATIVE=OFF -DENABLE_BLAS=OFF -DENABLE_OPENMP=ON -DENABLE_MPI=OFF -DBLA_VENDOR=FlexiBLAS -DENABLE_AVX=ON -DENABLE_AVX2=ON -DENABLE_LAPACK=ON -DENABLE_SCIPY_OPENBLAS=OFF";
-	sed -i -e '/scipy-openblas32/d' -e '/nvidia-.*-cu12/d' scripts/configure_pyproject_toml.py;
+	sed -i -e "/scipy-openblas32/d" -e "/nvidia-.*-cu12/d" scripts/configure_pyproject_toml.py;
 	python scripts/configure_pyproject_toml.py;
 '
 
