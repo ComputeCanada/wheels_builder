e8542aa13b8c9f7bc6aaee7bbf76594f6365e6d6
Charles Coulombe <charles.coulombe@calculquebec.ca>
2026-01-27 01:06:54 -0500

    Finally, use transformer-engine meta scheme. Added tranformer-engine-torch and removed transformer-engine configs

--- config/transformer_engine_torch.sh
+++ config/transformer_engine_torch.sh
@@ -1,3 +1,4 @@
+# pip download strip build_tools directory... so use git
 PACKAGE_DOWNLOAD_ARGUMENT="git+https://github.com/NVIDIA/TransformerEngine.git@v${VERSION:?version required}"
 
 if [[ -z "$TORCH_VERSION" ]]; then
@@ -13,11 +14,16 @@ fi
 
 PYTHON_DEPS="torch${TORCH_VERSION:+==$TORCH_VERSION}"
 PRE_BUILD_COMMANDS='
+	cd transformer_engine/pytorch;
 	export MAX_JOBS=${SLURM_CPUS_PER_TASK:-1};
 	export NVTE_BUILD_THREADS_PER_JOB=1;
 	export NVTE_RELEASE_BUILD=1;
+    export NVTE_CUDA_ARCHS="70;80;90";
+    python setup.py build_ext;
 '
 PRE_TEST_COMMANDS='
     export CUDNN_HOME=$EBROOTCUDNN;
 '
 PYTHON_IMPORT_NAME='transformer_engine'
+
+POST_BUILD_COMMANDS='$SCRIPT_DIR/manipulate_wheels.py --inplace --force --add_req numpy -w $WHEEL_NAME && WHEEL_NAME=$(ls *.whl)'
