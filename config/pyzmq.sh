326d9bbc0e549093cdfd425c26e8fdd3cad2ddff
Charles Coulombe <charles.coulombe@calculquebec.ca>
2026-02-17 09:02:25 -0500

    Updated pyzmq config

--- config/pyzmq.sh
+++ config/pyzmq.sh
@@ -2,4 +2,5 @@ PYTHON_DEPS='scikit_build_core'
 PYTHON_IMPORT_NAME="zmq"
 # This clean and force regeneration of cython source that are outdated
 PRE_BUILD_COMMANDS="python setup.py clean --all"
+MODULE_BUILD_DEPS='-python-build-bundle'
 
