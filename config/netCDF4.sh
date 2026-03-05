87de8de4c4ad5577bf5bea90fdef3bc96c52790f
Charles Coulombe <charles.coulombe@calculquebec.ca>
2026-03-05 07:56:31 -0500

    Added mpi4py in netcdf4 config

--- config/netCDF4.sh
+++ config/netCDF4.sh
@@ -1,6 +1,6 @@
 # For parallel io, mpi4py is required
 MODULE_BUILD_DEPS="openmpi hdf5-mpi netcdf-mpi"
-MODULE_RUNTIME_DEPS="openmpi"
+MODULE_RUNTIME_DEPS="openmpi mpi4py"
 PRE_DOWNLOAD_COMMANDS='export HDF5_DIR=$EBROOTHDF5; export NETCDF4_DIR=$EBROOTNETCDF'
 RPATH_TO_ADD='$EBROOTOPENMPI/lib'
 PYTHON_DEPS='pip>=23.0'
