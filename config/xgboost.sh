a550af62beb8908296cd3c40aedbb151461c7089
Charles Coulombe <charles.coulombe@calculquebec.ca>
2026-03-05 07:54:06 -0500

    Updated xgboost with 12.9

--- config/xgboost.sh
+++ config/xgboost.sh
@@ -1,6 +1,6 @@
 # cuda 12.6 with cccl has a bug : https://github.com/dmlc/xgboost/issues/10894, so use 12.2
 if [[ "$EBVERSIONGENTOO" == "2023" ]]; then
-	MODULE_BUILD_DEPS="cuda/12.2 nccl cmake"
+	MODULE_BUILD_DEPS="cuda/12.9 nccl cmake"
 else
 	MODULE_BUILD_DEPS="cuda/11.4 nccl cmake"
 fi
@@ -12,7 +12,7 @@ POST_DOWNLOAD_COMMANDS="tar -zcf ${PACKAGE}-${VERSION}.tar.gz $PACKAGE_FOLDER_NA
 PRE_BUILD_COMMANDS="
         mkdir build &&
         cd build && 
-        cmake .. -DCMAKE_CUDA_ARCHITECTURES='70;80;90' -DUSE_CUDA=ON -DUSE_NCCL=ON -DNCCL_ROOT=$EBROOTNCCL &&
+        cmake .. -DCMAKE_CUDA_ARCHITECTURES='80;90;100' -DUSE_CUDA=ON -DUSE_NCCL=ON -DNCCL_ROOT=$EBROOTNCCL &&
         make -j4 && cd ../python-package/;
         sed -i -e '/nvidia-.*-cu12/d' pyproject.toml;
 "
