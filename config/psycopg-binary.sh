ba0a20aabcd28575529bd9353d2535feaf9d8dfc
Charles Coulombe <charles.coulombe@calculquebec.ca>
2026-03-26 19:51:17 -0400

    Added psycopg-binary and psycopg-c config

---
+++ config/psycopg-binary.sh
@@ -0,0 +1,11 @@
+MODULE_BUILD_DEPS="postgresql"
+PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/psycopg/psycopg.git"
+PACKAGE_DOWNLOAD_NAME="$PACKAGE-$VERSION.tar.gz"
+PACKAGE_DOWNLOAD_METHOD="Git"
+PACKAGE_DOWNLOAD_CMD="git clone --jobs 16 --depth 1 --recursive $PACKAGE_DOWNLOAD_ARGUMENT --branch ${VERSION:?version required} $PACKAGE_FOLDER_NAME"
+POST_DOWNLOAD_COMMANDS="tar -zcf ${PACKAGE}-${VERSION}.tar.gz $PACKAGE_FOLDER_NAME"
+PRE_BUILD_COMMANDS="
+	python ./tools/ci/copy_to_binary.py && cd psycopg_binary;
+"
+PYTHON_DEPS="psycopg==$VERSION"
+TEST_COMMAND="python -c 'import psycopg, psycopg_binary'"
---
+++ config/psycopg-c.sh
@@ -0,0 +1,3 @@
+MODULE_BUILD_DEPS="postgresql"
+PYTHON_DEPS="psycopg[binary]"
+TEST_COMMAND="python -c 'import psycopg, psycopg_c'"
