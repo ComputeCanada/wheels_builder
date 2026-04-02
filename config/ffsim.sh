edfb78f7e72b58a59d3a697844104d851cee7db3
Charles Coulombe <charles.coulombe@calculquebec.ca>
2026-04-02 12:30:48 -0400

    Updated ffsim config. Fixed patching rpath

--- config/ffsim.sh
+++ config/ffsim.sh
@@ -1,4 +1,4 @@
 # Does not build if not with openblas
-MODULE_BUILD_DEPS='symengine/0.13 openblas'
+MODULE_BUILD_DEPS='symengine openblas'
 PRE_DOWNLOAD_COMMANDS='export CARGO_BUILD_JOBS=${SLURM_CPUS_PER_TASK:-1}'
-POST_BUILD_COMMANDS='setrpaths.sh --path $WHEEL --add_path $EBROOTOPENBLAS/lib'
+POST_BUILD_COMMANDS='setrpaths --path $WHEEL_NAME --add_path $EBROOTOPENBLAS/lib'
