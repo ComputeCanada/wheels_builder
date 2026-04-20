ad86ac974eefcfebe49de0b2fd3129223ed6d7f9
Charles Coulombe <charles.coulombe@calculquebec.ca>
2026-04-20 14:33:44 -0400

    Updated gpu4pyscf pre build commands

--- config/GPU4PySCF.sh
+++ config/GPU4PySCF.sh
@@ -3,6 +3,7 @@ MODULE_RUNTIME_DEPS='cuda/12.9'
 PRE_BUILD_COMMANDS='
 	export CMAKE_CONFIGURE_ARGS=-DCUDA_ARCHITECTURES="80;90;100";
 	export CMAKE_BUILD_ARGS="--parallel $SLURM_CPUS_PER_TASK";
-	sed -i -e "s/cupy-cuda{CUDA_VERSION}/cupy/" -e "s/ + '\''-cuda'\'' + CUDA_VERSION//" setup.py;
+	sed -i -e "s/-cuda{CUDA_VERSION}//" -e "s/ + '\''-cuda'\'' + CUDA_VERSION//" setup.py builder/setup_libxc.py;
+
 
 '
