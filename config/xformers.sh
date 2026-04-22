a20682547fcbcc59ed623488a725538207ec7b7f
Charles Coulombe <charles.coulombe@calculquebec.ca>
2026-04-22 07:22:06 -0400

    Updated xformers for 12.9

--- config/xformers.sh
+++ config/xformers.sh
@@ -1,11 +1,11 @@
 PYTHON_DEPS="torch${TORCH_VERSION:+==$TORCH_VERSION}"
 if [[ "$EBVERSIONGENTOO" == "2023" ]]; then
-	MODULE_BUILD_DEPS="cuda/12.6"
+	MODULE_BUILD_DEPS="cuda/12.9"
 else
 	MODULE_BUILD_DEPS="gcc/9.3.0 cuda/11.7"
 fi
 PRE_BUILD_COMMANDS="
-	export TORCH_CUDA_ARCH_LIST='8.0;9.0';
+	export TORCH_CUDA_ARCH_LIST='8.0;9.0;10.0';
 	export FORCE_CUDA=1;
 	export MAX_JOBS=2;
 "
