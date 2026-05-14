39be2b83d5d49907957df11c3a320add5b107bd0
Charles Coulombe <charles.coulombe@calculquebec.ca>
2026-05-14 06:48:40 -0400

    Updated torch friends for cuda 12.9

--- config/torchcodec.sh
+++ config/torchcodec.sh
@@ -1,8 +1,8 @@
-MODULE_BUILD_DEPS='cuda/12.6 protobuf abseil'
+MODULE_BUILD_DEPS='cuda/12.9 protobuf abseil'
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/pytorch/torchcodec/archive/refs/tags/v${VERSION:?version required}.tar.gz"
 PYTHON_DEPS='torch>=2.7.0 setuptools>77.0.0'
 PRE_BUILD_COMMANDS='
         export ENABLE_CUDA=1;
         export I_CONFIRM_THIS_IS_NOT_A_LICENSE_VIOLATION=1;
-        export TORCH_CUDA_ARCH_LIST="7.0;8.0;9.0";
+        export TORCH_CUDA_ARCH_LIST="8.0;9.0;10.0+PTX";
 '
