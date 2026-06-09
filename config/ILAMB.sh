c1919f5b3925ca08400b484cce1fcec2a330bc6e
aroudgar <aroudgar@sfu.ca>
2026-06-09 17:11:41 +0000

    adding new config for ILAMB package

---
+++ config/ILAMB.sh
@@ -0,0 +1,3 @@
+PACKAGE_DOWNLOAD_ARGUMENT=https://github.com/rubisco-sfa/ILAMB/archive/refs/tags/v${VERSION:?version required}.zip
+MODULE_RUNTIME_DEPS="gcc/12.3  openmpi/4.1.5 mpi4py/4.1.0 arrow/24.0.0"
+
