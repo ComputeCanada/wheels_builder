0e4aabad4ae62500dea576050706f05083c0a83a
Charles Coulombe <charles.coulombe@calculquebec.ca>
2026-06-12 08:50:00 -0400

    Updated nvmath for cuda 13

--- config/nvmath-python.sh
+++ config/nvmath-python.sh
@@ -1,3 +1,8 @@
-MODULE_BUILD_DEPS='cuda/12.9'
-PYTHON_DEPS='cuda-bindings==12.9.*'
+if [[ -n "$VERSION" ]] && [[ 10#$(translate_version $VERSION) -lt 10#$(translate_version '0.8.0')  ]]; then
+	MODULE_BUILD_DEPS='cuda/12.9'
+	PYTHON_DEPS='cuda-bindings==12.9.*'
+else
+	MODULE_BUILD_DEPS='cuda/13.2'
+	PYTHON_DEPS='cuda-bindings==13.2.*'
+fi
 PACKAGE_DOWNLOAD_ARGUMENT="git+https://github.com/NVIDIA/nvmath-python@v${VERSION:?version required}"
