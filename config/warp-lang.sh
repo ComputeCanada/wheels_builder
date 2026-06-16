0b80ffbdd4aa54e7c1ad4be2384f58c4a31e225e
Charles Coulombe <charles.coulombe@calculquebec.ca>
2026-06-16 06:53:14 -0400

    Updated warp-lang to remove aggressive strip-all which was corrupting shared object

--- config/warp-lang.sh
+++ config/warp-lang.sh
@@ -1,10 +1,11 @@
-MODULE_BUILD_DEPS='cuda/13 git-lfs libmathdx'
+MODULE_BUILD_DEPS='cuda/13 libmathdx'
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/NVIDIA/warp"
 PACKAGE_DOWNLOAD_NAME="$PACKAGE-$VERSION.tar.gz"
 PACKAGE_DOWNLOAD_METHOD="Git"
 PACKAGE_DOWNLOAD_CMD="git clone --jobs 16 --depth 1 --recursive $PACKAGE_DOWNLOAD_ARGUMENT --branch v${VERSION:?version required} $PACKAGE_FOLDER_NAME"
 POST_DOWNLOAD_COMMANDS="tar -zcf ${PACKAGE}-${VERSION}.tar.gz $PACKAGE_FOLDER_NAME"
 PRE_BUILD_COMMANDS='
+	sed -i -e "s/--strip-all//" warp/_src/build_dll.py;
 	python build_lib.py --mode release --verbose --no-build-llvm -j ${SLURM_CPUS_PER_TASK:-1} --use-dynamic-cuda --libmathdx-path=$EBROOTLIBMATHDX;
 '
 POST_BUILD_COMMANDS='wheel tags --remove --platform-tag=linux_x86_64 $WHEEL_NAME && WHEEL_NAME=$(ls *.whl)'
