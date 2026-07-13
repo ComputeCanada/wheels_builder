fd2f222ab4a97852b5ff54b5418416761794b4b9
Charles Coulombe <charles.coulombe@calculquebec.ca>
2026-07-12 23:50:52 -0400

    Updated torchcodec for 0.14.
    Ensure it find pybind11 correctly
    
    use cuda 13

--- config/torchcodec.sh
+++ config/torchcodec.sh
@@ -1,8 +1,10 @@
-MODULE_BUILD_DEPS='cuda/12.9 protobuf abseil'
+MODULE_BUILD_DEPS='cuda/13.2 protobuf abseil'
 PACKAGE_DOWNLOAD_ARGUMENT="https://github.com/pytorch/torchcodec/archive/refs/tags/v${VERSION:?version required}.tar.gz"
-PYTHON_DEPS='torch>=2.7.0 setuptools>77.0.0'
+PYTHON_DEPS="torch>=2.7.0 setuptools>77.0.0 torch${TORCH_VERSION:+==$TORCH_VERSION}"
 PRE_BUILD_COMMANDS='
         export ENABLE_CUDA=1;
         export I_CONFIRM_THIS_IS_NOT_A_LICENSE_VIOLATION=1;
         export TORCH_CUDA_ARCH_LIST="8.0;9.0;10.0+PTX";
+        export CMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH:$(python3 -c '\''import pybind11; print(pybind11.get_cmake_dir())'\'')";
+        export CXXFLAGS="-Wno-error=restrict";
 '
