a34970f8b63a6c3594f9f101fa9882244cf2da1f
Charles Coulombe <charles.coulombe@calculquebec.ca>
2026-07-21 08:54:19 -0400

    Updated maturin dep for cryptography

--- config/cryptography.sh
+++ config/cryptography.sh
@@ -1,3 +1,3 @@
-PYTHON_DEPS="setuptools_rust cffi pycparser jaraco.classes keyring maturin"
+PYTHON_DEPS="setuptools_rust cffi pycparser jaraco.classes keyring maturin>=1.9.4"
 PYTHON_IMPORT_NAME="cryptography.hazmat.bindings.openssl.binding"
 MODULE_BUILD_DEPS="rust"
