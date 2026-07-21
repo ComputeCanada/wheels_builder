0a5ecfe879228318474cbb069801e71fa82a3e36
Charles Coulombe <charles.coulombe@calculquebec.ca>
2026-07-21 08:56:56 -0400

    Updated tilelang for cuda 13

--- config/tilelang.sh
+++ config/tilelang.sh
@@ -1,6 +1,8 @@
-MODULE_BUILD_DEPS='cuda/12.9'
+MODULE_BUILD_DEPS='cuda/13.2'
 PRE_BUILD_COMMANDS='
     export MAX_JOBS=${SLURM_CPUS_PER_TASK:-1};
     export NO_VERSION_LABEL=ON;
     export USE_CUDA=ON;
 '
+PACKAGE_DOWNLOAD_ARGUMENT="git+https://github.com/tile-ai/tilelang@v${VERSION:?version required}"
+PYTHON_DEPS='z3-solver>=4.13.0'
