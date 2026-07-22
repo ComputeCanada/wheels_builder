c2a411fbd732f4ca9f82027f2c1727fbfe7e6f0c
Charles Coulombe <charles.coulombe@calculquebec.ca>
2026-07-21 22:39:00 -0400

    Updated jaxtyping config
    Patch should be applied for version between <=0.3.2 and >=0.2.23
    
    Rework patch so changes are in one position, and use str.partition instead of regex

--- config/jaxtyping.sh
+++ config/jaxtyping.sh
@@ -1,1 +1,4 @@
-PATCHES='jaxtyping-fix_version_parsing.patch'
+# the patch is only valid between 0.3.2 and 0.2.23 since those version checked for the equinox version.
+if [[ $(translate_version $VERSION) -le $(translate_version '0.3.2') && $(translate_version $VERSION) -ge $(translate_version '0.2.23') ]]; then
+	PATCHES='jaxtyping-fix_version_parsing.patch'
+fi
