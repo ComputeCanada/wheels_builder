10d93148f43039a087bc5bc27019277a452ea4f1
Olivier Fisette <ofisette.fisette@calculquebec.ca>
2026-08-27 13:42:54 +0000

    Unpin Parasail dependency to use module-provided extension

--- config/ngspeciesid.sh
+++ config/ngspeciesid.sh
@@ -2,3 +2,5 @@
 PYTHON_IMPORT_NAME=""
 TEST_COMMAND="NGSpeciesID --help"
 MODULE_RUNTIME_DEPS="parasail"
+# Unpin Parasail 1.2.4 to use the 2.* module-provided extension instead
+POST_BUILD_COMMANDS='$SCRIPT_DIR/manipulate_wheels.py --inplace --force --update_req parasail -w $WHEEL_NAME && WHEEL_NAME=$(ls *.whl)'
