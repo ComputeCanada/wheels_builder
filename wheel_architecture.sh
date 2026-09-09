b36300aa204c10bcc1393c2e1dcc473f8178d3bf
Charles Coulombe <charles.coulombe@calculquebec.ca>
2026-09-09 11:59:37 -0400

    Updated wheel_architecture for 2026
    Fixed wrong variable used for 2023

--- wheel_architecture.sh
+++ wheel_architecture.sh
@@ -7,6 +7,7 @@ tmp=$(mktemp --directory)
 cd $tmp
 WORKS_ON_GENTOO2020=1
 WORKS_ON_GENTOO2023=1
+WORKS_ON_GENTOO2026=1
 WORKS_ON_NIX=1
 
 # On Nix we don't have Py 3.9 and newer so those wheels can be generic and don't need
@@ -34,12 +35,15 @@ function version_lte {
 NIX_GLIBC="/cvmfs/soft.computecanada.ca/nix/var/nix/profiles/16.09/lib/libc.so.6"
 GENTOO2020_GLIBC="/cvmfs/soft.computecanada.ca/gentoo/2020/lib64/libc.so.6"
 GENTOO2023_GLIBC="/cvmfs/soft.computecanada.ca/gentoo/2023/x86-64-v3/lib64/libc.so.6"
+GENTOO2026_GLIBC="/cvmfs/soft.computecanada.ca/gentoo/2026/x86-64-v3/lib64/libc.so.6"
 NIX_LDD="/cvmfs/soft.computecanada.ca/nix/var/nix/profiles/16.09/bin/ldd"
 GENTOO2020_LDD="/cvmfs/soft.computecanada.ca/gentoo/2020/usr/bin/ldd"
 GENTOO2023_LDD="/cvmfs/soft.computecanada.ca/gentoo/2023/x86-64-v3/usr/bin/ldd"
+GENTOO2026_LDD="/cvmfs/soft.computecanada.ca/gentoo/2026/x86-64-v3/usr/bin/ldd"
 NIX_GLIBC_VERSION=$(strings "$NIX_GLIBC" | grep "^GLIBC_" | cut -d'_' -f2 | sort -V | grep "^[0-9]" | tail -1)
 GENTOO2020_GLIBC_VERSION=$(strings "$GENTOO2020_GLIBC" | grep "^GLIBC_" | cut -d'_' -f2 | sort -V | grep "^[0-9]" | tail -1)
 GENTOO2023_GLIBC_VERSION=$(strings "$GENTOO2023_GLIBC" | grep "^GLIBC_" | cut -d'_' -f2 | sort -V | grep "^[0-9]" | tail -1)
+GENTOO2026_GLIBC_VERSION=$(strings "$GENTOO2026_GLIBC" | grep "^GLIBC_" | cut -d'_' -f2 | sort -V | grep "^[0-9]" | tail -1)
 
 self_contained_shared_objects=$(find . -name '*.so*' -print0 | xargs -0 -n1 -- basename 2>/dev/null | tr '\n' '|' | sed -e "s/|$//g")
 
@@ -69,15 +73,23 @@ for fname in $(find . -type f); do
 	if [[ $WORKS_ON_NIX -eq 1 && $rpath =~ 'nix' || $rpath =~ 'easybuild/software/2017' ]]; then
 		WORKS_ON_GENTOO2020=0
 		WORKS_ON_GENTOO2023=0
+		WORKS_ON_GENTOO2026=0
 		echo "$fname" is nix, rpath=$rpath  >&2
 	elif [[ $rpath =~ 'gentoo/2020' || $rpath =~ 'easybuild/software/2020' || $rpath =~ 'easybuild/software/2019' ]]; then
 		WORKS_ON_NIX=0
 		WORKS_ON_GENTOO2023=0
+		WORKS_ON_GENTOO2026=0
 		echo "$fname" is gentoo2020, rpath=$rpath  >&2
 	elif [[ $rpath =~ 'gentoo/2023' || $rpath =~ 'easybuild/software/2023' ]]; then
 		WORKS_ON_NIX=0
 		WORKS_ON_GENTOO2020=0
+		WORKS_ON_GENTOO2026=0
 		echo "$fname" is gentoo2023, rpath=$rpath  >&2
+	elif [[ $rpath =~ 'gentoo/2026' || $rpath =~ 'easybuild/software/2026' ]]; then
+		WORKS_ON_NIX=0
+		WORKS_ON_GENTOO2020=0
+		WORKS_ON_GENTOO2023=0
+		echo "$fname" is gentoo2026, rpath=$rpath  >&2
 	fi
 	if [[ $WORKS_ON_NIX -eq 1 && "$(version_lte $min_required_glibc $NIX_GLIBC_VERSION)" == "no" ]]; then
 		WORKS_ON_NIX=0
@@ -89,7 +101,11 @@ for fname in $(find . -type f); do
 	fi
 	if [[ "$(version_lte $min_required_glibc $GENTOO2023_GLIBC_VERSION)" == "no" ]]; then
 		WORKS_ON_GENTOO2023=0
-		echo "$fname" requires a glibc more recent than that provided by Gentoo 2023: $min_required_glibc ">" $GENTOO2020_GLIBC_VERSION >&2
+		echo "$fname" requires a glibc more recent than that provided by Gentoo 2023: $min_required_glibc ">" $GENTOO2023_GLIBC_VERSION >&2
+	fi
+	if [[ "$(version_lte $min_required_glibc $GENTOO2026_GLIBC_VERSION)" == "no" ]]; then
+		WORKS_ON_GENTOO2026=0
+		echo "$fname" requires a glibc more recent than that provided by Gentoo 2026: $min_required_glibc ">" $GENTOO2026_GLIBC_VERSION >&2
 	fi
 	if [[ $WORKS_ON_NIX -eq 1 ]]; then
 		$NIX_LDD "$fname" | grep -E -v $self_contained_shared_objects | grep "not found" >&2
@@ -99,6 +115,8 @@ for fname in $(find . -type f); do
 	GENTOO2020_HAVE_LIBS=$?
 	$GENTOO2023_LDD "$fname" | grep -E -v $self_contained_shared_objects | grep "not found" >&2
 	GENTOO2023_HAVE_LIBS=$?
+	$GENTOO2026_LDD "$fname" | grep -E -v $self_contained_shared_objects | grep "not found" >&2
+	GENTOO2026_HAVE_LIBS=$?
 	if [[ $WORKS_ON_NIX -eq 1 && $NIX_HAVE_LIBS -eq 0 ]]; then
 		WORKS_ON_NIX=0
 		echo "$fname" is missing some libraries in Nix >&2
@@ -111,12 +129,18 @@ for fname in $(find . -type f); do
 		WORKS_ON_GENTOO2023=0
 		echo "$fname" is missing some libraries in Gentoo 2023 >&2
 	fi
-	if [[ $WORKS_ON_GENTOO2020 -eq 1 && $WORKS_ON_GENTOO2023 -eq 1 ]]; then
+	if [[ $GENTOO2026_HAVE_LIBS -eq 0 ]]; then
+		WORKS_ON_GENTOO2026=0
+		echo "$fname" is missing some libraries in Gentoo 2026 >&2
+	fi
+	if [[ $WORKS_ON_GENTOO2020 -eq 1 && $WORKS_ON_GENTOO2023 -eq 1 && $WORKS_ON_GENTOO2026 -eq 1 ]]; then
 		if [[ $WORKS_ON_NIX -eq 1 || $filename =~ $NEWER_PYTHON_REGEXP ]]; then
 			COMPATIBILITY_LAYER="generic"
 		else
 			COMPATIBILITY_LAYER="gentoo"
 		fi
+	elif [[ $WORKS_ON_GENTOO2026 -eq 1 ]]; then
+		COMPATIBILITY_LAYER="gentoo2026"
 	elif [[ $WORKS_ON_GENTOO2023 -eq 1 ]]; then
 		COMPATIBILITY_LAYER="gentoo2023"
 	elif [[ $WORKS_ON_GENTOO2020 -eq 1 ]]; then
