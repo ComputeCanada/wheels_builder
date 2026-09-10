395183441ab9bab445ac7ccb82cf726a9f3cb73a
Charles Coulombe <charles.coulombe@calculquebec.ca>
2026-09-10 09:21:55 -0400

    Updated numpy for 2.5.3 for 2026

--- config/numpy.sh
+++ config/numpy.sh
@@ -6,7 +6,7 @@ fi
 MODULE_BUILD_DEPS="arch/$GENERIC_ARCH flexiblas"
 
 # From 1.26, strictly use meson.
-PYTHON_DEPS="pytest hypothesis meson-python setuptools"
+PYTHON_DEPS="pytest hypothesis meson-python setuptools pytest-timeout"
 PIP_WHEEL_ARGS='
     -Csetup-args=-Dblas=flexiblas
     -Csetup-args=-Dlapack=flexiblas
@@ -17,6 +17,11 @@ PIP_WHEEL_ARGS='
 '
 # cython is not picked up in one test, and it fail. Install cython in the virtual env.
 PRE_TEST_COMMANDS="pip install --ignore-installed 'cython>3.0.0'"
-MODULE_BUILD_DEPS_DEFAULT="cython/.3.2.4"
+if [[ $EBVERSIONGENTOO == 2026 ]]; then
+    MODULE_BUILD_DEPS_DEFAULT="cython/.3.3.0"
+elif [[ $EBVERSIONGENTOO == 2023 ]]; then
+    MODULE_BUILD_DEPS_DEFAULT="cython/.3.2.4"
+fi
 PRE_TEST_COMMANDS='ulimit -s 8192'
-PYTHON_TESTS="numpy.__config__.show(); numpy.test()"
+# test_xerbla_override spinlock under StdEnv/2026. `np.linalg.lapack_lite` is now deprecated.
+PYTHON_TESTS="numpy.__config__.show(); numpy.test(extra_argv=['--timeout=30', '--timeout-method=signal', '-k', 'not test_xerbla_override'])"
