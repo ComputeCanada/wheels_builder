e9cc011fbfde9eddbbadb11d51afd399e951badc
Charles Coulombe <charles.coulombe@calculquebec.ca>
2026-09-24 19:33:25 -0400

    RSeQC now supports python 3

--- build_wheel.sh
+++ build_wheel.sh
@@ -147,7 +147,7 @@ PYTHON_DEPS_DEFAULT="setuptools>70 pip>=25.0,<25.3"
 MODULE_BUILD_DEPS_DEFAULT="$NUMPY_MODULE python-build-bundle pytest cython/$CYTHON_VERSION rust"
 CARGO_BUILD_JOBS=default
 
-PYTHON27_ONLY="cogent OBITools gdata qcli emperor RSeQC preprocess Amara pysqlite IPTest ipaddress functools32 blmath bamsurgeon"
+PYTHON27_ONLY="cogent OBITools gdata qcli emperor preprocess Amara pysqlite IPTest ipaddress functools32 blmath bamsurgeon"
 if [[ $PYTHON27_ONLY =~ " $PACKAGE " ]]; then
 	PYTHON_VERSIONS="python/2.7"
 fi
